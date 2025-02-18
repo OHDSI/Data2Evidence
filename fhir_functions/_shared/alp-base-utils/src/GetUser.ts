1dbc443db540c513a5114195b81f419b75d68228
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2025-02-18 09:55:40 +0800

    split functions (#670)
    * restructure docker-compose
    
    * cleanup caddyfile
    
    * fix docker compose up
    
    * add cli options
    
    * cleanup
    
    * add tag name
    
    * add status and logs
    
    * adjust http tests
    
    * fix http test
    
    * add fhir server port
    
    * split functions
    
    * fix ci
    
    * update codeowners
    
    * update codeowners

---
+++ fhir_functions/_shared/alp-base-utils/src/GetUser.ts
@@ -0,0 +1,26 @@
+import { User } from "./User";
+import { Constants } from "./Constants";
+import { IMRIDBRequest } from "./types";
+import { Logger } from "./index";
+import { decodeString } from "./utils";
+const log = Logger.CreateLogger("mri-util-log");
+import { decode } from "jsonwebtoken";
+
+export const getUser = (req: Pick<IMRIDBRequest, "headers">): User => {
+  try {
+    const lang = (req.headers["x-language"] as string) || "en";
+    const userToken: string = JSON.stringify(
+      decode(req.headers["authorization"].replace(/bearer /i, "")),
+    );
+    
+    const user = new User(JSON.parse(userToken), lang);
+
+    // For HANA JWT Authentication
+    if (req.headers["x-idp-authorization"]) {
+      user.thirdPartyToken = JSON.stringify(decode(req.headers["x-idp-authorization"].replace(/bearer /i, "")));
+    }
+    return user;
+  } catch (err) {
+    throw err;
+  }
+};
