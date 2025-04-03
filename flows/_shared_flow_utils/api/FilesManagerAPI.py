294ce885059a0fdbd5e0539bbc63f1fa600f6b1f
Nicholas Ang <nicholas.ang@data4life-asia.care>
2025-04-04 07:11:25 +0800

    use filesmanagerapi

--- flows/_shared_flow_utils/api/FilesManagerAPI.py
+++ flows/_shared_flow_utils/api/FilesManagerAPI.py
@@ -1,17 +1,11 @@
 import requests
 from prefect.logging import get_run_logger
-from pydantic import BaseModel
 from _shared_flow_utils.api.OpenIdAPI import OpenIdAPI
 
-class FileSaveResponse(BaseModel):
-    id: str
-    username: str
-    dataKey: str
-    fileName: str
 
-class FilesManagerClient:
-    def __init__(self, perseus_host: str):
-        self.base_url = f"http://{perseus_host}:33001/files-manager/api"
+class FilesManagerAPI:
+    def __init__(self):
+        self.url = self.get_service_route("filesManager")
         self.logger = get_run_logger()
         self.auth = OpenIdAPI()
 
@@ -23,11 +17,11 @@ class FilesManagerClient:
         }
 
     def get_file(self, user_data_id: str) -> bytes:
-        url = f"{self.base_url}/{user_data_id}"
-        headers = self._get_headers()
-        
+        url = f"{self.url}/{user_data_id}"
+        headers = self.get_options()
+
         self.logger.info(f"Getting file from {url}")
         response = requests.get(url, headers=headers)
         response.raise_for_status()
-        
+
         return response.content
