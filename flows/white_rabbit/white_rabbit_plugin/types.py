61c4828924c4d8a268d9e6af1bb1d96cb41b4cc9
Nicholas Ang <nicholas.ang@data4life-asia.care>
2025-04-04 08:43:47 +0800

    fix types

--- flows/white_rabbit/white_rabbit_plugin/types.py
+++ flows/white_rabbit/white_rabbit_plugin/types.py
@@ -26,53 +26,17 @@ class WhiteRabbitRequestType(BaseModel):
 
 
 class iniSettings(BaseModel):
-
     WORKING_FOLDER: str
     DATA_TYPE: str
-    SERVER_LOCATION: Optional[str] = None
-    USER_NAME: Optional[str] = None
-    PASSWORD: Optional[str] = None
-    DATABASE_NAME: Optional[str] = None
+    SERVER_LOCATION: str
+    USER_NAME: str
+    PASSWORD: str
+    DATABASE_NAME: str
     DELIMITER: str
     TABLES_TO_SCAN: str
     SCAN_FIELD_VALUES: str
-    MIN_CELL_COUNT: int
-    MAX_DISTINCT_VALUES: int
-    ROWS_PER_TABLE: int
+    MIN_CELL_COUNT: str
+    MAX_DISTINCT_VALUES: str
+    ROWS_PER_TABLE: str
     CALCULATE_NUMERIC_STATS: str
-    NUMERIC_STATS_SAMPLER_SIZE: int
-
-    class Config:
-        validate_assignment = True
-
-    @validator('DELIMITER', pre=True, always=True)
-    def set_delimiter(cls, DELIMITER):
-        return DELIMITER or ","
-
-    @validator('TABLES_TO_SCAN', pre=True, always=True)
-    def set_tables_to_scan(cls, TABLES_TO_SCAN):
-        return TABLES_TO_SCAN or "*"
-
-    @validator('SCAN_FIELD_VALUES', pre=True, always=True)
-    def set_scan_field_values(cls, SCAN_FIELD_VALUES):
-        return SCAN_FIELD_VALUES or "yes"
-
-    @validator('MIN_CELL_COUNT', pre=True, always=True)
-    def set_min_cell_count(cls, MIN_CELL_COUNT):
-        return MIN_CELL_COUNT or 5
-
-    @validator('MAX_DISTINCT_VALUES', pre=True, always=True)
-    def set_max_distinct_values(cls, MAX_DISTINCT_VALUES):
-        return MAX_DISTINCT_VALUES or 1000
-
-    @validator('ROWS_PER_TABLE', pre=True, always=True)
-    def set_rows_per_table(cls, ROWS_PER_TABLE):
-        return ROWS_PER_TABLE or 100000
-
-    @validator('CALCULATE_NUMERIC_STATS', pre=True, always=True)
-    def set_calculate_numeric_stats(cls, CALCULATE_NUMERIC_STATS):
-        return CALCULATE_NUMERIC_STATS or "no"
-
-    @validator('NUMERIC_STATS_SAMPLER_SIZE', pre=True, always=True)
-    def set_numeric_stats_sampler_size(cls, NUMERIC_STATS_SAMPLER_SIZE):
-        return NUMERIC_STATS_SAMPLER_SIZE or 500
+    NUMERIC_STATS_SAMPLER_SIZE: str
