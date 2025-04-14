c438b428e7942afb2e528e9b63817f9093416b93
brandantck <brandan.tanck@gmail.com>
2025-04-14 17:08:32 +0800

    add logic for source endpoint

---
+++ functions/d2e-webapi/src/routes/datasetRoutes.ts
@@ -0,0 +1,27 @@
+import fastify from "fastify";
+
+import { cohortdefinition } from "./cohortdefinition.ts";
+import { conceptset } from "./conceptset.ts";
+import { vocabulary } from "./vocabulary.ts";
+
+export default (app: fastify.FastifyInstance) => {
+  // Add hook to pull datasetId from req header
+  app.addHook("preHandler", (req, res, done) => {
+    if (!req.headers.datasetid) {
+      res.status(400).send("datasetid missing in request header");
+    }
+
+    req.datasetId = req.headers.datasetid as string;
+    done();
+  });
+
+  app.register(cohortdefinition, {
+    prefix: "/cohortdefinition",
+  });
+  app.register(conceptset, {
+    prefix: "/conceptset",
+  });
+  app.register(vocabulary, {
+    prefix: "/vocabulary",
+  });
+};
