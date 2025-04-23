bfe509086636132d06b9d13e3fa237e60240ca67
brandantck <brandan.tanck@gmail.com>
2025-04-23 17:34:53 +0800

    fix invalid reason where clause

--- functions/terminology-svc/src/services/cachedb-dao.ts
+++ functions/terminology-svc/src/services/cachedb-dao.ts
@@ -90,7 +90,7 @@ export class CachedbDAO {
 
       const invalidReasonWhereClause = includeInvalid
         ? ""
-        : `AND invalid_reason = '' `;
+        : `AND invalid_reason IS NULL `;
 
       const sql = `
         select *
