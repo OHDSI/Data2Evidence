d1813a2194d865cf86d809ece913470e230f7323
santan maddi <saisantan2004@gmail.com>
2025-04-30 19:53:22 +0800

    add dqd runner role to ui

--- functions/alp-usermgmt/src/types.d.ts
+++ functions/alp-usermgmt/src/types.d.ts
@@ -34,12 +34,12 @@ export interface IAppRequest extends Request {
 
 export interface RoleMap {
   alp_tenant_id: string[] // list of all tenant ids
-  alp_role_study_write_dqd_researcher: string[] // list of study ids
   alp_role_study_researcher: string[] // list of study ids
   alp_role_study_mgr: string[] // list of study ids
   alp_role_study_admin: string[] // list of study ids
   alp_role_tenant_admin: string[] // list of tenant ids
   alp_role_tenant_viewer: string[] // list of tenant ids
+  alp_role_study_write_dqd_researcher: boolean // alp job runner
   alp_role_user_admin: boolean // alp user admin
   alp_role_system_admin: boolean // alp system admin
   alp_role_nifi_admin: boolean // nifi admin
