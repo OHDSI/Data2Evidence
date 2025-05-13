646fdaf8ea445d70fa30708d52ae13089cc6fa6d
hengxian-jiang <hengxian.jiang@u.nus.edu>
2025-05-13 22:22:36 +0800

    Remove minio related stuff in portal

--- functions/portal/src/minio/minio.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@danet/core'
-import { MinioClient } from './minio.client.ts'
-
-@Module({
-  injectables: [MinioClient]
-})
-export class MinioModule { }
--- functions/portal/src/prefect/dto/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './prefect-flow-run.dto.ts';
-
