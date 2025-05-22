83097490ce4259dc4b75a7aa68fb62f86bb58387
Nicholas Ang <66071629+KangJingA@users.noreply.github.com>
2025-05-22 14:20:10 +0800

    [integrate-1971-1989] Run all cells, Jupyter kernel management (#180)
    * add custom kernel manager
    
    * remove jupyter init code, update starboardembed props
    
    * remove unneeded accesstoken code
    
    * Update Starboard.tsx
    
    * Update authz.ts
    
    * Update Starboard.tsx
    
    * Update launch_docker.py
    
    * Update docker-compose.yml
    
    * Update yarn.lock
    
    * Update yarn.lock

--- ui/apps/portal/src/plugins/Starboard/Starboard.tsx
+++ ui/apps/portal/src/plugins/Starboard/Starboard.tsx
@@ -4,27 +4,23 @@ import { StarboardNotebook } from "./utils/notebook";
 import { StarboardEmbed } from "@data2evidence/d2e-starboard-wrap";
 import { Card, Loader } from "@portal/components";
 import { api } from "../../axios/api";
-import { useActiveDataset, useFeedback, useTranslation } from "../../contexts";
+import { useFeedback, useTranslation } from "../../contexts";
 import { EmptyNotebook } from "./components/EmptyNotebook";
 import { Header } from "./components/NotebookHeader/NotebookHeader";
 import { convertJupyterToStarboard, notebookContentToText } from "./utils/jupystar";
 import { i18nKeys } from "../../contexts/app-context/states";
 import env from "../../env";
 import "./Starboard.scss";
-import { getAuthToken } from "../../containers/auth/auth";
 
 const MRI_ROOT_URL = "analytics-svc";
 const uiFilesUrl = env.REACT_APP_DN_BASE_URL;
-const codeSuggestionUrl = "code-suggestion";
 interface StarboardProps extends PageProps<ResearcherStudyMetadata> {}
 
 export const Starboard: FC<StarboardProps> = ({ metadata }) => {
   const { getText } = useTranslation();
   const { setFeedback } = useFeedback();
   const [loading, setLoading] = useState(true);
-  const { activeDataset } = useActiveDataset();
-  const activeDatasetId = activeDataset.id;
-
+  const activeDatasetId = metadata?.studyId!;
   // JWT Token and Jupyter Kernel Extraction
   const [jwtToken, setJWTToken] = useState("");
 
@@ -38,48 +34,12 @@ os.environ['PYQE_URL'] = '${MRI_ROOT_URL}/'
 os.environ['TOKEN'] = '${jwtToken}'
 os.environ['PYQE_TLS_CLIENT_CA_CERT_PATH'] = ''`;
 
-  const extractJupyterKernel = `\n#%% [esm]
-import * as a from "${uiFilesUrl}starboard-jupyter/index.js"
-a.plugin.register(1, {
-    serverSettings: {
-      baseUrl: "${uiFilesUrl}jupyter",
-      token: "${jwtToken}",
-      appendToken: true,
-      init: {
-          headers: {
-            datasetId: "${activeDatasetId}",
-            }
-          }
-        }
-      }
-    )`;
-
-  const initialiseJupyterKernel = `\n# %% [jupyter]
-Sys.setenv(TREX__AUTHORIZATION_TOKEN = "${jwtToken}")
-Sys.setenv(TREX__DATASET_ID = "${activeDatasetId}")
-  `;
-
   const [runtime, setRuntime] = useState<StarboardEmbed>();
   const [unsaved, setUnsaved] = useState(false);
 
   const [notebooks, setNotebooks] = useState<StarboardNotebook[]>();
   const [activeNotebook, setActiveNotebook] = useState<StarboardNotebook | undefined>();
   const [isShared, setIsShared] = useState<boolean | undefined>();
-
-  // Get Bearer Token for code-suggestion
-  const [accessToken, setToken] = useState("");
-  const getBearerToken = useCallback(async () => {
-    const token = await getAuthToken(false);
-    return `Bearer ${token}`;
-  }, []);
-
-  useEffect(() => {
-    const fetchToken = async () => {
-      const bearerToken = await getBearerToken();
-      setToken(bearerToken);
-    };
-    fetchToken();
-  }, [getBearerToken]);
 
   const updateActiveNotebook = useCallback((notebook?: StarboardNotebook) => {
     setActiveNotebook(notebook);
@@ -117,8 +77,7 @@ Sys.setenv(TREX__DATASET_ID = "${activeDatasetId}")
         const findJwtToken = (await metadata?.getToken()) || "";
         setJWTToken(findJwtToken);
       }
-      notebookContent += setupPYQE + extractJupyterKernel + initialiseJupyterKernel;
-
+      notebookContent += setupPYQE;
       const mount = document.querySelector("#starboard-root");
       while (mount?.firstChild) {
         mount.removeChild(mount.firstChild);
@@ -128,8 +87,10 @@ Sys.setenv(TREX__DATASET_ID = "${activeDatasetId}")
         notebookContent: notebookContent || "",
         src: `${uiFilesUrl}starboard-notebook-base/index.html`,
         preventNavigationWithUnsavedChanges: true,
-        suggestionUrl: `${uiFilesUrl}${codeSuggestionUrl}?datasetId=${activeDatasetId}`,
-        bearerToken: accessToken,
+        serverUrl: uiFilesUrl,
+        token: jwtToken,
+        userId: metadata?.userId,
+        datasetId: activeDatasetId,
         onUnsavedChangesStatusChange: () => setUnsaved(true),
       });
 
