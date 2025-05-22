ee4079ba1eb1b14e30aa57efeb82cb9d3ee80487
Nicholas Ang <nicholas.ang@data4life-asia.care>
2025-05-22 19:37:09 +0800

    add history

--- ui/apps/portal/src/components/Chat/Chat.tsx
+++ ui/apps/portal/src/components/Chat/Chat.tsx
@@ -1,10 +1,10 @@
-import React, { FC, useMemo } from "react";
-import { AiChat, useAiChatApi, useAsStreamAdapter } from "@nlux/react";
+import React, { FC, useMemo, useCallback } from "react";
+import { AiChat, ChatItem, useAiChatApi, useAsStreamAdapter } from "@nlux/react";
 import { Drawer } from "@mui/material";
 import "@nlux/themes/nova.css";
 
 import { createSend, noOpSend } from "./Send";
-
+import { useConversationHistory } from "../../contexts";
 export interface ChatProps {
   open?: boolean;
   onClose?: () => void;
@@ -14,11 +14,45 @@ export interface ChatProps {
 
 const Chat: FC<ChatProps> = ({ open, onClose, datasetId, currentContent }) => {
   const api = useAiChatApi();
+  const content = currentContent();
+
   const send = useMemo(() => {
-    return datasetId ? createSend(datasetId, currentContent()) : noOpSend;
-  }, [datasetId, currentContent()]);
+    return datasetId ? createSend(datasetId, content) : noOpSend;
+  }, [datasetId, content]);
 
   const adapter = useAsStreamAdapter(send, [send]);
+  const { conversationHistory, setConversationHistory } = useConversationHistory();
+
+  const handleMessage = useCallback(
+    (role: "assistant" | "user", message: string | string[]) => {
+      setConversationHistory({
+        role,
+        message: Array.isArray(message) ? message.join("\n") : message,
+      });
+    },
+    [setConversationHistory]
+  );
+
+  const onMessageReceived = useCallback(
+    (payload: any) => {
+      handleMessage("assistant", payload.message);
+    },
+    [handleMessage]
+  );
+
+  const onMessageSent = useCallback(
+    (payload: any) => {
+      handleMessage("user", payload.message);
+    },
+    [handleMessage]
+  );
+
+  const memoizedHistory = useMemo(() => {
+    return conversationHistory.map((item) => ({
+      role: item.role as "assistant" | "user",
+      message: item.message,
+    }));
+  }, [open]);
 
   if (!datasetId) {
     return null;
@@ -39,6 +73,9 @@ const Chat: FC<ChatProps> = ({ open, onClose, datasetId, currentContent }) => {
         adapter={adapter}
         displayOptions={{ colorScheme: "light" }}
         composerOptions={{ placeholder: "Type your query" }}
+        events={{ messageReceived: onMessageReceived, messageSent: onMessageSent }}
+        messageOptions={{ waitTimeBeforeStreamCompletion: "never" }}
+        initialConversation={memoizedHistory}
       />
     </Drawer>
   );
