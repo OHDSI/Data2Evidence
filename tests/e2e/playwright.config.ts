82083aa32921bd93ceb98d3072810aa4ea9d8081
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2025-06-02 09:04:43 +0000

    add e2e test

---
+++ tests/e2e/playwright.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: 'tests',
+  timeout: 30000,
+
+  use: {
+    browserName: 'chromium',
+    headless: true,
+    ignoreHTTPSErrors: true, 
+  },
+  reporter: 'list',
+});
