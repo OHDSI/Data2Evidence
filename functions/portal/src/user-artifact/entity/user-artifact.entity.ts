cc61018f44f98117f628923164da5ed613b165e6
brandantck <brandan.tanck@gmail.com>
2025-06-06 17:29:31 +0800

    add service_name as composite key

--- functions/portal/src/user-artifact/entity/user-artifact.entity.ts
+++ functions/portal/src/user-artifact/entity/user-artifact.entity.ts
@@ -7,6 +7,12 @@ export class UserArtifact extends Audit {
   @PrimaryColumn({ name: 'user_id' })
   userId: string
 
+  @PrimaryColumn({
+    type: "enum",
+    enum: ServiceName,
+  })
+  service_name: ServiceName;
+
   @Column('jsonb')
   artifacts: Record<ServiceName, any[]>
 }
