12f2c638d81920e40d70d776b9962cb71e1b3d76
Jerome Ng <jerome.ng@data4life-asia.care>
2025-06-13 19:11:08 +0800

    refactor types

--- ui/apps/vue-mri-ui-lib/src/query-filter/components/QueryFilterDemo.vue
+++ ui/apps/vue-mri-ui-lib/src/query-filter/components/QueryFilterDemo.vue
@@ -16,67 +16,16 @@ import { QueryFilterCardModel, QueryFilterEvent, QueryFilterChip, QueryFilterMan
 import { type CriteriaOption } from '../utils/CriteriaConfigLoader'
 import QueryFilterTagInputAdapter from '../../lib/ui/QueryFilterTagInputAdapter.vue'
 import { getPortalAPI } from '../../utils/PortalUtils'
-
-// Type definitions
-interface ConceptSetItem {
-  value: string
-  text?: string
-  display_value?: string
-  conceptIds?: number[]
-  concepts?: ConceptItem[]
-  items?: ConceptItem[]
-}
-
-interface ConceptItem {
-  id?: number
-  concept_id?: number
-  CONCEPT_ID?: number
-}
-
-interface ConceptDetail {
-  concept_class_id: string
-  concept_code: string
-  concept_id: number
-  concept_name: string
-  domain_id: string
-  invalid_reason?: string
-  standard_concept: string
-  vocabulary_id: string
-  valid_start_date?: string
-  valid_end_date?: string
-}
-
-interface ApiConfig {
-  configId: string
-  configVersion: string
-  datasetId: string
-}
-
-interface ConceptSetDomainValues {
-  values: ConceptSetItem[]
-  isLoading: boolean
-  loadedStatus: 'NO_RESULTS' | 'HAS_RESULTS' | 'TOO_MANY_RESULTS'
-}
-
-interface TagInputModel {
-  id: string
-  props: {
-    type: string
-    value: ConceptSetItem[]
-    attributePath: string
-    domainFilter: string
-    standardConceptCodeFilter: string
-  }
-}
-
-interface ConceptSetAction {
-  values?: ConceptSetItem
-  config?: any
-}
-
-interface ConceptSetDetails {
-  [conceptSetId: string]: any[]
-}
+import type {
+  ConceptSetItem,
+  ConceptItem,
+  ConceptDetail,
+  ApiConfig,
+  ConceptSetDomainValues,
+  TagInputModel,
+  ConceptSetAction,
+  ConceptSetDetails,
+} from '../types/ConceptSetTypes'
 
 const activeTab = ref<'earliest' | 'all' | 'latest'>('all')
 const showDebug = ref(false)
