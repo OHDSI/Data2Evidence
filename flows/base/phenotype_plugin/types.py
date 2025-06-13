280e01cf5eaa7e52af9d61b5dddc7f7776c16ba7
Zhi Min <zhi.min@data4life-asia.care>
2025-06-13 17:14:39 +0800

    [Internal - 1997] Fix the phenotype result table name (#219)
    * fix the phenotype result tablename
    
    * order by phenotype_result_id
    
    * add replica for phenotype result tables

--- flows/base/phenotype_plugin/types.py
+++ flows/base/phenotype_plugin/types.py
@@ -4,10 +4,8 @@ class PhenotypeOptionsType(BaseModel):
     databaseCode: str   # alpdev_pg
     cdmschemaName: str   # cdmdefault
     cohortschemaName: str   # cdmdefault
-    cohorttableName: str   # cohorts_devtest1_phenotype
     cohortsId: str   # as.integer(c(25,3,4)) or 'default'
     vocabschemaName: str # cdmvocab
-    description: str   
     
     @property
     def use_cache_db(self) -> str:
