d26f082012b8c0ecb9ed84e5e039caadd29c0f89
Nicholas Ang <nicholas.ang@data4life-asia.care>
2025-06-16 17:10:02 +0800

    Update result_viewer.ts

--- functions/strategus-results/src/strategus-results/template/result_viewer.ts
+++ functions/strategus-results/src/strategus-results/template/result_viewer.ts
@@ -1,11 +1,45 @@
-export const RESULT_VIEWER_TEMPLATE = `
+export const RESULT_VIEWER_CODE = `
 library(ShinyAppBuilder)
 library(OhdsiShinyModules)
 
 resultsDatabaseSchema <- $DATABASE_SCHEMA
+
+# Specify the connection to the results database
 resultsConnectionDetails <- DatabaseConnector::createConnectionDetails(
   dbms = "postgresql",
   server = $DATABASE_SERVER,
   user = $DATABASE_USER,
   password = $DATABASE_PASSWORD
-)\n`;
+)
+
+# ADD OR REMOVE MODULES TAILORED TO YOUR STUDY
+shinyConfig <- initializeModuleConfig() |>
+  addModuleConfig(
+    createDefaultAboutConfig()
+  )  |>
+  addModuleConfig(
+    createDefaultDatasourcesConfig()
+  )  |>
+  addModuleConfig(
+    createDefaultCohortGeneratorConfig()
+  ) |>
+  addModuleConfig(
+    createDefaultCohortDiagnosticsConfig()
+  ) |>
+  addModuleConfig(
+    createDefaultCharacterizationConfig()
+  ) |>
+  addModuleConfig(
+    createDefaultPredictionConfig()
+  ) |>
+  addModuleConfig(
+    createDefaultEstimationConfig()
+  ) 
+
+# now create the shiny app based on the config file and view the results
+# based on the connection 
+ShinyAppBuilder::createShinyApp(
+  config = shinyConfig, 
+  connectionDetails = resultsConnectionDetails,
+  resultDatabaseSettings = createDefaultResultDatabaseSettings(schema = resultsDatabaseSchema)
+)`;
