7379feeba47bfbb315a1f700ac673827860cdedc
Khairul Syazwan <khairul.masri@data4life-asia.care>
2025-06-16 23:46:37 +0800

    Add extCohortToIfr method

--- ui/apps/vue-mri-ui-lib/src/utils/ExtCohortToIfr.ts
+++ ui/apps/vue-mri-ui-lib/src/utils/ExtCohortToIfr.ts
@@ -1,24 +1,112 @@
-import { ExtCohortDefinition, IFRDefinition } from './IfrToExtCohortDeps/types'
+import {
+  IFRDefinition,
+  BaseCriteria,
+  ConceptSetElement,
+  ConceptItem,
+  ConceptSet,
+  InclusionCriteria,
+} from './IfrToExtCohortDeps/types'
 import _ from 'lodash'
 import backendConfig from './configs/BackendConfigStatic.json'
+import frontendConfig from './configs/FrontendConfigStatic.json'
 
-export const convertExtCohortToIFR = (extCohort: ExtCohortDefinition): IFRDefinition => {
+export const convertExtCohortToIFR = (extCohort: any, datasetId: string, feconf?, beconf?): IFRDefinition => {
   const conceptSets = extCohort.ConceptSets
   const inclusionRules = extCohort.InclusionRules
-  const [rulesWithDemographics, rulesWithoutDemographics] = inclusionRules.reduce(
-    ([withDemographics, withoutDemographics], rule) => {
-      if (rule.expression.DemographicCriteriaList && rule.expression.DemographicCriteriaList.length > 0) {
-        withDemographics.push(rule)
-      } else {
-        withoutDemographics.push(rule)
-      }
-      return [withDemographics, withoutDemographics]
-    },
-    [[], []] as [typeof inclusionRules, typeof inclusionRules]
-  )
+  const entryCriteriaList = extCohort.PrimaryCriteria.CriteriaList
+  const exitCriteria = extCohort.CensoringCriteria
+
+  const configMeta = feconf ? feconf.meta : frontendConfig.meta
+
+  let instanceCounter = 1
+
+  const filterInclusionRules = (inclusionRules: InclusionCriteria) => {
+    const hasDemographicCriteria = inclusionRules.filter(
+      rule =>
+        rule.expression?.DemographicCriteriaList &&
+        rule.expression.DemographicCriteriaList.length > 0 &&
+        rule.expression.CriteriaList.length === 0
+    )
+
+    const hasOtherCriteria = inclusionRules.filter(
+      rule => !rule.expression?.DemographicCriteriaList || rule.expression.DemographicCriteriaList.length === 0
+    )
+
+    return { hasDemographicCriteria, hasOtherCriteria }
+  }
+  const { hasDemographicCriteria, hasOtherCriteria } = filterInclusionRules(inclusionRules)
+
+  const getConfigPath = (cohortDefinitionKey: string) => {
+    const path = _.findKey(backendConfig.config.patient.interactions, value => {
+      return value.cohortDefinitionKey === cohortDefinitionKey
+    })
+    return path ? `patient.interactions.${path}` : 'patient.interactions.default'
+  }
+
+  const getDefaultAttributePath = (configPath: string): string => {
+    const interactionPath = `config.${configPath}.attributes`
+    const attributes = _.get(frontendConfig, interactionPath, {})
+
+    const attributeKey = _.findKey(attributes, attribute => _.get(attribute, 'filtercard.initial') === true)
+    return attributeKey ? `attributes.${attributeKey}` : undefined
+  }
+
+  const getConceptNameConfigPath = (configPath: string): string => {
+    const searchPath = `config.${configPath}.attributes`
+    const attributes = _.get(backendConfig, searchPath, {})
+    const attributeKey = _.findKey(attributes, (_, key) => key.toLowerCase().includes('concept_name'))
+
+    return `attributes.${attributeKey}`
+  }
+
+  const getFilterCardName = (cohortDefinitionKey: string) => {
+    const configPath = getConfigPath(cohortDefinitionKey)
+
+    const config = _.get(backendConfig.config, configPath)
+    if (!config) {
+      throw new Error(`Config not found for path: ${configPath}`)
+    }
+
+    return config.name
+  }
+
+  const getConceptSet = (codesetId: number): ConceptSet => {
+    const conceptSet = conceptSets.find((set: ConceptSet) => set.id === codesetId)
+    if (!conceptSet) {
+      throw new Error(`Concept set with ID ${codesetId} not found`)
+    }
+    return conceptSet
+  }
+
+  const extractConceptNames = (conceptItems: ConceptSetElement): string[] => {
+    return conceptItems.map((item: ConceptItem) => item.concept.CONCEPT_NAME)
+  }
 
   const createBasicDataFilterCard = () => {
-    const basicFilterCards = []
+    const seenProperties = new Set<string>()
+
+    const basicFilterCards = [
+      {
+        configPath: 'patient.attributes.Age',
+        instanceID: 'patient.attributes.Age',
+        type: 'Attribute',
+        constraints: {
+          content: [],
+          type: 'BooleanContainer',
+          op: 'OR',
+        },
+      },
+      {
+        configPath: 'patient.attributes.Gender_concept_name',
+        instanceID: 'patient.attributes.Gender_concept_name',
+        type: 'Attribute',
+        constraints: {
+          content: [],
+          type: 'BooleanContainer',
+          op: 'OR',
+        },
+      },
+    ]
 
     const OPERATORS = {
       gte: '>=',
@@ -27,54 +115,37 @@ export const convertExtCohortToIFR = (extCohort: ExtCohortDefinition): IFRDefini
       lt: '<',
     }
 
-    rulesWithDemographics.forEach(rule => {
-      const demographicCriteriaList = rule.expression.DemographicCriteriaList
+    if (hasDemographicCriteria.length > 0) {
+      hasDemographicCriteria.forEach(rule => {
+        const demographicCriteriaList = rule.expression.DemographicCriteriaList
 
-      demographicCriteriaList.forEach(criteria => {
-        if (criteria.Age && !Array.isArray(criteria.Age)) {
-          basicFilterCards.push({
-            configPath: 'patient.attributes.Age',
-            instanceID: 'patient.attributes.Age',
-            type: 'Attribute',
-            constraints: {
-              content:
-                criteria.Age.Op && criteria.Age.Value
-                  ? [
-                      {
-                        type: 'Expression',
-                        operator: OPERATORS[criteria.Age.Op],
-                        value: criteria.Age.Value,
-                      },
-                    ]
-                  : [],
-              type: 'BooleanContainer',
-              op: 'OR',
-            },
-          })
-        }
-
-        if (criteria.Gender && Array.isArray(criteria.Gender)) {
-          criteria.Gender.forEach(gender => {
-            basicFilterCards.push({
-              configPath: 'patient.attributes.Gender_concept_name',
-              instanceID: 'patient.attributes.Gender_concept_name',
-              type: 'Attribute',
-              constraints: {
-                content: [
-                  {
-                    type: 'Expression',
-                    operator: '=',
-                    value: gender.CONCEPT_NAME,
-                  },
-                ],
-                type: 'BooleanContainer',
-                op: 'OR',
-              },
+        demographicCriteriaList.forEach(criteria => {
+          if (criteria.Age && !Array.isArray(criteria.Age) && !seenProperties.has('Age')) {
+            basicFilterCards[0].constraints.content.push({
+              type: 'Expression',
+              operator: OPERATORS[criteria.Age.Op],
+              value: criteria.Age.Value,
             })
-          })
-        }
+            seenProperties.add('Age')
+          }
+
+          if (criteria.Gender && Array.isArray(criteria.Gender) && !seenProperties.has('Gender')) {
+            const genderConcept = criteria.Gender[0]
+            if (
+              genderConcept.CONCEPT_NAME.toUpperCase() === 'MALE' ||
+              genderConcept.CONCEPT_NAME.toUpperCase() === 'FEMALE'
+            ) {
+              basicFilterCards[1].constraints.content.push({
+                type: 'Expression',
+                operator: '=',
+                value: genderConcept.CONCEPT_NAME.toUpperCase(),
+              })
+            }
+            seenProperties.add('Gender')
+          }
+        })
       })
-    })
+    }
 
     return [
       {
@@ -102,96 +173,127 @@ export const convertExtCohortToIFR = (extCohort: ExtCohortDefinition): IFRDefini
     ]
   }
 
-  const getConfigPath = (cohortDefinitionKey: string) => {
-    const path = _.findKey(backendConfig.config.patient.interactions, value => {
-      return value.cohortDefinitionKey === cohortDefinitionKey
-    })
-    return path ? `patient.interactions.${path}` : 'patient.interactions.default'
-  }
-
-  const getConceptNameConfigPath = (cohortDefinitionKey: string) => {
-    const parentPath = getConfigPath(cohortDefinitionKey)
-    const searchPath = `${parentPath}.attributes`
-    const searchString = 'concept_name'
-    const attributes = _.get(backendConfig, searchPath)
-    const attributeKey = _.findKey(attributes, (_, key) => key.toLowerCase().includes(searchString))
-    return `${searchPath}.${attributeKey}`
-  }
-
-  const getFilterCardName = (cohortDefinitionKey: string) => {
-    const configPath = getConfigPath(cohortDefinitionKey)
-    return backendConfig.config[configPath].name
-  }
-
-  const createFilterCards = () => {
-    const filterCards = []
-
-    rulesWithoutDemographics.forEach((rule, index) => {
+  const createInclusionFilterCards = () => {
+    return hasOtherCriteria.map(rule => {
+      const filterCards = []
       const criteriaList = rule.expression.CriteriaList
-      let currentInstanceNumber = index + 1
-
-      const currentFilterCard = criteriaList.map(criteria => {
+      criteriaList.forEach(criteria => {
         const cohortDefinitionKey = Object.keys(criteria.Criteria)[0]
-        const filterCard = {
-          configPath: getConfigPath(cohortDefinitionKey),
-          instanceNumber: currentInstanceNumber,
-          instanceID: `${getConfigPath(cohortDefinitionKey)}.${currentInstanceNumber}`,
-          name: getFilterCardName(cohortDefinitionKey),
-          inactive: false,
-          isEntry: false,
-          isExit: false,
-          type: 'FilterCard',
-          attributes: {
-            content: criteriaList.map((criteria, index) => {
-              const attributeName = Object.keys(criteria.Criteria)[index]
-
-              if (criteria.Criteria[attributeName]['CodesetId']) {
-                const conceptSet = conceptSets.find(
-                  conceptSet => conceptSet.id === criteria.Criteria[attributeName]['CodesetId']
-                )
-
-                const conceptName = conceptSet.expression.items[0].concept.CONCEPT_NAME
-
-                return {
-                  configPath: getConceptNameConfigPath(cohortDefinitionKey),
-                  instanceID: `${getConceptNameConfigPath(cohortDefinitionKey)}.${currentInstanceNumber}`,
-                  type: 'Attribute',
-                  constraints: {
-                    content: [
-                      {
-                        type: 'Expression',
-                        operator: '=',
-                        value: conceptName,
-                      },
-                    ],
-                    type: 'BooleanContainer',
-                    op: 'OR',
-                  },
-                }
-              }
-            }),
-            type: 'BooleanContainer',
-            op: 'AND',
-          },
-          advanceTimeFilter: null,
-        }
-        return filterCard
+        filterCards.push(createFilterCard(criteria.Criteria, cohortDefinitionKey, false, false))
       })
 
-      const returnValue = { content: currentFilterCard, type: 'BooleanContainer', op: 'OR' }
-      filterCards.push(returnValue)
+      return {
+        content: filterCards,
+        type: 'BooleanContainer',
+        op: 'OR',
+      }
     })
-    return filterCards
   }
-  const basicFilters = createBasicDataFilterCard()
-  const filterCards = createFilterCards()
-  const allFilterCards = [...basicFilters, ...filterCards]
 
+  const createFilterCard = (criteria: BaseCriteria, cohortDefinitionKey: string, isEntry: boolean, isExit: boolean) => {
+    const configPath = getConfigPath(cohortDefinitionKey)
+    const instanceID = `${configPath}.${instanceCounter}`
+    const name = getFilterCardName(cohortDefinitionKey)
+
+    const buildAttributes = (criteria, configPath, instanceId) => {
+      if (!('CodesetId' in criteria[cohortDefinitionKey])) {
+        const defaultAttributePath = getDefaultAttributePath(configPath)
+        if (!defaultAttributePath) {          
+          return
+        } else {
+          return {
+            configPath: `${configPath}.${defaultAttributePath}`,
+            instanceID: `${instanceId}.${defaultAttributePath}.`,
+            type: 'Attribute',
+            constraints: {
+              content: [],
+              type: 'BooleanContainer',
+              op: 'OR',
+            },
+          }
+        }
+      } else {
+        const conceptSet = getConceptSet(criteria[cohortDefinitionKey].CodesetId)
+        const conceptNames = extractConceptNames(conceptSet.expression.items)
+        const conceptNameConfigPath = getConceptNameConfigPath(configPath)
+        return {
+          configPath: `${configPath}.${conceptNameConfigPath}`,
+          instanceID: `${instanceId}.${conceptNameConfigPath}.`,
+          type: 'Attribute',
+          constraints: {
+            content: conceptNames.map(name => ({
+              type: 'Expression',
+              operator: '=',
+              value: name,
+            })),
+            type: 'BooleanContainer',
+            op: 'OR',
+          },
+        }
+      }
+    }
+
+    const filterCard = {
+      configPath,
+      instanceNumber: instanceCounter,
+      instanceID,
+      name,
+      inactive: false,
+      isEntry,
+      isExit,
+      type: 'FilterCard',
+      attributes: {
+        content: [buildAttributes(criteria, configPath, instanceID)].filter(Boolean),
+        type: 'BooleanContainer',
+        op: 'AND',
+      },
+      advanceTimeFilter: null,
+    }
+    instanceCounter++
+    return filterCard
+  }
+
+  const createEntryFilterCards = () => {
+    if (entryCriteriaList.length > 0) {
+      const criteria = entryCriteriaList[0]
+      const cohortDefinitionKey = Object.keys(criteria)[0]
+      return [
+        {
+          content: [createFilterCard(criteria, cohortDefinitionKey, true, false)],
+          type: 'BooleanContainer',
+          op: 'OR',
+        },
+      ]
+    }
+    return []
+  }
+
+  const createExitFilterCards = () => {
+    if (exitCriteria.length > 0) {
+      const criteria = exitCriteria[0]
+      const cohortDefinitionKey = Object.keys(criteria)[0]
+      return [
+        {
+          content: [createFilterCard(criteria, cohortDefinitionKey, false, true)],
+          type: 'BooleanContainer',
+          op: 'OR',
+        },
+      ]
+    }
+    return []
+  }
+
+  const basicFilters = createBasicDataFilterCard()
+  const filterCards = createInclusionFilterCards()
+  const entryCards = createEntryFilterCards()
+  const exitCards = createExitFilterCards()
+
+  const allFilterCards = [...basicFilters, ...filterCards, ...entryCards, ...exitCards]
   return {
     filter: {
       configMetadata: {
-        id: '', // Change to config ID
-        version: '', // Change to config version
+        id: configMeta.configId,
+        version: configMeta.configVersion,
       },
       cards: {
         content: allFilterCards,
@@ -209,7 +311,6 @@ export const convertExtCohortToIFR = (extCohort: ExtCohortDefinition): IFRDefini
       { attributeId: 'patient.attributes.pcount', categoryId: 'y1' },
     ],
     metadata: { version: 3 },
-    datasetId: '', // Add dataset ID here
+    datasetId,
   }
 }
-
