c2c1a13d72ed75dffa7894ff24698b9c20ef2fb1
Nicholas Ang <nicholas.ang@data4life-asia.care>
2025-06-16 14:18:18 +0800

    get read db credentials from trex

--- functions/strategus-results/src/strategus-results/services.ts
+++ functions/strategus-results/src/strategus-results/services.ts
@@ -5,14 +5,15 @@ import {
   IKernelConnection,
 } from "@jupyterlab/services";
 import { services } from "../env.ts";
-
+import { USER_SCOPE, IDatabaseCredential, IReadCredential } from "../type";
 interface IKernelModel extends Kernel.IModel {
   username: string;
 }
 
 export const createStrategusResultsViewer = async (
   token: string,
-  studyId: string
+  studyId: string,
+  databaseCode: string
 ): Promise<void> => {
   console.log("Creating Strategus Results Viewer for study:", studyId);
 
@@ -26,35 +27,97 @@ export const createStrategusResultsViewer = async (
       }),
     });
 
-    let kernel: Kernel.IKernelConnection | null = null;
+    const kernel: IKernelConnection = await getKernel(studyId, manager);
+    const readCredentials = await getReadCredentials(databaseCode);
+    kernel.dispose();
+  } catch (error) {
+    throw error;
+  }
+};
 
+const getKernel: IKernelConnection = async (
+  studyId: string,
+  manager: KernelManager
+): Promise<IKernelConnection> => {
+  try {
     await manager.ready;
     await manager.refreshRunning();
 
     const running = manager.running();
     const existingKernel: IKernelConnection = running.find(
-      (kernel) => kernel.name === studyId
+      (kernel) => kernel.username === studyId
     );
 
     if (existingKernel) {
       console.log(`Kernel found for study ${studyId}:`, existingKernel.id);
-      kernel = manager.connectTo({
+      return manager.connectTo({
         model: { name: "r_ohdsi_docker", id: existingKernel.id },
       });
     } else {
       console.log(`No kernel found for study ${studyId}, creating new kernel`);
-      kernel = await manager.startNew({
+      return await manager.startNew({
         name: "r_ohdsi_docker",
         env: {
           KERNEL_USERNAME: studyId,
         },
       });
     }
-
-    if (kernel) {
-      kernel.dispose();
-    }
   } catch (error) {
-    throw error;
+    throw Error(`Failed to create or connect to kernel for study ${studyId}`);
   }
+};
+
+const getReadCredentials = async (
+  databaseCode: string
+): Promise<IReadCredential> => {
+  const dbm = Trex.databaseManager();
+
+  const databaseCredentials =
+    dbm.getDatabaseCredentials() as IDatabaseCredential[];
+
+  const parsedDatabaseCredentials = databaseCredentials.map((db) => {
+    const { credentials, dialect, name, port, ...rest } = db;
+
+    const decryptedCreds = credentials.reduce<{ [key: string]: string }>(
+      (acc, c) => {
+        const { username, password, userScope } = c;
+        switch (userScope) {
+          case USER_SCOPE.ADMIN:
+          case USER_SCOPE.READ:
+            acc[userScope.toLowerCase() + "User"] = username;
+            acc[userScope.toLowerCase() + "Password"] = password;
+          default:
+            acc["user"] = username;
+            acc["password"] = password;
+        }
+        return acc;
+      },
+      {}
+    );
+
+    return {
+      code: rest.code,
+      credentials: decryptedCreds,
+    };
+  });
+
+  const readCredentials = parsedDatabaseCredentials.find(
+    (parsedDatabaseCredential) => parsedDatabaseCredential.code === databaseCode
+  );
+
+  if (!readCredentials) {
+    throw Error("No database credentials");
+  }
+
+  if (
+    !readCredentials.credentials.readUser ||
+    !readCredentials.credentials.readPassword
+  ) {
+    throw Error("Missing read credentials");
+  }
+
+  return {
+    readUser: readCredentials.credentials.readUser,
+    readPassword: readCredentials.credentials.readPassword,
+  };
 };
---
+++ functions/strategus-results/src/type.d.ts
@@ -0,0 +1,30 @@
+export enum USER_SCOPE {
+  ADMIN = "Admin",
+  READ = "Read",
+}
+
+export interface IDatabaseCredentialCredentialsItem {
+  username: string;
+  userScope: string;
+  serviceScope: string;
+  password: string;
+}
+
+export interface IDatabaseCredential {
+  code: string;
+  id: string;
+  host: number;
+  port: string;
+  name: string;
+  dialect: string;
+  credentials: IDatabaseCredentialCredentialsItem[];
+  vocab_schemas: string[];
+  publications: string[];
+  db_extra: any;
+  cation_: string;
+}
+
+export interface IReadCredential {
+  readUser: string;
+  readPassword: string;
+}
