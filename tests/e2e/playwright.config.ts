5e5b0c7a0ab12a948a352a80a6b0851bb2a6e56e
mwaiyee <waiyee.man@data4life-asia.care>
2025-06-23 14:49:16 +0800

    edit timeout

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: 'tests',
-  timeout: 100000,
+  timeout: 200000,
 
   use: {
     browserName: 'chromium',
