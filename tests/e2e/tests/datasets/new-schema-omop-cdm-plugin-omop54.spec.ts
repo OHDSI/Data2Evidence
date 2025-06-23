bc7e7a854874a4a800d30d4171fd6b1cb7f5311a
Afreen Sikandara <afreen.sikandara@data4life-asia.care>
2025-06-23 13:22:55 +0800

    Fix login page link

--- tests/e2e/tests/datasets/new-schema-omop-cdm-plugin-omop54.spec.ts
+++ tests/e2e/tests/datasets/new-schema-omop-cdm-plugin-omop54.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('dataset-new-schema-omop-cdm-plugin-54', async ({ page }) => {
-  await page.goto('https://localhost:41100/portal');
+  await page.goto('https://localhost:443/portal');
   await page.locator('input[name="identifier"]').click();
   await page.locator('input[name="identifier"]').fill('admin');
   await page.locator('input[name="password"]').click();
