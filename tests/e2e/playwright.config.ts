d1beee9e719d91f95ca687b0dc9e8cb2c32f4ced
Afreen Sikandara <afreen.sikandara@data4life-asia.care>
2025-06-23 17:02:10 +0800

    Increase timeout

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: 'tests',
-  timeout: 120000,
+  timeout: 240000,
 
   use: {
     browserName: 'chromium',
     headless: true,
     ignoreHTTPSErrors: true,
-    actionTimeout: 30000,
-    navigationTimeout: 60000
+    actionTimeout: 120000,
+    navigationTimeout: 120000
   },
   reporter: 'list',
 });
