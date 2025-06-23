dd9388b7b7f708733d9cfc10d60a2092b8a68008
Afreen Sikandara <afreen.sikandara@data4life-asia.care>
2025-06-23 16:24:09 +0800

    uncomment

--- tests/e2e/tests/datasets/new-schema-i2b2-plugin.spec.ts
+++ tests/e2e/tests/datasets/new-schema-i2b2-plugin.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('dataset-new-schema-i2b2-plugin', async ({ page }) => {
-  // await page.goto('https://localhost:41100/portal');
-  // await page.locator('input[name="identifier"]').click();
-  // await page.locator('input[name="identifier"]').fill('admin');
-  // await page.locator('input[name="password"]').click();
-  // await page.locator('input[name="password"]').fill('Updatepassword12345');
-  // await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.goto('https://localhost:443/portal');
+  await page.locator('input[name="identifier"]').click();
+  await page.locator('input[name="identifier"]').fill('admin');
+  await page.locator('input[name="password"]').click();
+  await page.locator('input[name="password"]').fill('Updatepassword12345');
+  await page.getByRole('button', { name: 'Sign in' }).click();
   await page.getByTestId('button').nth(1).click();
   await page.getByRole('button', { name: 'Switch to Admin portal' }).click();
   await page.getByRole('link', { name: 'Datasets' }).click();
