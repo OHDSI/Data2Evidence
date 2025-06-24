52d5a5467e1f15f924164e53e7c7e05d0c4ae7ed
santan maddi <saisantan2004@gmail.com>
2025-06-24 17:21:33 +0800

    redirect to portal instead of sign-in

--- tests/e2e/tests/researcher_dataset-Information.spec.ts
+++ tests/e2e/tests/researcher_dataset-Information.spec.ts
@@ -5,7 +5,7 @@ test.use({
 });
 
 test('Researcher-Dataset information', async ({ page }) => {
-  await page.goto('https://localhost:443/sign-in');
+  await page.goto('https://localhost:443/portal');
   await page.locator('input[name="identifier"]').fill('admin');
   await page.locator('input[name="password"]').fill('Updatepassword12345');
   await page.getByRole('button', { name: 'Sign in' }).click();
