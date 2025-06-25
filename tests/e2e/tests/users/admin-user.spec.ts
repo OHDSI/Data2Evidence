1244e15a0eb9cb63201c0d23db3a22e323ace26c
Nicholas Ang <nicholas.ang@data4life-asia.care>
2025-06-25 14:54:48 +0800

    Update admin-user.spec.ts

--- tests/e2e/tests/users/admin-user.spec.ts
+++ tests/e2e/tests/users/admin-user.spec.ts
@@ -5,7 +5,7 @@ test.use({
 })
 
 test('test', async ({ page }) => {
-  await page.goto('https://localhost:41100/sign-in')
+  await page.goto('https://localhost:443/portal')
   await page.locator('input[name="identifier"]').click()
   await page.locator('input[name="identifier"]').fill('admin')
   await page.locator('input[name="password"]').click()
