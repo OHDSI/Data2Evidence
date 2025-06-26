a3d540eb296f5a14f11832c29e3d01ca2ccba066
brandantck <brandan.tanck@gmail.com>
2025-06-26 16:27:24 +0800

    update port

--- tests/e2e/tests/dataflow/create-edit-save-delete.spec.ts
+++ tests/e2e/tests/dataflow/create-edit-save-delete.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test('dataflow-create-edit-save-delete', async ({ page }) => {
-  await page.goto('https://localhost:41100/portal')
+  await page.goto('https://localhost:443/portal')
   await page.locator('input[name="identifier"]').click()
   await page.locator('input[name="identifier"]').fill('admin')
   await page.locator('input[name="password"]').click()
