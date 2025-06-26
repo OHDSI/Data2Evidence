06844f383a5cfcb659629e24259e93b0cb1b5240
hengxian-jiang <hengxian.jiang@u.nus.edu>
2025-06-26 21:52:46 +0800

    Add dataset permission management e2e test

---
+++ tests/e2e/tests/datasets/dataset-user-permission.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+
+test('dataset-user-permission', async ({ page }) => {
+  console.log('Sign in')
+  await page.goto(`https://localhost:443/portal`)
+  await page.locator('input[name="identifier"]').click()
+  await page.locator('input[name="identifier"]').fill('admin')
+  await page.locator('input[name="password"]').click()
+  await page.locator('input[name="password"]').fill('Updatepassword12345')
+  await page.getByRole('button', { name: 'Sign in' }).click()
+
+  console.log('Switch to Admin portal')
+  await page.getByTestId('button').nth(1).click();
+  await page.getByRole('button', { name: 'Switch to Admin portal' }).click();
+
+  console.log('Go to Datasets')
+  await page.getByRole('link', { name: 'Datasets' }).click();
+  await expect(page.getByRole('button', { name: 'Select action' }).first()).toBeVisible();
+
+  console.log('Manage dataset permissions')
+  await page.getByRole('button', { name: 'Select action' }).first().click();
+  await page.getByRole('option', { name: 'Permissions' }).click();
+  await page.getByRole('tab', { name: 'Access' }).click();
+
+  console.log('Grant access to admin user')
+  await page.getByTestId('dialog').getByTestId('button').click();
+  await expect(page.getByRole('menuitem', { name: 'admin' })).toBeVisible();
+  await page.getByRole('menuitem', { name: 'admin' }).click();
+  await expect(page.getByRole('cell', { name: 'admin' })).toBeVisible();
+
+  console.log('Revoke access to admin user')
+  await expect(page.getByRole('button', { name: 'Revoke' })).toBeVisible();
+  await page.getByRole('button', { name: 'Revoke' }).click();
+  await page.waitForTimeout(3000);
+  await expect(page.getByTestId('snackbar').locator('div').filter({ hasText: 'You\'ve revoked access for' }).first()).toBeVisible();
+});
