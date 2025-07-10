5613499dccf9d20c89b723c6fa27020aa3ebb811
santan maddi <saisantan2004@gmail.com>
2025-07-10 16:38:30 +0800

    success: dataset data quality tab

--- tests/e2e/tests/datasets/researcher-dataset_quality.spec.ts
+++ tests/e2e/tests/datasets/researcher-dataset_quality.spec.ts
@@ -5,14 +5,14 @@ test.use({
 });
 
 test('test', async ({ page }) => {
-  await page.goto('https://localhost:443/sign-in');
-  await page.locator('input[name="identifier"]').click();
+  await page.goto('https://localhost:443/portal');
   await page.locator('input[name="identifier"]').fill('admin');
-  await page.locator('input[name="password"]').click();
   await page.locator('input[name="password"]').fill('Updatepassword12345');
   await page.getByRole('button', { name: 'Sign in' }).click();
-  await page.goto('https://localhost:443/portal/researcher');
-  await page.getByText('Demo dataset').first().click();
+
+  await page.waitForTimeout(5000)
+  await page.getByText(new RegExp('^Demo dataset$')).click();
+
   await page.getByRole('tab', { name: 'Data Quality' }).click();
   await expect(page.getByTestId('card-content')).toContainText('Corrected pass percentage for NA and Errors: 94% (886/943).');
 });
