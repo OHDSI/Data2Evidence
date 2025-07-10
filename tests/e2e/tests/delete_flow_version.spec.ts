0c49550a49f0d009fb7961791dee62c644667879
santan maddi <saisantan2004@gmail.com>
2025-07-10 17:20:23 +0800

    success: delete a version of ui-flow

--- tests/e2e/tests/delete_flow_version.spec.ts
+++ tests/e2e/tests/delete_flow_version.spec.ts
@@ -1,17 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test.use({
-  ignoreHTTPSErrors: true
-});
-
 test('test', async ({ page }) => {
-  await page.goto('https://localhost:443/portal/public/overview');
-  await page.getByTestId('button').nth(1).click();
-  await page.locator('input[name="identifier"]').click();
+  await page.goto('https://localhost:443/portal');
   await page.locator('input[name="identifier"]').fill('admin');
-  await page.locator('input[name="password"]').click();
   await page.locator('input[name="password"]').fill('Updatepassword12345');
   await page.getByRole('button', { name: 'Sign in' }).click();
+
   await page.getByTestId('button').nth(1).click();
   await page.getByRole('button', { name: 'Switch to Admin portal' }).click();
   await page.getByRole('link', { name: 'ETL' }).click();
@@ -20,18 +14,22 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Name' }).press('Tab');
   await page.getByRole('textbox', { name: 'Comment' }).fill('testcase_flow');
   await page.getByRole('button', { name: 'Create' }).click();
-  await page.getByText('PythonStableRun python code.').click();
+
+  await page.locator('div.node-type-selection__title').filter({ hasText: new RegExp('^Python$') }).click();  
   await page.getByRole('button', { name: 'Save' }).click();
   await page.getByRole('textbox', { name: 'Describe your changes' }).fill('init 1');
   await page.getByRole('button', { name: 'Save' }).click();
   await page.getByRole('button', { name: 'Add node' }).click();
-  await page.getByText('SQLStableRun SQL in a database').click();
+  
+  await page.locator('div.node-type-selection__title').filter({ hasText: new RegExp('^SQL$') }).click();
   await page.getByRole('button', { name: 'Save' }).click();
   await page.getByRole('textbox', { name: 'Describe your changes' }).fill('init 2');
   await page.getByRole('button', { name: 'Save' }).click();
+
   await page.getByLabel('Show version history').getByRole('button').click();
   await page.getByRole('button', { name: 'Delete' }).first().click();
   await page.getByRole('button', { name: 'Delete' }).click();
+
   await expect(page.getByRole('list')).toContainText('Version #2');
   await expect(page.getByRole('list')).toContainText('Version #1');
 });
