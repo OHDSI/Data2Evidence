2702bbce74e35835b1be6c00ccb57290827be3b4
santan maddi <saisantan2004@gmail.com>
2025-07-10 17:11:40 +0800

    success: switch to admin portal

--- tests/e2e/tests/setup/admin_portal.spec.ts
+++ tests/e2e/tests/setup/admin_portal.spec.ts
@@ -1,22 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test.use({
-  actionTimeout: 60000, 
-  navigationTimeout: 60000, // 1 minute for navigation
-  ignoreHTTPSErrors: true
-});
-
 test('test', async ({ page }) => {
-  await page.goto('https://localhost:443/sign-in');
-  await page.locator('input[name="identifier"]').click();
+  await page.goto('https://localhost:443/portal');
   await page.locator('input[name="identifier"]').fill('admin');
-  await page.locator('input[name="identifier"]').press('Tab');
-  await page.getByRole('button').filter({ hasText: /^$/ }).press('Tab');
   await page.locator('input[name="password"]').fill('Updatepassword12345');
   await page.getByRole('button', { name: 'Sign in' }).click();
-  await page.goto('https://localhost:41100/portal/researcher');
+
   await page.getByTestId('button').nth(1).click();
   await page.getByRole('button', { name: 'Switch to Admin portal' }).click();
+
   await expect(page.getByRole('heading')).toContainText('Users');
   await expect(page.getByTestId('button')).toContainText('Add user');
 });
