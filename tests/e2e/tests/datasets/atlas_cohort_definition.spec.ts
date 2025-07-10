d891739b1a81660fa8263b37757d6602aa323c3e
santan maddi <saisantan2004@gmail.com>
2025-07-10 16:35:06 +0800

    success: atlas cohort definition save

--- tests/e2e/tests/datasets/atlas_cohort_definition.spec.ts
+++ tests/e2e/tests/datasets/atlas_cohort_definition.spec.ts
@@ -1,27 +1,23 @@
 import { test, expect } from '@playwright/test';
 
-test.use({
-  ignoreHTTPSErrors: true
-});
-
 test('test', async ({ page }) => {
-  await page.goto('https://localhost:443/sign-in');
-  await page.locator('input[name="identifier"]').click();
+  await page.goto('https://localhost:443/portal');
   await page.locator('input[name="identifier"]').fill('admin');
-  await page.locator('input[name="identifier"]').press('Tab');
-  await page.getByRole('button').filter({ hasText: /^$/ }).press('Tab');
   await page.locator('input[name="password"]').fill('Updatepassword12345');
   await page.getByRole('button', { name: 'Sign in' }).click();
-  await page.goto('https://localhost:443/portal/researcher');
-  await page.getByText('Demo dataset updated').click();
+
+  await page.waitForTimeout(5000) // Wait for the metadata table to load
+  await page.getByText(new RegExp('^Demo dataset$')).click();
+
   await page.getByRole('link', { name: 'Cohorts' }).click();
   await page.getByRole('button', { name: 'Atlas' }).click();
+
+  await page.waitForTimeout(10000)
   await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByRole('button', { name: 'Accept' }).click();
   await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByText('New Cohort').click();
   await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByRole('textbox', { name: 'New Cohort Definition' }).click();
-  await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByRole('textbox', { name: 'New Cohort Definition' }).press('ControlOrMeta+a');
   await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByRole('textbox', { name: 'New Cohort Definition' }).fill('testcohort_atlas');
-  await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByRole('button', { name: '' }).click();
+  await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByTitle(new RegExp('^Save$')).click();
   await page.locator('iframe[title="Atlas Lite"]').contentFrame().locator('a').filter({ hasText: 'Back to Cohorts' }).click();
   await expect(page.locator('#pane-left')).toContainText('testcohort_atlas');
   await expect(page.locator('#pane-left')).toContainText('Atlas Cohort Definition');
