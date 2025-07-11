2c2ecce15310bd3ce90a209aa3cc677ee32e52ae
Nicholas Ang <nicholas.ang@data4life-asia.care>
2025-07-11 13:46:39 +0800

    add form except cohort

--- ui/apps/analysis/src/features/flow/containers/Node/NodeTypes/TreatmentPatternsNode/TreatmentPatternsDrawer.tsx
+++ ui/apps/analysis/src/features/flow/containers/Node/NodeTypes/TreatmentPatternsNode/TreatmentPatternsDrawer.tsx
@@ -1,7 +1,15 @@
 import React, { ChangeEvent, FC, useCallback, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { NodeProps } from "reactflow";
-import { Box } from "@portal/components";
+import {
+  Box,
+  TextInput,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  SelectChangeEvent,
+} from "@portal/components";
 import { useFormData } from "~/features/flow/hooks";
 import {
   markStatusAsDraft,
@@ -28,6 +36,7 @@ export interface TreatmentPatternsDrawerProps
 interface FormData extends TreatmentPatternsNodeData {}
 console.log(IncludeTreatments, FilterTreatments, CensorType);
 const EMPTY_FORM_DATA: FormData = {
+  name: "",
   cohorts: [],
   includeTreatments: IncludeTreatments.StartDate,
   indexDateOffset: 0,
@@ -57,6 +66,7 @@ export const TreatmentPatternsDrawer: FC<TreatmentPatternsDrawerProps> = ({
   useEffect(() => {
     if (node.data) {
       setFormData({
+        name: node.data.name,
         cohorts: node.data.cohorts,
         includeTreatments: node.data.includeTreatments,
         indexDateOffset: node.data.indexDateOffset,
@@ -91,7 +101,171 @@ export const TreatmentPatternsDrawer: FC<TreatmentPatternsDrawerProps> = ({
   }, [formData]);
   return (
     <NodeDrawer {...props} width="500px" onOk={handleOk} onClose={onClose}>
-      <Box mb={4}>hello</Box>
+      <Box mb={4}>
+        <TextInput
+          label="Name"
+          value={formData.name}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            onFormDataChange({ name: e.target.value })
+          }
+        />
+      </Box>
+      <Box mb={4}>
+        <FormControl variant="standard" fullWidth>
+          <InputLabel shrink>Include Treatments</InputLabel>
+          <Select
+            value={formData.includeTreatments}
+            onChange={(e: SelectChangeEvent) =>
+              onFormDataChange({
+                includeTreatments: e.target.value,
+              })
+            }
+          >
+            {Object.values(IncludeTreatments).map((option) => (
+              <MenuItem key={option} value={option}>
+                {option}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+      <Box mb={4}>
+        <TextInput
+          label="indexDateOffset"
+          value={formData.indexDateOffset}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            onFormDataChange({ indexDateOffset: e.target.value })
+          }
+          type="number"
+        />
+      </Box>
+      <Box mb={4}>
+        <TextInput
+          label="minEraDuration"
+          value={formData.minEraDuration}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            onFormDataChange({ minEraDuration: e.target.value })
+          }
+          type="number"
+        />
+      </Box>
+      <Box mb={4}>
+        <TextInput
+          label="splitEventCohorts"
+          value={formData.splitEventCohorts}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            onFormDataChange({ splitEventCohorts: e.target.value })
+          }
+        />
+      </Box>
+      <Box mb={4}>
+        <TextInput
+          label="splitTime"
+          value={formData.splitTime}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            onFormDataChange({ splitTime: e.target.value })
+          }
+          type="number"
+        />
+      </Box>
+      <Box mb={4}>
+        <TextInput
+          label="eraCollapseSize"
+          value={formData.eraCollapseSize}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            onFormDataChange({ eraCollapseSize: e.target.value })
+          }
+          type="number"
+        />
+      </Box>
+      <Box mb={4}>
+        <TextInput
+          label="combinationWindow"
+          value={formData.combinationWindow}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            onFormDataChange({ combinationWindow: e.target.value })
+          }
+          type="number"
+        />
+      </Box>
+      <Box mb={4}>
+        <TextInput
+          label="minPostCombinationDuration"
+          value={formData.minPostCombinationDuration}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            onFormDataChange({ minPostCombinationDuration: e.target.value })
+          }
+          type="number"
+        />
+      </Box>
+      <Box mb={4}>
+        <FormControl variant="standard" fullWidth>
+          <InputLabel shrink>filterTreatments</InputLabel>
+          <Select
+            value={formData.filterTreatments}
+            onChange={(e: SelectChangeEvent) =>
+              onFormDataChange({
+                filterTreatments: e.target.value,
+              })
+            }
+          >
+            {Object.values(FilterTreatments).map((option) => (
+              <MenuItem key={option} value={option}>
+                {option}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+      <Box mb={4}>
+        <TextInput
+          label="maxPathLength"
+          value={formData.maxPathLength}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            onFormDataChange({ maxPathLength: e.target.value })
+          }
+          type="number"
+        />
+      </Box>
+      <Box mb={4}>
+        <TextInput
+          label="ageWindow"
+          value={formData.ageWindow}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            onFormDataChange({ ageWindow: e.target.value })
+          }
+          type="number"
+        />
+      </Box>
+      <Box mb={4}>
+        <TextInput
+          label="minCellCount"
+          value={formData.minCellCount}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            onFormDataChange({ minCellCount: e.target.value })
+          }
+          type="number"
+        />
+      </Box>
+      <Box mb={4}>
+        <FormControl variant="standard" fullWidth>
+          <InputLabel shrink>censorType</InputLabel>
+          <Select
+            value={formData.censorType}
+            onChange={(e: SelectChangeEvent) =>
+              onFormDataChange({
+                CensorType: e.target.value,
+              })
+            }
+          >
+            {Object.values(CensorType).map((option) => (
+              <MenuItem key={option} value={option}>
+                {option}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
     </NodeDrawer>
   );
 };
