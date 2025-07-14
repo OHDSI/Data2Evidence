13a74c1f8d7cb034315e2ca819441890d4aa8a33
santan maddi <saisantan2004@gmail.com>
2025-07-14 13:00:10 +0800

    comment atlas cohort definition

--- tests/e2e/tests/datasets/atlas_cohort_definition.spec.ts
+++ tests/e2e/tests/datasets/atlas_cohort_definition.spec.ts
@@ -1,24 +1,26 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://localhost:443/portal');
-  await page.locator('input[name="identifier"]').fill('admin');
-  await page.locator('input[name="password"]').fill('Updatepassword12345');
-  await page.getByRole('button', { name: 'Sign in' }).click();
+// TODO: The test case in the manual test suite is not relevant, as the old Atlas is not available in the D2E setup.
 
-  await page.waitForTimeout(5000)
-  await page.locator('div.dataset-card__title').filter({ hasText: new RegExp('^Demo dataset$') }).click();  
+// test('test', async ({ page }) => {
+//   await page.goto('https://localhost:443/portal');
+//   await page.locator('input[name="identifier"]').fill('admin');
+//   await page.locator('input[name="password"]').fill('Updatepassword12345');
+//   await page.getByRole('button', { name: 'Sign in' }).click();
 
-  await page.getByRole('link', { name: 'Cohorts' }).click();
-  await page.getByRole('button', { name: 'Atlas' }).click();
+//   await page.waitForTimeout(5000)
+//   await page.locator('div.dataset-card__title').filter({ hasText: new RegExp('^Demo dataset$') }).click();  
 
-  await page.waitForTimeout(10000)
-  await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByRole('button', { name: 'Accept' }).click();
-  await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByText('New Cohort').click();
-  await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByRole('textbox', { name: 'New Cohort Definition' }).click();
-  await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByRole('textbox', { name: 'New Cohort Definition' }).fill('testcohort_atlas');
-  await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByTitle(new RegExp('^Save$')).click();
-  await page.locator('iframe[title="Atlas Lite"]').contentFrame().locator('a').filter({ hasText: 'Back to Cohorts' }).click();
-  await expect(page.locator('#pane-left')).toContainText('testcohort_atlas');
-  await expect(page.locator('#pane-left')).toContainText('Atlas Cohort Definition');
-});
+//   await page.getByRole('link', { name: 'Cohorts' }).click();
+//   await page.getByRole('button', { name: 'Atlas' }).click();
+
+//   await page.waitForTimeout(10000)
+//   await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByRole('button', { name: 'Accept' }).click();
+//   await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByText('New Cohort').click();
+//   await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByRole('textbox', { name: 'New Cohort Definition' }).click();
+//   await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByRole('textbox', { name: 'New Cohort Definition' }).fill('testcohort_atlas');
+//   await page.locator('iframe[title="Atlas Lite"]').contentFrame().getByTitle(new RegExp('^Save$')).click();
+//   await page.locator('iframe[title="Atlas Lite"]').contentFrame().locator('a').filter({ hasText: 'Back to Cohorts' }).click();
+//   await expect(page.locator('#pane-left')).toContainText('testcohort_atlas');
+//   await expect(page.locator('#pane-left')).toContainText('Atlas Cohort Definition');
+// });
