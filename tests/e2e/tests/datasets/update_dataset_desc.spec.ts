a37cb351a04c7d30b64cccb9ebbf00b9454c375c
santan maddi <saisantan2004@gmail.com>
2025-07-14 12:38:29 +0800

    success: update dataset desc

--- tests/e2e/tests/datasets/update_dataset_desc.spec.ts
+++ tests/e2e/tests/datasets/update_dataset_desc.spec.ts
@@ -9,15 +9,15 @@ test('test', async ({ page }) => {
   await page.getByTestId('button').nth(1).click();
   await page.getByRole('button', { name: 'Switch to Admin portal' }).click();
   await page.getByRole('link', { name: 'Datasets' }).click();
-  await page.getByRole('button', { name: 'Select action' }).nth(2).click();
+  await page.getByRole('button', { name: 'Select action' }).click();
   await page.getByRole('option', { name: 'Update dataset' }).click();
   await page.getByRole('textbox', { name: 'Dataset summary' }).fill('Demo dataset updated');
-  await page.locator('#simplemde-editor-1-wrapper').getByRole('textbox').fill('Demo  updated');
+  await page.locator('#simplemde-editor-1-wrapper').getByRole('textbox').fill('Demo dataset updated');
   await page.getByRole('button', { name: 'Save' }).click();
   await page.getByRole('link', { name: 'Account' }).click();
   await page.getByRole('button', { name: 'Switch to Researcher portal' }).click();
   
-  await expect(page.locator('div.dataset-card__title div.dataset-card__summary').filter({ hasText: new RegExp('^Demo dataset updated$') })).toContainText('Demo dataset updated');
+  await expect(page.locator('div.dataset-card__summary').filter({ hasText: new RegExp('^Demo dataset updated$') })).toContainText('Demo dataset updated');
   
   await page.getByText('Demo dataset', { exact: true }).click();
   await expect(page.getByRole('paragraph')).toContainText('Demo dataset updated');
