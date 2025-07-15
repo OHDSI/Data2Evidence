3d7fe800b5bc3822f49879017cd2797debc36be3
mwaiyee <waiyee.man@data4life-asia.care>
2025-07-15 15:33:02 +0800

    fix cp from clipboard

--- tests/e2e/tests/datasets/copy-datasets-id-schema-name.spec.js
+++ tests/e2e/tests/datasets/copy-datasets-id-schema-name.spec.js
@@ -1,7 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('copy-dataset-id-schema-name', async ({ page }) => {
-  await page.goto('https://localhost:443/portal');
+
+test('copy-dataset-id-schema-name', async ({ page, context}) => {
+  await context.grantPermissions(["clipboard-read", "clipboard-write"]);
+
+  await page.goto(`https://localhost:443/portal`)
   await page.locator('input[name="identifier"]').click();
   await page.locator('input[name="identifier"]').fill('admin');
   await page.locator('input[name="password"]').click();
@@ -12,10 +15,14 @@ test('copy-dataset-id-schema-name', async ({ page }) => {
   await page.getByRole('button', { name: 'Switch to Admin portal' }).click();
   await page.getByRole('link', { name: 'Datasets' }).click();
 
-  // Click button for dataset ID and schema name
-  const dataset_id = await page.locator('button.alp-icon-button--icon-only').first().locator('xpath=ancestor::td').textContent();
-  const schema_name = await page.locator('button.alp-icon-button--icon-only').nth(1).locator('xpath=ancestor::td').textContent();
+  // Get the dataset ID
+  await page.locator('div.alp-text__copy-button-container').nth(0).locator('button.alp-icon-button--icon-only').click();
+  const dataset_id = await page.evaluate(async () => await navigator.clipboard.readText());
 
+  // Get the schema name
+  await page.locator('div.alp-text__copy-button-container').nth(1).locator('button.alp-icon-button--icon-only').click();
+  const schema_name = await page.evaluate(async () => await navigator.clipboard.readText());
+  
   // Check if the cell contains the dataset ID and schema name
   await expect(page.locator('tbody')).toContainText(dataset_id);
   await expect(page.locator('tbody')).toContainText(schema_name);
