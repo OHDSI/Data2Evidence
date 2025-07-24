5690bfd4ff3bc34f7f201b6a05fb64cf909bda90
Afreen <afreen.sikandara@data4life-asia.care>
2025-07-24 16:05:11 +0800

    e2e PA Bookmarks (#422)
    * e2e PA Bookmarks
    
    * Update bookmark.spec.ts
    
    Signed-off-by: Afreen <afreen.sikandara@data4life-asia.care>
    
    * Skip test cases for testing
    
    * FIx
    
    * Increase timeout for tests
    
    * Skip test for faster testing in GH
    
    * Fix delete
    
    * Add screenshot for debugging
    
    * Remove verify delete filter
    
    * Enable tests
    
    * Fix typo
    
    ---------
    
    Signed-off-by: Afreen <afreen.sikandara@data4life-asia.care>

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: 'tests',
-  timeout: 120000, // 2 minutes per test
+  timeout: 180000, // 3 minutes per test
   expect: {
     timeout: 120000 // 20 seconds for expect conditions
   },
