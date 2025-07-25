f3fd250786671ccab50e4d5f28c8dfc32aac6252
Nicholas Ang <nicholas.ang@data4life-asia.care>
2025-07-25 16:09:29 +0800

    Update cli.sh

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -273,6 +273,9 @@ case $cmd in
         cmd="docker pull --platform linux/amd64 ${DOCKER_IMAGE_PREFIX:-ghcr.io/ohdsi/}d2e/flow-base:${PLUGINS_IMAGE_TAG}" # not part of dc.yml
         echo . $cmd
         $cmd
+        cmd="docker pull --platform linux/amd64 ${DOCKER_IMAGE_PREFIX:-ghcr.io/ohdsi/}d2e-r-ohdsi-kernel:${DOCKER_TAG_NAME}"
+        echo . $cmd
+        $cmd
         cmd="$dockerbasecmd pull"
         echo . $cmd
         $cmd
