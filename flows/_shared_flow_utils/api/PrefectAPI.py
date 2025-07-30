e598592ad0e8405b648f6213af05776236338576
alicia-koh <alicia.koh@data4life-asia.care>
2025-07-30 09:37:25 +0800

    Use refresh token for long running flows

--- flows/_shared_flow_utils/api/PrefectAPI.py
+++ flows/_shared_flow_utils/api/PrefectAPI.py
@@ -1,13 +1,13 @@
+import os
+import httpx
 from jwt import decode
-from datetime import timedelta
+from time import time
 
 from prefect import task
-from prefect.cache_policies import TASK_SOURCE
+from prefect.blocks.system import Secret
 
 from _shared_flow_utils.types import AuthToken, AppTokenPayload, User
 
-# Token serialized as pickle and stored temporarily in .prefect/storage
-@task(log_prints=True, cache_policy=TASK_SOURCE, cache_expiration=timedelta(seconds=300))
 def get_auth_token_from_input() -> AuthToken:
     iter = AuthToken.receive(key_prefix="authtoken", timeout=300, poll_interval=3)
     return iter.next()
@@ -16,8 +16,40 @@ def get_token_value(auth_token: AuthToken) -> str:
     return auth_token.token.get_secret_value().replace("Bearer ", "")
 
 def get_third_party_token_value(auth_token: AuthToken) -> str:
-    return auth_token.thirdpartytoken.get_secret_value()
-  
+    third_party_token = auth_token.thirdpartytoken.get_secret_value()
+    decoded_exp = decode(third_party_token, options={"verify_signature": False}).get(
+        "exp"
+    )
+
+    if decoded_exp and time() <= decoded_exp:
+        return third_party_token
+
+    # Use refresh token to get a new token
+    refresh_token_endpoint = Secret.load("refresh-token-endpoint")
+    refresh_token_client_id = Secret.load("refresh-token-client-id")
+    refresh_token_client_secret = Secret.load("refresh-token-client-secret")
+
+    if not (refresh_token_endpoint and refresh_token_client_id and refresh_token_client_secret):
+        raise ValueError("Missing required secrets for token refresh")
+
+    third_party_refresh_token = get_third_party_refresh_token_value(auth_token)
+
+    payload = {
+        "grant_type": "refresh_token",
+        "refresh_token": third_party_refresh_token,
+        "client_id": refresh_token_client_id.get_secret_value(),
+        "client_secret": refresh_token_client_secret.get_secret_value(),
+    }
+
+    response = httpx.post(
+        refresh_token_endpoint.get_secret_value(),
+        data=payload,
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+    )
+
+    response.raise_for_status()
+    return response.json().get("id_token")
+
 def get_third_party_refresh_token_value(auth_token: AuthToken) -> str:
     return auth_token.thirdpartyrefreshtoken.get_secret_value()
 
@@ -28,7 +60,7 @@ def build_user_from_token(token: str) -> User:
         user = {
             "user_id": decoded_token.get("oid", decoded_token.get("sub", "")),
             "name": decoded_token.get("name", ""),
-            "email": decoded_token.get("email", "")
+            "email": decoded_token.get("email", ""),
         }
     else:
         user = {
