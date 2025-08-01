b419aadee600966f0569639f022b6ee5fcd4e856
santan maddi <saisantan2004@gmail.com>
2025-08-01 16:22:55 +0800

    change selector

--- tests/e2e/tests/09-patient-analytics/patient-group_comparison.spec.ts
+++ tests/e2e/tests/09-patient-analytics/patient-group_comparison.spec.ts
@@ -87,8 +87,9 @@ test('pa-compare-cohorts', async ({ page }) => {
   // Verify the comparison modal is visible
   await expect(page.locator('.modal-body')).toBeVisible();
   await page.locator('.mainChartToolbar').getByTitle('Export to File').click();
-  await page.locator('dropdownmenuitem-container hasNoSubMenu menu-item hover-select').getByText('Export to PNG File').click();
+
   const downloadPromise = page.waitForEvent('download');
+  await page.locator('#pane-left').getByText('Export to PNG File').click();  
   const download = await downloadPromise;
 
   // Verify the downloaded file is a PNG
