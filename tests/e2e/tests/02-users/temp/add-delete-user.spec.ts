00a32c196841ef279b589822c7aeba1c839f2b3f
hengxian-jiang <hengxian.jiang@u.nus.edu>
2025-08-05 11:40:29 +0800

    test specific element timeout

--- tests/e2e/tests/02-users/temp/add-delete-user.spec.ts
+++ tests/e2e/tests/02-users/temp/add-delete-user.spec.ts
@@ -31,9 +31,9 @@ test(TEST_NAME, async ({ page }) => {
   await page.getByRole('button', { name: 'Add' }).click()
   console.log("adding user...")
   // Check if user is added
-  await page.waitForTimeout(6000)
+  // await page.waitForTimeout(6000)
   console.log("after wait timeout")
-  await expect(page.getByRole('cell', { name: 'test_user' })).toBeVisible()
+  await expect(page.getByRole('cell', { name: 'test_user' })).toBeVisible({ timeout: 10000 })
 
   // Delete user
   await page.getByRole('button', { name: 'Delete' }).nth(1).click()
