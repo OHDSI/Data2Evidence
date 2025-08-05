dbcdf63422fff0fb5115a605b709d9f65f006635
hengxian-jiang <hengxian.jiang@u.nus.edu>
2025-08-05 11:09:36 +0800

    add temp test folder

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
-  testDir: 'tests',
+  testDir: 'tests/02-users/temp',
   timeout: 180000, // 3 minutes per test
   expect: {
     timeout: 120000 // 20 seconds for expect conditions
