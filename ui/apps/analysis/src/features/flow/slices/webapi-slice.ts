a31f459fca4c8e41ac6788d81e39f5d4a62efb83
Nicholas Ang <nicholas.ang@data4life-asia.care>
2025-08-06 16:49:52 +0800

    update webapi types

--- ui/apps/analysis/src/features/flow/slices/webapi-slice.ts
+++ ui/apps/analysis/src/features/flow/slices/webapi-slice.ts
@@ -1,12 +1,16 @@
 import { createApi } from "@reduxjs/toolkit/dist/query/react";
 import { createBaseQueryFn } from "./base-query";
+import { WebApiCohortDefinitionResultDto } from "../types";
 
 export const webapiSlice = createApi({
   reducerPath: "webapi",
   baseQuery: createBaseQueryFn("d2e-webapi/"),
   tagTypes: ["WebApi", "WebApiCohortDefinition"],
   endpoints: (builder) => ({
-    getWebApiCohortDefinitions: builder.query<any, void>({
+    getWebApiCohortDefinitions: builder.query<
+      WebApiCohortDefinitionResultDto[],
+      void
+    >({
       query: () => "/cohortdefinition",
     }),
   }),
