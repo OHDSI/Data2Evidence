196ff9b3c553d706be3b7a4e7867e14aa5adbb54
Nicholas Ang <nicholas.ang@data4life-asia.care>
2025-08-14 13:38:51 +0800

    fix cdm and lookup service

--- functions/perseus/src/cdm-schema/cdm-schema.service.ts
+++ functions/perseus/src/cdm-schema/cdm-schema.service.ts
@@ -20,9 +20,14 @@ export class CDMSchemaService {
   }
 
   private async getSchemaFromCsv(cdmVersion: string): Promise<TableSchema[]> {
-    const p = `${path
-      .dirname(path.fromFileUrl(import.meta.url).replace(/\/cdm-schema/, ""))
-      .replace(/\/usr\/src/, ".")}/model/sources/CDM/CDMv${cdmVersion}.csv`;
+    const p = `${path(
+      path.fromFileUrl(import.meta.url).replace(/\/cdm-schema/, "")
+    )
+      .replace(/\/usr\/src/, ".")
+      .replace(
+        /\/var\/tmp\/sb-compile-trex\/d2ef/,
+        Deno.env.get("TREX_FUNCTION_PATH")
+      )}/model/sources/CDM/CDMv${cdmVersion}.csv`;
 
     try {
       const fileContent = await Deno.readTextFile(p);
