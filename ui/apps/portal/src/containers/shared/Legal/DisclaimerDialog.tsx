70c984eee84d19e30cf729dd54a2254defb8b16a
Nicholas Ang <nicholas.ang@data4life-asia.care>
2025-08-15 15:25:55 +0800

    Update DisclaimerDialog.tsx

--- ui/apps/portal/src/containers/shared/Legal/DisclaimerDialog.tsx
+++ ui/apps/portal/src/containers/shared/Legal/DisclaimerDialog.tsx
@@ -1,10 +1,12 @@
-import React, { FC, useCallback } from "react";
+import React, { FC, useState, useCallback, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
 import { Dialog, Button } from "@portal/components";
+import { api } from "../../../axios/api";
+import { ConfigTypes } from "../../../constant";
 import Divider from "@mui/material/Divider";
 import { useDisclaimer, useTranslation } from "../../../contexts";
 import { config } from "../../../config";
-import { LegalCard } from "./LegalCard";
 import "./DisclaimerDialog.scss";
 import { i18nKeys } from "../../../contexts/app-context/states";
 
@@ -12,6 +14,8 @@ export const DisclaimerDialog: FC = () => {
   const navigate = useNavigate();
   const { disclaimer, setIsDisclaimerAccepted } = useDisclaimer();
   const { getText } = useTranslation();
+  const [configs, setConfigs] = useState<{ [key: string]: string }>({});
+  const [loading, setLoading] = useState(false);
 
   const shouldOpen = disclaimer.shouldDisplay === true && !disclaimer.isDisclaimerAccepted;
   const handleAccept = useCallback(() => {
@@ -22,6 +26,28 @@ export const DisclaimerDialog: FC = () => {
     navigate(config.ROUTES.logout);
   }, [navigate]);
 
+  const fetchConfigs = useCallback(async () => {
+    try {
+      setLoading(true);
+      const configs = await api.systemPortal.getConfigsByTypes([ConfigTypes.DISCLAIMER]);
+      setConfigs({ ...configs });
+    } catch (error: any) {
+      // TODO: handle error
+      // if ("message" in error) {
+      //   setError({ message: error.message });
+      // }
+    } finally {
+      setLoading(false);
+    }
+  }, [shouldOpen]);
+
+  useEffect(() => {
+    if (!shouldOpen) {
+      return;
+    }
+    fetchConfigs();
+  }, [shouldOpen]);
+
   return (
     <Dialog
       title={getText(i18nKeys.DISCLAIMER_DIALOG__TITLE)}
@@ -30,7 +56,7 @@ export const DisclaimerDialog: FC = () => {
       maxWidth="lg"
     >
       <div className="disclaimer-dialog__content">
-        <LegalCard />
+        <ReactMarkdown>{configs[ConfigTypes.DISCLAIMER]}</ReactMarkdown>
       </div>
       <Divider />
       <div className="button-group-actions">
