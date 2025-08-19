ff14570e68939f739db7843001e8550d463c86bf
santan maddi <saisantan2004@gmail.com>
2025-08-19 17:48:42 +0800

    add local dev changes but default to False

--- flows/_shared_flow_utils/api/BaseAPI.py
+++ flows/_shared_flow_utils/api/BaseAPI.py
@@ -7,6 +7,8 @@ class BaseAPI:
     def __init__(self):
         self.python_verify_ssl = Variable.get("python_verify_ssl")
         self.tls_internal_ca_cert = Secret.load("tls-internal-ca-cert")
+        is_dev_env = Variable.get("is-dev-env")
+        self.is_dev_env = is_dev_env if is_dev_env else False # change to True for local development
         
         if self.python_verify_ssl == 'true' and self.tls_internal_ca_cert is None:
             raise ValueError("'tls-internal-ca-cert' prefect secret is undefined")
@@ -27,7 +29,11 @@ class BaseAPI:
 
     def get_options(self) -> dict[str, str]:
         # Prefect task to get token from flow input
+        bearer_token = None
+        if not self.is_dev_env:
+            bearer_token = GetAuthTokens().get_auth_token().get_secret_value()
+
         return {
             "Content-Type": "application/json",
-            "Authorization": GetAuthTokens().get_auth_token().get_secret_value()
+            "Authorization": bearer_token if bearer_token else "Bearer <token>"
         }
