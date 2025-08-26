b6486cbf77c073640b8794f7bdee70b2eee44732
santan maddi <saisantan2004@gmail.com>
2025-08-26 15:15:51 +0800

    add body limit to the endpoint

--- functions/jobplugins/index.ts
+++ functions/jobplugins/index.ts
@@ -22,6 +22,7 @@ const app = express();
 const env = Deno.env.toObject();
 
 app.use(express.json({ limit: "50mb" }));
+app.use(express.raw({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(extractUsernameFromJwt);
