9b5f729e5d57727036b061fe6f00ff87af495c48
alicia-koh <alicia.koh@data4life-asia.care>
2025-08-26 17:32:36 +0800

    Update V1.0.0.2.0__add_replica_identity.sql

--- flows/data_management/data_management_plugin/db/migrations/postgres/changesets/omop5-4/V1.0.0.2.0__add_replica_identity.sql
+++ flows/data_management/data_management_plugin/db/migrations/postgres/changesets/omop5-4/V1.0.0.2.0__add_replica_identity.sql
@@ -1,8 +1,7 @@
 --liquibase formatted sql
 --changeset alp:V1.0.0.2.0__add_replica_identity
---ignore:true
 -- preconditions onFail:MARK_RAN 
 -- Add replica identity for cohort and cohort_definition
-ALTER TABLE cohort REPLICA IDENTITY FULL;
+-- ALTER TABLE cohort REPLICA IDENTITY FULL;
 
-ALTER TABLE cohort_definition REPLICA IDENTITY FULL;
+-- ALTER TABLE cohort_definition REPLICA IDENTITY FULL;
