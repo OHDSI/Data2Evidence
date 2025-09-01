bb4a71231e26e4c5bdb0b286dea1afd63d56d0c6
Khairul Syazwan <khairul.masri@data4life-asia.care>
2025-09-01 10:58:00 +0800

    Create NotificationStack, remove sap var & eventBus

--- ui/apps/vue-mri-ui-lib/src/App.vue
+++ ui/apps/vue-mri-ui-lib/src/App.vue
@@ -1,18 +1,6 @@
 <template>
   <div id="app" class="UiTheme_d4l mri-app-vue-container">
-    <fatal
-      v-if="getFatalNotification.show"
-      :message="getFatalNotification.message"
-      :header="getText('MRI_PA_CONFIG_ADMIN_ERROR')"
-      @ok="okFatal"
-    />
-    <alert
-      v-if="getAlertNotification.show"
-      :message="getAlertNotification.message"
-      :header="getAlertNotification.title"
-      :messageType="getAlertNotification.messageType"
-      @ok="okAlert"
-    />
+    <NotificationStack />
     <!-- <ui5adaptor /> -->
     <splashScreen v-if="getInitialLoad" />
     <patientanalytics v-show="!getInitialLoad" />
@@ -21,16 +9,14 @@
 
 <script lang="ts">
 declare var sap
-import { mapActions, mapGetters, mapMutations } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 import configSelection from './components/ConfigSelection.vue'
-import notification from './components/Notification.vue'
 import patientanalytics from './components/PatientAnalytics.vue'
 import ui5adaptor from './components/UI5Adaptor.vue'
 import SplashScreen from './components/SplashScreen.vue'
 import store from './store'
-import { MESSAGE_ALERT_SHOW_TOGGLE, MESSAGE_FATAL_SHOW_TOGGLE } from './store/mutation-types'
-
-const eventbus = sap.ui.getCore().getEventBus()
+import { QueryFilter } from './query-filter'
+import NotificationStack from './components/NotificationStack.vue'
 
 export default {
   store,
@@ -71,13 +57,7 @@ export default {
     window.addEventListener('alp-dataset-change', datasetChangeHandler)
   },
   computed: {
-    ...mapGetters([
-      'getConfigSelectionDialogState',
-      'getFatalNotification',
-      'getAlertNotification',
-      'getText',
-      'getInitialLoad',
-    ]),
+    ...mapGetters(['getConfigSelectionDialogState', 'getInitialLoad']),
   },
   methods: {
     ...mapActions([
@@ -89,23 +69,15 @@ export default {
       'refreshPatientCount',
       'setLocale',
     ]),
-    ...mapMutations([MESSAGE_FATAL_SHOW_TOGGLE, MESSAGE_ALERT_SHOW_TOGGLE]),
-    okFatal() {
-      this[MESSAGE_FATAL_SHOW_TOGGLE]()
-      eventbus.publish('VUE_NAVIGATE_FLP')
-    },
-    okAlert() {
-      this[MESSAGE_ALERT_SHOW_TOGGLE]()
-    },
   },
   components: {
     patientanalytics,
     ui5adaptor,
     configSelection,
-    fatal: notification,
-    alert: notification,
     SplashScreen,
+    NotificationStack,
   },
 }
 </script>
 <style lang="scss" src="./styles/style.scss"></style>
+
