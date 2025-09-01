8860be7976fbf3a861bc8aec5ddc3159d29a993b
Khairul Syazwan <khairul.masri@data4life-asia.care>
2025-09-01 14:03:39 +0800

    Add compatConfig

--- ui/apps/vue-mri-ui-lib/src/components/NotificationStack.vue
+++ ui/apps/vue-mri-ui-lib/src/components/NotificationStack.vue
@@ -1,21 +1,3 @@
-<template>
-  <div class="notification-stack">
-    <fatal
-      v-if="getFatalNotification.show"
-      :message="getFatalNotification.message"
-      :header="getText('MRI_PA_CONFIG_ADMIN_ERROR')"
-      @ok="okFatal"
-    />
-    <alert
-      v-if="getAlertNotification.show"
-      :message="getAlertNotification.message"
-      :header="getAlertNotification.title"
-      :messageType="getAlertNotification.messageType"
-      @ok="okAlert"
-    />
-  </div>
-</template>
-
 <script lang="ts">
 import { mapGetters, mapMutations } from 'vuex'
 import notification from './Notification.vue'
@@ -23,6 +5,9 @@ import { MESSAGE_ALERT_SHOW_TOGGLE, MESSAGE_FATAL_SHOW_TOGGLE } from '../store/m
 
 export default {
   name: 'NotificationStack',
+  compatConfig: {
+    MODE: 3,
+  },
   components: {
     fatal: notification,
     alert: notification,
@@ -41,6 +26,26 @@ export default {
   },
 }
 </script>
+
+<template>
+  <div class="notification-stack">
+    <fatal
+      v-if="getFatalNotification.show"
+      :message="getFatalNotification.message"
+      :header="getText('MRI_PA_CONFIG_ADMIN_ERROR')"
+      @ok="okFatal"
+    />
+    <alert
+      v-if="getAlertNotification.show"
+      :message="getAlertNotification.message"
+      :header="getAlertNotification.title"
+      :messageType="getAlertNotification.messageType"
+      @ok="okAlert"
+    />
+  </div>
+</template>
+
+
 
 <style scoped>
 .notification-stack {
