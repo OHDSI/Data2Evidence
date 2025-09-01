5f0e075e2f0a9d8e68b7deacb4b817544709d4b1
Zhimin-arya <zhi.min@data4life-asia.care>
2025-09-01 11:47:11 +0800

    increase workers and maxfailures for test only

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -20,6 +20,6 @@ export default defineConfig({
     ['list'], // You can combine multiple reporters
     ['playwright-ctrf-json-reporter', {}]
   ],
-  workers: 1,
-  maxFailures: process.env.CI ? 1 : 0
+  workers: 50%,
+  maxFailures: process.env.CI ? 10 : 0
 })
