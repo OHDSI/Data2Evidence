df12a966213bfd56594e203336d111e8243702c3
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2025-09-07 14:13:03 +0800

    fix

--- services/trex/ext/circe/build.sh
+++ services/trex/ext/circe/build.sh
@@ -3,7 +3,6 @@
 set -e
 
 # --- Environment Setup ---
-export DUCKDB_VERSION=v1.3.2
 export GRAAL_HOME=/opt/graalvm
 export JAVA_HOME=/opt/graalvm
 export PATH=/opt/graalvm/bin:$PATH
@@ -30,10 +29,10 @@ sudo apt-get update && sudo apt-get install -y --no-install-recommends \
     zlib1g-dev \
     file
 
-export DUCKDB_VERSION=v1.3.2
 echo "Building extension..."
 make configure
-make release -j8
+make circe-native
+make release
 
 echo "Moving extension binary..."
 mv build/release/extension/*/circe*_extension .
