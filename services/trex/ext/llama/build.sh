d7765a851d74e3bafdc579c4c78a0423bc249d4a
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2025-09-08 08:52:06 +0800

    fix

--- services/trex/ext/llama/build.sh
+++ services/trex/ext/llama/build.sh
@@ -3,6 +3,7 @@
 set -e
 export DUCKDB_VERSION=v1.3.2
 
+sudo apt-get update
 sudo apt-get install -y build-essential cmake python3 python3-pip pkg-config libssl-dev libcurl4-openssl-dev libvulkan1 libvulkan-dev vulkan-tools glslc libshaderc1 spirv-tools vulkan-validationlayers mesa-vulkan-drivers 
 
 export DUCKDB_VERSION=v1.3.2
