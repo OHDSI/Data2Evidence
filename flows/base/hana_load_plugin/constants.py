839e572450520f9d946f35c604d17bef0ab0dafc
yutingshe <yuting.she@data4life-asia.care>
2025-09-09 10:53:05 +0800

    Add hana_load_plugin

---
+++ flows/base/hana_load_plugin/constants.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+# Constants
+DATASET = "GiBleed"
+CDM_VERSION = "5.3"
+
+BASE_URL = f"https://github.com/OHDSI/EunomiaDatasets/raw/main/datasets/{DATASET}/{DATASET}_{CDM_VERSION}.zip"
+
+DATA_DIR = Path(f"/app/flows/hana_load_plugin/{DATASET}")
+ZIP_PATH = DATA_DIR / f"{DATASET}_{CDM_VERSION}.zip"
+EXTRACT_DIR = DATA_DIR / f"{DATASET}_{CDM_VERSION}"
+
+CREATE_SCRIPT_DIR = Path("/app/flows/hana_load_plugin/create_script")
+
+# Hana does not support foreign keys, so we skip hana_constraints.sql
+SQL_FILES_ORDER = [
+    "hana_ddl.sql",
+    "hana_primarykey.sql",
+    "hana_indices.sql"
+    # "hana_constraints.sql",
+]
