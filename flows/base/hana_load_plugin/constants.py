7292bb56de701b45bae00a72c9de3503ed74560f
yutingshe <yuting.she@data4life-asia.care>
2025-09-11 10:36:18 +0800

    Update path

--- flows/base/hana_load_plugin/constants.py
+++ flows/base/hana_load_plugin/constants.py
@@ -6,11 +6,11 @@ CDM_VERSION = "5.3"
 
 BASE_URL = f"https://github.com/OHDSI/EunomiaDatasets/raw/main/datasets/{DATASET}/{DATASET}_{CDM_VERSION}.zip"
 
-DATA_DIR = Path(f"/app/flows/hana_load_plugin/{DATASET}")
+DATA_DIR = Path(f"/app/base/hana_load_plugin/{DATASET}")
 ZIP_PATH = DATA_DIR / f"{DATASET}_{CDM_VERSION}.zip"
 EXTRACT_DIR = DATA_DIR / f"{DATASET}_{CDM_VERSION}"
 
-CREATE_SCRIPT_DIR = Path("/app/flows/hana_load_plugin/create_script")
+CREATE_SCRIPT_DIR = Path("/app/base/hana_load_plugin/create_script")
 
 # Hana does not support foreign keys, so we skip hana_constraints.sql
 SQL_FILES_ORDER = [
