23b0d8507112eb73aa944b400d3ddf67a739281f
alicia-koh <alicia.koh@data4life-asia.care>
2025-09-12 10:18:09 +0800

    Merge datamart and create cache flow

--- flows/base/create_cachedb_file_plugin/config.py
+++ flows/base/create_cachedb_file_plugin/config.py
@@ -1,6 +1,10 @@
 from enum import Enum
+
+from dataclasses import dataclass
+
 from typing_extensions import Self
-from typing import Optional, List, Dict
+from typing import Optional, List, Dict, Set
+
 from pydantic import BaseModel, Field, model_validator
 
 
@@ -41,6 +45,35 @@ DUCKDB_FULLTEXT_SEARCH_CONFIG = {
     },
 }
 
+@dataclass
+class QueryColumns:
+    """
+    Dataclass to hold columns used for constructing queries
+    """
+    table: str
+    columns_to_copy: Set[str]
+    patient_filter_col: str | None
+    timestamp_filter_col: str | None
+
+@dataclass  
+class CopyParameters:
+    """
+    Dataclass to hold parameters for creating cache
+    """
+
+    source_database: str
+    target_database: str
+    source_schema: str
+    target_schema: str
+
+    patient_filter: List[int] | None
+    table_filter: Dict[str, List[str]] | None
+    date_filter: str | None
+
+    fts_tables: List[str]
+
+    limit_statement: str
+
 
 class DatamartTableConfig(BaseModel):
     table_name: str = Field(alias="tableName")
@@ -52,31 +85,34 @@ class DatamartTableConfig(BaseModel):
 
 
 class DatamartConfig(BaseModel):
-    timestamp: Optional[str] = Field(default="")
+    timestamp: Optional[str] = Field(default=None)
+
     table_config: Optional[List[DatamartTableConfig]] = Field(
-        default_factory=list, alias="tableConfig"
+        default=None, alias="tableConfig"
     )
+
     patients_to_be_copied: Optional[List[int]] = Field(
-        default_factory=list, alias="patientsToBeCopied"
+        default=None, alias="patientsToBeCopied"
     )
+
+
+    def table_config_to_dict(self) -> Dict[str, List[str]] | None:
+        """
+        Converts the table_config list to a dictionary mapping table names to columns.
+        """
+        if not self.table_config:
+            return None
+        return {
+            cfg.table_name: cfg.columns_to_be_copied for cfg in self.table_config
+        }
 
     class Config:
         # Accept field name and aliases as input
         validate_by_name = True
 
-    def table_config_to_dict(self) -> Dict[str, List[str]]:
-        """
-        Converts the table_config list to a dictionary mapping table names to columns.
-        """
-        return {
-            cfg.table_name: cfg.columns_to_be_copied for cfg in self.table_config or []
-        }
-
-
 class CacheFlowAction(str, Enum):
-    CREATE_CACHE = "create_cache"  # Create a cachedb file
-    CREATE_DATAMART = "create_datamart"  # Create datamart cachedb file
-    GET_VERSION_INFO = "get_version_info"  # Get version info for datamart
+    CREATE_DATAMART_CACHE = "create_datamart_cache"
+    GET_VERSION_INFO = "get_version_info"
 
 
 class CreateCacheOptions(BaseModel):
@@ -102,28 +138,17 @@ class CreateCacheOptions(BaseModel):
 
     datasets: Optional[List[Dict]] = Field(default=None)
 
-    @property
-    def snapshot_copy_config_or_none(self) -> Optional[DatamartConfig]:
-        """
-        Returns snapshot_copy_config unless flow_action_type is CREATE_CACHE, in which case returns None.
-        """
-        if self.flow_action_type == CacheFlowAction.CREATE_CACHE:
-            return None
-        return self.snapshot_copy_config
 
     @property
-    def target_schema_name(self) -> Optional[str]:
+    def target_schema_name(self) -> str:
         """
-        Returns snapshot_schema_name, or if flow_action_type is CREATE_CACHE,
-        returns schema_name instead.
+        Returns snapshot_schema_name if provided, otherwise returns schema_name.
         """
-        if self.flow_action_type == CacheFlowAction.CREATE_CACHE:
-            return self.schema_name
-        return self.snapshot_schema_name
+        return self.snapshot_schema_name or self.schema_name
 
     @property
     def use_trex_connection(self) -> bool:
-        return False
+        return True
 
     @property
     def use_cache_db(self) -> bool:
@@ -133,16 +158,10 @@ class CreateCacheOptions(BaseModel):
     def check_required_fields(self) -> Self:
         # Mapping of required fields for each flow action type
         required_fields_map = {
-            CacheFlowAction.CREATE_CACHE: [
+            CacheFlowAction.CREATE_DATAMART_CACHE: [
                 "database_code",
                 "schema_name",
                 "tables_to_create_duckdb_fts_index",
-            ],
-            CacheFlowAction.CREATE_DATAMART: [
-                "database_code",
-                "schema_name",
-                "snapshot_schema_name",
-                # "snapshot_copy_config"
             ],
             CacheFlowAction.GET_VERSION_INFO: ["datasets"],
         }
@@ -158,31 +177,23 @@ class CreateCacheOptions(BaseModel):
             )
         return self
 
-    @model_validator(mode="after")
-    def validate_trex_connection_and_flow_action(self) -> Self:
-        # If use_trex_connection is False, flow_action_type must be CREATE_CACHE
-        if (
-            not self.use_trex_connection
-            and self.flow_action_type != CacheFlowAction.CREATE_CACHE
-        ):
-            raise ValueError(
-                "If use_trex_connection is False, flow_action_type must be 'create_cache'."
-            )
-        if (
-            self.flow_action_type == CacheFlowAction.CREATE_DATAMART
-            and not self.use_trex_connection
-        ):
-            raise ValueError(
-                "If flow_action_type is 'create_datamart', use_trex_connection must be True."
-            )
-        return self
-
     class Config:
         # Make model store raw values instead of enum object
         use_enum_values = True
         # Accept field name and aliases as input
         validate_by_name = True
 
+
+    @model_validator(mode="after")
+    def validate_trex_connection_and_flow_action(self) -> Self:
+        if (
+            self.flow_action_type == CacheFlowAction.GET_VERSION_INFO
+            and not self.use_trex_connection
+        ):
+            raise ValueError(
+                f"If flow_action_type is '{CacheFlowAction.GET_VERSION_INFO}', use_trex_connection must be True."
+            )
+        return self
 
 class CreateCDWValidationConfig(BaseModel):
     databaseCode: str
