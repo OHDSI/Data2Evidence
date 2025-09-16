a9e2e880ade2e2c350b13b32feee63b7e99063b1
Khairul Syazwan <80243918+khairul-syazwan@users.noreply.github.com>
2025-09-16 09:26:03 +0800

    Move load bookmarks call to webApi (#857)
    * Move load all bookmarks call to webapi
    
    * Update mock server route for bookmark
    
    * /cohortdefinition to return combined array
    
    * Removed unused methods in bookmark service
    
    * Update frontend to accept new shape of bookmarks
    
    * Update webapi endpoint
    
    * Update types and schema for optional fields
    
    * Adjust formatting of raw cohort def data
    
    * Update mock server cohort def data
    
    * Fix minor errors
    
    * Remove logs

--- ui/apps/vue-mri-ui-lib/src/query-filter/mock-server/webapi-routes.js
+++ ui/apps/vue-mri-ui-lib/src/query-filter/mock-server/webapi-routes.js
@@ -142,7 +142,7 @@ const setupWebapiRoutes = app => {
     return res.send()
   })
 
-  app.get('/analytics-svc/api/services/bookmark', async (req, res) => {
+  app.get('/d2e-webapi/cohortdefinition', async (req, res) => {
     const cacheKey = CACHE_KEYS.COHORT_DEFINITIONS
     logRequest(req)
     let data = cache[cacheKey]
@@ -155,29 +155,21 @@ const setupWebapiRoutes = app => {
     for (let i = 0; i < MAX_COHORT_DEFINITIONS; i += 1) {
       const d = data[i]
       mappedData[i] = {
-        id: d.id,
-        name: d.name,
-        username: 'current_user',
-        createdOn: new Date(d.createdDate).toISOString(),
-        updatedOn: new Date(d.createdDate).toISOString(),
+        createdBy: 'current_user',
+        modifiedDate: d.createdDate,
+        ...d,
       }
     }
-    return res.send({
-      atlasCohortDefinitions: [
-        {
-          id: 1,
-          name: 'Atlas Cohort',
-          username: 'current_user',
-          createdOn: '2025-06-19T21:08:09.028Z',
-          updatedOn: '2025-06-19T21:08:09.028Z',
-        },
-        ...mappedData,
-      ],
-      bookmarks: [],
-      // we will not need this for pa-atlas. Info on the generation runs can be found at
-      // https://atlas-demo.ohdsi.org/WebAPI/cohortdefinition/1794229/info
-      materializedCohorts: [],
-    })
+    return res.send([
+      {
+        id: 1,
+        name: 'Atlas Cohort',
+        createdBy: 'current_user',
+        createdDate: 1750334889028,
+        modifiedDate: 1750334889028,
+      },
+      ...mappedData,
+    ])
   })
 
   app.get(
