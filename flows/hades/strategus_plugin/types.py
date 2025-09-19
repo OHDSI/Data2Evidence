331218d01ff61483f1db8dd3f38794216053f795
alicia-koh <110385419+alicia-koh@users.noreply.github.com>
2025-09-19 10:23:18 +0800

    Make strategus plugin work with trex (#856)
    * Make strategus plugin work with trex
    
    * Upgrade Cyclops
    
    * Upgrade Cyclops for Kernel
    
    * Update Dockerfile
    
    * Use set_trex_env_var from rutils
    
    ---------
    
    Signed-off-by: alicia-koh <110385419+alicia-koh@users.noreply.github.com>

--- flows/hades/strategus_plugin/types.py
+++ flows/hades/strategus_plugin/types.py
@@ -1,9 +1,12 @@
-
 from enum import Enum
 
+
 class CohortNodeType(Enum):
-	EVENT = "event"
-	TARGET = "target"
-	EXIT = "exit"
-	OUTCOME = "outcome"
-	COMPARATOR = "comparator"
+    EVENT = "event"
+    TARGET = "target"
+    EXIT = "exit"
+    OUTCOME = "outcome"
+    COMPARATOR = "comparator"
+
+
+USE_TREX_CONNECTION = True
