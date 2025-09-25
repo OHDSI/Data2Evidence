8ce58d7e2066cf0d5b090311af044b217e9bb70f
Afreen Sikandara <afreen.sikandara@data4life-asia.care>
2025-09-25 10:02:48 +0800

    Update env variable with plugin seed and remove from cli

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -222,8 +222,6 @@ case $cmd in
         source "$ENVFILE"
         database_host=${PROJECT_NAME:-d2e}-demodb
         docker exec $database_host psql -h localhost -U postgres -c "SET search_path TO demo_cdm; CREATE TABLE IF NOT EXISTS cohort (cohort_definition_id integer NOT NULL,subject_id integer NOT NULL,cohort_start_date DATE NOT NULL,cohort_end_date DATE NOT NULL)"
-        export PLUGINS_SEED="$(echo $PLUGINS_SEED | sed 's/[]]$/, \"i2b2-flow\"]/')"
-        $node_modules_path/scripts/cli.sh start -n "$ENVFILE" -s trex
         ;;
     init)
         CADDY__ALP__PUBLIC_FQDN=${CADDY__ALP__PUBLIC_FQDN:-localhost}
