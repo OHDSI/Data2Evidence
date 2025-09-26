6e535cf65446d77b5ba425c9a2ce7ec78fff1e05
amit-sharma-d4l <amit.sharma@data4life-asia.care>
2025-09-26 14:21:43 +0800

    Updating code to add boolean flag for test skip.

--- tests/e2e/tests/12-concepts/concepts-page.ts
+++ tests/e2e/tests/12-concepts/concepts-page.ts
@@ -1,19 +1,21 @@
 import { test, expect } from '@playwright/test'
 
-test('Concepts page', async ({ page }) => {
-  await page.goto('https://localhost:443')
+const TEST_NAME = 'Concepts page'
+const SHOULD_SKIP = false
+test.fixme(SHOULD_SKIP, `${TEST_NAME} test is temporarily disabled.`)
+
+test(TEST_NAME, async ({ page }) => {
+  await page.goto('/portal')
   await page.locator('input[name="identifier"]').click()
   await page.locator('input[name="identifier"]').fill('admin')
   await page.locator('input[name="password"]').click()
   await page.locator('input[name="password"]').fill('Updatepassword12345')
   await page.getByRole('button', { name: 'Sign in' }).click()
-  await page.getByTestId('button').nth(1).click()
-  await page.getByRole('button', { name: 'Dataset' }).click()
-  await page.getByRole('option', { name: 'Demo Dataset' }).click()
+  await page.getByText('Demo dataset').first().click()
   await page.getByRole('link', { name: 'Concepts' }).click()
   await expect(page.getByRole('main')).toMatchAriaSnapshot(`
     - tablist:
       - tab "Concept Search" [selected]
       - tab "Concept Sets"
     `)
-});
+})
