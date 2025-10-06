95c2ea4ac5c494e2c6400d26e3420d26a5800ff8
Zhimin-arya <zhi.min@data4life-asia.care>
2025-10-06 13:28:49 +0800

    add the directly connect duckdb file mode

--- flows/search_embedding/search_embedding_plugin/flow.py
+++ flows/search_embedding/search_embedding_plugin/flow.py
@@ -1,30 +1,47 @@
 import pandas as pd
 import os
 from transformers import AutoTokenizer, AutoModel
+import duckdb
 
 from prefect import flow, task
 from prefect.logging import get_run_logger
+from prefect.variables import Variable
 
 from .types import *
 from .utils import *
 from _shared_flow_utils.dao.DBDao import DBDao, SupportedDatabaseDialects
+import time
 
 os.environ['plugin_name'] = 'search_embedding_plugin'
 
 @flow(log_prints=True)
 def search_embedding_plugin(options: SearchEmbeddingType):
+    # time.sleep(600)  # Ensure all services are up
     schema_name = options.schema_name
-    dbdao = DBDao(
-        dialect=SupportedDatabaseDialects.TREX if options.use_trex_connection else None,
-        use_cache_db=options.use_cache_db,
-        database_code=options.database_code,
-    )
-    ## Vss extension is installed by default in trex, just need to load it
-    dbdao.execute_sql(f"LOAD vss")
-    create_embeddings(dbdao, schema_name)
+    database_code = options.database_code
+    use_trex_connection = options.use_trex_connection
+    if use_trex_connection:
+        # -------------------- Trex connection to cache --------------------
+        dbdao = DBDao(
+            dialect=SupportedDatabaseDialects.TREX,
+            use_cache_db=use_trex_connection,
+            database_code=database_code,
+        )
+        ## Vss extension is installed by default in trex, just need to load it
+        dbdao.execute_sql("LOAD vss")
+        create_embeddings_trex(dbdao, schema_name)
+    else:
+        # -------------------- Direct file connection to cache --------------------
+        duckdb_file_path = resolve_duckdb_file_path(database_code, Variable.get("duckdb_data_folder"))
+        vss_extension_path = f'{DUCKDB_EXTENSIONS_FILEPATH}/vss.duckdb_extension';
+        conn = duckdb.connect(duckdb_file_path, read_only=True)
+        conn.close()
+        with duckdb.connect(duckdb_file_path, read_only=True) as conn:
+            conn.load_extension(vss_extension_path)
+            create_embeddings_duckdb(conn, schema_name)
 
 @task(log_prints=True, task_run_name="embedding_concept_table_{schema_name}.concept")
-def create_embeddings(dbdao, schema_name):
+def create_embeddings_trex(dbdao, schema_name):
     logger = get_run_logger()
     logger.info("***************** Start embedding *****************")
     
@@ -47,10 +64,8 @@ def create_embeddings(dbdao, schema_name):
         embeddings = embedding_concept_table(concept_name, tokenizer, model).tolist()
         columns = list(gte_tmp_cols.keys())
         col_values = list(zip(concept_id, embeddings))    
-
         ## Insert embedding into tmp gte table
         dbdao.batch_insert_values(schema_name, gte_tmp_table, columns, col_values)
-        
         ## Monitoring embedding progress
         percent = (i/step + 1)/(int(length / step) + (length % step > 0)) * 100
         logger.info(f'{round(percent,2)} % completed')
@@ -68,5 +83,55 @@ def create_embeddings(dbdao, schema_name):
     
     ## Create vss index on embedding column
     create_gte_index(dbdao, schema_name, embedding_col)
+    
+@task(log_prints=True, task_run_name="embedding_concept_table_duckdb")
+def create_embeddings_duckdb(conn, schema_name):
+    logger = get_run_logger()
+    logger.info("***************** Start embedding *****************")
+    concept = conn.execute('SELECT concept_id, concept_name FROM "?".concept', [schema_name]).fetchnumpy()
+    length = len(concept['concept_name'])
+    step = 100
+    
+    ## Create temporary table for embeddings
+    conn.execute("DROP TABLE IF EXISTS ?.gte_embeddings", [schema_name])
+    conn.execute("CREATE TABLE ?.gte_embeddings (concept_id int, vec FLOAT[384]);", [schema_name])
+    
+    ## Generate embedding
+    tokenizer = AutoTokenizer.from_pretrained("Supabase/gte-small")
+    model = AutoModel.from_pretrained("Supabase/gte-small")
+    for i in range(0, length, step):
+        concept_name = concept['concept_name'][i:i+step].tolist()
+        concept_id = concept['concept_id'][i:i+step]
+        embeddings = embedding_concept_table(concept_name, tokenizer, model).tolist()
+        rst = pd.DataFrame({'concept_id':concept_id, 'gte-small_384': embeddings})
+        ## Insert embedding into tmp gte table
+        conn.execute("INSERT INTO ?.gte_embeddings SELECT concept_id, gte-small_384 FROM rst", [schema_name])
+        percent = (i/step + 1)/(int(length / step) + (length % step > 0)) * 100
+        logger.info(f'{round(percent,2)} % completed')
 
-
+    logger.info("***************** Insert embedding *****************")
+    ## Check if column exists using parameterized query
+    embedding_col_name = 'concept_name_embedding'
+    column_exists = check_duckdb_column_exists(conn, f"{schema_name}.concept", embedding_col_name)
+    if not column_exists:
+        conn.execute(f"ALTER TABLE ?.concept ADD COLUMN {embedding_col_name} FLOAT[384];", [schema_name])
+        
+    ## Drop index if exists
+    conn.execute(f"DROP INDEX IF EXISTS ?.gte_cos_idx;", [schema_name])
+    
+    ## Update embedding column safely
+    conn.execute(f"""
+        UPDATE ?.concept AS c
+        SET {embedding_col_name} = g.vec
+        FROM ?.gte_embeddings AS g
+        WHERE c.concept_id = g.concept_id;
+        """, 
+        [schema_name, schema_name]
+    )
+    
+    # Clean up temporary table
+    conn.execute("DROP TABLE ?.gte_embeddings;", [schema_name])
+    
+    # Create index
+    conn.execute("SET hnsw_enable_experimental_persistence=TRUE;")
+    conn.execute(f"CREATE INDEX gte_cos_idx ON ?.concept USING HNSW ({embedding_col_name}) WITH (metric = 'cosine')",[schema_name])
--- flows/search_embedding/search_embedding_plugin/utils.py
+++ flows/search_embedding/search_embedding_plugin/utils.py
@@ -2,15 +2,22 @@ import torch.nn.functional as F
 from torch import Tensor
 from prefect.logging import get_run_logger
 from psycopg2 import sql as pg_sql
+from pathlib import Path
 
 DUCKDB_EXTENSIONS_FILEPATH = "/app/duckdb_extensions"
 
 def average_pool(last_hidden_states: Tensor,
                  attention_mask: Tensor) -> Tensor:
+    """
+    Performs average pooling on the token embeddings.
+    """
     last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
     return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
 
 def embedding_concept_table(concept_name_list,tokenizer, model):
+    """
+    Generate embeddings for a list of concept names using the provided tokenizer and model.
+    """
     # Tokenize the input texts
     batch_dict = tokenizer(concept_name_list, max_length=512, padding=True, truncation=True, return_tensors='pt')
     outputs = model(**batch_dict)
@@ -19,7 +26,19 @@ def embedding_concept_table(concept_name_list,tokenizer, model):
     embeddings = F.normalize(embeddings, p=2, dim=1)
     return embeddings
 
+def check_duckdb_column_exists(conn, table_name, column):
+    """
+    Check if a column exists in a DuckDB table.
+    """
+    rst = conn.execute('PRAGMA table_info(?);', [table_name]).fetchall()
+    columns = [row[1] for row in rst]
+    return any(col.lower() == column.lower() for col in columns)
+
 def create_tmp_gte_table(trexdao, schema_name, gte_tmp_table, gte_tmp_cols):
+    """
+    Create a temporary table to store intermediate embeddings.
+    If the table already exists, it will be truncated.
+    """
     logger = get_run_logger()
     if trexdao.check_table_exists(schema_name, gte_tmp_table):
         if set(gte_tmp_cols.keys()) == set(trexdao.get_columns(schema_name, gte_tmp_table)):
@@ -33,6 +52,9 @@ def create_tmp_gte_table(trexdao, schema_name, gte_tmp_table, gte_tmp_cols):
         trexdao.create_table(schema_name, gte_tmp_table, gte_tmp_cols)
 
 def add_embedding_column(dbdao, schema_name, embedding_col):
+    """ 
+    Add a new column to the concept table to store embeddings.
+    """
     sql = pg_sql.SQL("ALTER TABLE {schema_name}.concept ADD COLUMN {column_name} FLOAT[384];").format(
     schema_name=pg_sql.Identifier(schema_name),
     column_name=pg_sql.Identifier(embedding_col)
@@ -40,12 +62,18 @@ def add_embedding_column(dbdao, schema_name, embedding_col):
     dbdao.execute_sql(sql)
     
 def drop_gte_index(dbdao, schema_name):
+    """ 
+    Drop the existing GTE index on the concept table if it exists.
+    """
     sql = pg_sql.SQL("DROP INDEX IF EXISTS {schema_name}.gte_cos_idx;").format(
         schema_name=pg_sql.Identifier(schema_name)
         )
     dbdao.execute_sql(sql)
     
 def update_concept_embedding(dbdao, schema_name, gte_tmp_table, embedding_col):
+    """ 
+    Update the concept table with embeddings from the temporary GTE table.
+    """ 
     sql = pg_sql.SQL("""
                     UPDATE {schema_name}.concept AS c
                     SET {embedding_col} = g.vec
@@ -59,10 +87,19 @@ def update_concept_embedding(dbdao, schema_name, gte_tmp_table, embedding_col):
     dbdao.execute_sql(sql)
     
 def create_gte_index(dbdao, schema_name, embedding_col):
+    """ 
+    Create a GTE index on the embedding column of the concept table.
+    """ 
     dbdao.execute_sql("SET hnsw_enable_experimental_persistence=TRUE;")
     sql = pg_sql.SQL("CREATE INDEX gte_cos_idx ON {schema_name}.concept USING HNSW ({embedding_col}) WITH (metric = 'cosine')").format(
         schema_name=pg_sql.Identifier(schema_name),
         embedding_col=pg_sql.Identifier(embedding_col)
         )   
     dbdao.execute_sql(sql)
+
+def resolve_duckdb_file_path(duckdb_database_name: str, folder_path: str) -> str:
+    """
+    Returns the full path to the DuckDB database file
+    """
+    return str(Path(folder_path) / f"{duckdb_database_name}.db")
 
