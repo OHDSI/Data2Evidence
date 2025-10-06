89e8b31d01f886548546bd34a1b9716b785a7ef5
santan maddi <saisantan2004@gmail.com>
2025-10-06 13:41:53 +0800

    create docker network

--- flows/hades/strategus_plugin/run_hades_test.sh
+++ flows/hades/strategus_plugin/run_hades_test.sh
@@ -7,6 +7,7 @@ docker compose -f flows/hades/strategus_plugin/dc-local.yml up -d
 # Name of the service as defined in dc-local.yml
 SERVICE_NAME="app"
 
+docker network create alp_alp
 # Run the test inside the container
 docker compose -f flows/hades/strategus_plugin/dc-local.yml exec $SERVICE_NAME \
   /app/.venv/bin/python -m pytest /app/flows/hades/strategus_plugin/tests/
