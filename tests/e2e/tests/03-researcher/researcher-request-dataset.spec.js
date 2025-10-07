1b6f45a1035ca3e75fee5ceef6aa868d5d7a79d8
Afreen Sikandara <afreen.sikandara@data4life-asia.care>
2025-10-07 11:38:14 +0800

    Cleanup after

--- tests/e2e/tests/03-researcher/researcher-request-dataset.spec.js
+++ tests/e2e/tests/03-researcher/researcher-request-dataset.spec.js
@@ -6,7 +6,7 @@ test.fixme(SHOULD_SKIP, `${TEST_NAME} test is temporarily disabled.`)
 
 test(TEST_NAME, async ({ page }) => {
   // Go to portal and log in as admin
-  await page.goto('https://localhost:443/portal');
+  await page.goto('/portal');
   await page.locator('input[name="identifier"]').click();
   await page.locator('input[name="identifier"]').fill('admin');
   await page.locator('input[name="password"]').click();
@@ -79,4 +79,18 @@ test(TEST_NAME, async ({ page }) => {
   await expect(page.getByTestId('header')).toBeVisible();
   await expect(page.getByTestId('card').locator('div').filter({ hasText: 'Dataset InfoData QualityData' }).first()).toBeVisible();
   await expect(page.getByTestId('nav')).toContainText('Demo datasetDatasetConceptsCohortsNotebooksAnalysisAccount');
+
+  //Clean up - delete the researcher user created
+  //Login as admin again
+  await page.getByRole('link', { name: 'Account' }).click()
+  await page.getByRole('button', { name: 'Logout' }).click()
+  await page.locator('input[name="identifier"]').click()
+  await page.locator('input[name="identifier"]').fill('admin')
+  await page.locator('input[name="password"]').click()
+  await page.locator('input[name="password"]').fill('Updatepassword12345')
+  await page.getByRole('button', { name: 'Sign in' }).click()
+  await page.getByTestId('button').nth(1).click()
+  await page.getByRole('button', { name: 'Switch to Admin portal' }).click()
+  await page.getByTestId('table-row').filter({ hasText: 'test_researcher' }).getByRole('button', { name: 'Delete' }).click()
+  await page.getByRole('button', { name: 'Yes, delete' }).click()
 });
