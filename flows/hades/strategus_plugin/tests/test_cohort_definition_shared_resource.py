91ec32b44282b3c12d876a4b57854a0b57149a55
santan maddi <saisantan2004@gmail.com>
2025-10-09 14:15:41 +0800

    add tests for outcomes nodes

--- flows/hades/strategus_plugin/tests/test_cohort_definition_shared_resource.py
+++ flows/hades/strategus_plugin/tests/test_cohort_definition_shared_resource.py
@@ -8,7 +8,7 @@ def test_cohort_definition_shared_resource_task_returns_expected_classes(mock_ge
     # Mock the WebAPI instance and its methods
     mock_webapi_instance = MagicMock()
     mock_webapi_instance.get_cohort_definition.return_value = {
-        "expression": {},
+        "expression": {"ConceptSets":[{"id":0,"name":"Celecoxib","expression":{"items":[{"concept":{"CONCEPT_CLASS_ID":"Ingredient","CONCEPT_CODE":"140587","CONCEPT_ID":1118084,"CONCEPT_NAME":"celecoxib","DOMAIN_ID":"Drug","INVALID_REASON":"V","INVALID_REASON_CAPTION":"Valid","STANDARD_CONCEPT":"S","STANDARD_CONCEPT_CAPTION":"Standard","VOCABULARY_ID":"RxNorm"}}]}}],"PrimaryCriteria":{"CriteriaList":[{"DrugEra":{"CodesetId":0}}],"ObservationWindow":{"PriorDays":0,"PostDays":0},"PrimaryCriteriaLimit":{"Type":"All"}},"QualifiedLimit":{"Type":"First"},"ExpressionLimit":{"Type":"All"},"InclusionRules":[],"EndStrategy":{"CustomEra":{"DrugCodesetId":0,"GapDays":30,"Offset":0}},"CensoringCriteria":[],"CollapseSettings":{"CollapseType":"ERA","EraPad":0},"CensorWindow":{},"cdmVersionRange":">=5.0.0"},
         "name": "Test Cohort"
     }
     MockWebAPI.return_value = mock_webapi_instance
@@ -30,15 +30,25 @@ def test_cohort_definition_shared_resource_task_returns_expected_classes(mock_ge
     assert obj.cohortType.value == "target"
 
     task_run_context = {"id": "run1", "name": "test", "flow_run_id": "flow1"}
-    # The .task() method does not require input, just context
-    # result = obj.task(task_run_context)
-    # assert isinstance(result, Result)
-    # assert result.error is False
+    result = obj.task(task_run_context)
+    assert isinstance(result, Result)
+    print("*******Error Message*******:")
+    print(result.data)
+    assert result.error is False
 
-    # # The result data is a dict with keys 'cohortDefinitionSharedResource' and 'cohortGeneratorModuleSpecifications'
-    # assert isinstance(result.data, dict)
-    # assert "cohortDefinitionSharedResource" in result.data
-    # assert "cohortGeneratorModuleSpecifications" in result.data
-    # # Check R class for the module specifications
-    # r_classes = list(result.data["cohortGeneratorModuleSpecifications"].rclass) if hasattr(result.data["cohortGeneratorModuleSpecifications"], 'rclass') else []
-    # assert "CohortGeneratorModuleSpecifications" in r_classes
+    # The result data is a dict with keys 'cohortDefinitionSharedResource' and 'cohortGeneratorModuleSpecifications'
+    # Assert the R object class
+    assert isinstance(result.data, dict)
+    assert "cohortDefinitionSharedResource" in result.data
+    assert "cohortGeneratorModuleSpecifications" in result.data
+    # Check R class for the module specifications
+    r_classes = list(result.data["cohortGeneratorModuleSpecifications"].rclass) if hasattr(result.data["cohortGeneratorModuleSpecifications"], 'rclass') else []
+    assert "CohortGeneratorModuleSpecifications" in r_classes
+
+    # # assert result.data is a pandas data frame with columns cohortId, cohortName, sql and json
+    # df = convert_R_to_py(result.data)
+    # assert hasattr(df, 'columns')
+    # assert "cohortId" in df.columns
+    # assert "cohortName" in df.columns
+    # assert "sql" in df.columns
+    # assert "json" in df.columns
