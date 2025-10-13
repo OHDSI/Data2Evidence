dd2e0458b9ab059054af1469785c5d89bb82fe34
Zhimin-arya <zhi.min@data4life-asia.care>
2025-10-13 14:08:25 +0800

    use global variables for both connection

--- flows/search_embedding/search_embedding_plugin/flow.py
+++ flows/search_embedding/search_embedding_plugin/flow.py
@@ -10,13 +10,19 @@ from prefect.variables import Variable
 from .types import *
 from .utils import *
 from _shared_flow_utils.dao.DBDao import DBDao, SupportedDatabaseDialects
-import time
 
 os.environ['plugin_name'] = 'search_embedding_plugin'
 
+# Define global variables for shared configurations
+STEP = 100
+tokenizer = AutoTokenizer.from_pretrained("Supabase/gte-small")
+model = AutoModel.from_pretrained("Supabase/gte-small")
+tmp_embedding_table = 'tmp_embeddings'
+embedding_col_name = 'concept_name_embedding'
+index_col = 'embedding_cos_idx'
+
 @flow(log_prints=True)
 def search_embedding_plugin(options: SearchEmbeddingType):
-    # time.sleep(600)  # Ensure all services are up
     schema_name = options.schema_name
     database_code = options.database_code
     use_trex_connection = options.use_trex_connection
@@ -46,42 +52,37 @@ def create_embeddings_trex(dbdao, schema_name):
     ## Load concept table
     concept = dbdao.execute_sql(f'SELECT concept_id, concept_name FROM {schema_name}.concept', fetch=True)
     concept = pd.DataFrame(concept, columns=['concept_id','concept_name'])
-    step, length =100, len(concept)
+    length = len(concept)
     
     ## Create tmp embedding table
-    tmp_embedding_table = 'tmp_embeddings'
     embedding_cols = {'concept_id':'int', 'vec':'FLOAT[384]'}
     create_tmp_embedding_table(dbdao, schema_name, tmp_embedding_table, embedding_cols)
 
     ## Generate embedding
-    tokenizer = AutoTokenizer.from_pretrained("Supabase/gte-small")
-    model = AutoModel.from_pretrained("Supabase/gte-small")
-    for i in range(0, length, step):
-        concept_name = concept['concept_name'][i:i+step].tolist()
-        concept_id = concept['concept_id'][i:i+step].tolist()
+    for i in range(0, length, STEP):
+        concept_name = concept['concept_name'][i:i+STEP].tolist()
+        concept_id = concept['concept_id'][i:i+STEP].tolist()
         embeddings = embedding_concept_table(concept_name, tokenizer, model).tolist()
         columns = list(embedding_cols.keys())
         col_values = list(zip(concept_id, embeddings))    
         ## Insert embedding into tmp embedding table
         dbdao.batch_insert_values(schema_name, tmp_embedding_table, columns, col_values)
         ## Monitoring embedding progress
-        percent = (i/step + 1)/(int(length / step) + (length % step > 0)) * 100
+        percent = (i/STEP + 1)/(int(length / STEP) + (length % STEP > 0)) * 100
         logger.info(f'{round(percent,2)} % completed')
     
     logger.info("***************** Insert embedding *****************")
     ## Add embedding column to concept table
-    embedding_col = 'concept_name_embedding'
-    if embedding_col not in dbdao.get_columns(schema_name, 'concept'):
-        add_embedding_column(dbdao, schema_name, embedding_col)
+    if embedding_col_name not in dbdao.get_columns(schema_name, 'concept'):
+        add_embedding_column(dbdao, schema_name, embedding_col_name)
 
     ## Copy embedding column from intermediate table to concept table, must drop vss index (if exist) before update embedding column
-    index_col = 'embedding_cos_idx'
     drop_embedding_index(dbdao, schema_name, index_col)
-    update_concept_embedding(dbdao, schema_name, tmp_embedding_table, embedding_col)
+    update_concept_embedding(dbdao, schema_name, tmp_embedding_table, embedding_col_name)
     dbdao.drop_table(schema_name, tmp_embedding_table, cascade=True)
     
     ## Create vss index on embedding column
-    create_embedding_index(dbdao, schema_name, embedding_col, index_col)
+    create_embedding_index(dbdao, schema_name, embedding_col_name, index_col)
     
 @task(log_prints=True, task_run_name="embedding_concept_table_duckdb")
 def create_embeddings_duckdb(conn, schema_name):
@@ -89,30 +90,24 @@ def create_embeddings_duckdb(conn, schema_name):
     logger.info("***************** Start embedding *****************")
     concept = conn.execute(f'SELECT concept_id, concept_name FROM "{schema_name}".concept').fetchnumpy()
     length = len(concept['concept_name'])
-    step = 100
     
     ## Create temporary table for embeddings
-    tmp_embedding_table = 'tmp_embeddings'
     conn.execute(f'DROP TABLE IF EXISTS "{schema_name}"."{tmp_embedding_table}"')
     conn.execute(f'CREATE TABLE "{schema_name}"."{tmp_embedding_table}" (concept_id int, vec FLOAT[384]);')
     
     ## Generate embedding
-    tokenizer = AutoTokenizer.from_pretrained("Supabase/gte-small")
-    model = AutoModel.from_pretrained("Supabase/gte-small")
-    for i in range(0, length, step):
-        concept_name = concept['concept_name'][i:i+step].tolist()
-        concept_id = concept['concept_id'][i:i+step]
+    for i in range(0, length, STEP):
+        concept_name = concept['concept_name'][i:i+STEP].tolist()
+        concept_id = concept['concept_id'][i:i+STEP]
         embeddings = embedding_concept_table(concept_name, tokenizer, model).tolist()
         rst = pd.DataFrame({'concept_id':concept_id, 'embedding': embeddings})
         ## Insert embedding into tmp embedding table
         conn.execute(f"INSERT INTO {schema_name}.{tmp_embedding_table} SELECT concept_id, embedding FROM rst")
-        percent = (i/step + 1)/(int(length / step) + (length % step > 0)) * 100
+        percent = (i/STEP + 1)/(int(length / STEP) + (length % STEP > 0)) * 100
         logger.info(f'{round(percent,2)} % completed')
 
     logger.info("***************** Insert embedding *****************")
     ## Check if column exists using parameterized query
-    embedding_col_name = 'concept_name_embedding'
-    index_col = 'embedding_cos_idx'
     column_exists = check_duckdb_column_exists(conn, f"{schema_name}.concept", embedding_col_name)
     if not column_exists:
         conn.execute(f"ALTER TABLE \"{schema_name}\".concept ADD COLUMN {embedding_col_name} FLOAT[384];")
