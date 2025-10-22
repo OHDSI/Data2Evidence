87789e4ff628a8f44cac444003e663c7e695848c
Nicholas Ang <nicholas.ang@data4life-asia.care>
2025-10-22 14:58:16 +0800

    add monaco editor

--- ui/apps/portal/src/plugins/SystemAdmin/StudyOverview/ManageDashboardDialog/ManageDashboardDialog.tsx
+++ ui/apps/portal/src/plugins/SystemAdmin/StudyOverview/ManageDashboardDialog/ManageDashboardDialog.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useCallback, useState } from "react";
 import Divider from "@mui/material/Divider";
 import { Button, Dialog } from "@portal/components";
+import * as monaco from "monaco-editor";
+import { loader, Editor } from "@monaco-editor/react";
 import { Study, CloseDialogType } from "../../../../types";
 import "./ManageDashboardDialog.scss";
 
@@ -10,7 +12,13 @@ interface ManageDashboardDialogProps {
   onClose?: (type: CloseDialogType) => void;
 }
 
+const SafeEditor = Editor as any;
+
 const ManageDashboardDialog: FC<ManageDashboardDialogProps> = ({ study, open, onClose }) => {
+  loader.config({ monaco });
+
+  const [dashboardCode, setDashboardCode] = useState<string>("hello123");
+
   const handleClose = useCallback(
     (type: CloseDialogType) => {
       typeof onClose === "function" && onClose(type);
@@ -23,10 +31,26 @@ const ManageDashboardDialog: FC<ManageDashboardDialogProps> = ({ study, open, on
       className="manage-dashboard-dialog"
       title={"Manage Dashboard"}
       closable
+      fullWidth
+      maxWidth="lg"
       open={open}
       onClose={() => handleClose("cancelled")}
     >
       <Divider />
+      <div className="manage-dashboard-dialog__content">
+        <SafeEditor
+          height="70vh"
+          defaultLanguage="r"
+          defaultValue={dashboardCode}
+          options={{
+            scrollBeyondLastLine: false,
+            fontSize: "14px",
+          }}
+          onChange={setDashboardCode}
+        />
+      </div>
+      <Divider />
+
       <div className="button-group-actions">
         <Button text="Cancel" onClick={() => handleClose("cancelled")} variant="outlined" block />
         <Button text="Save" block />
