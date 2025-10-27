9c848941427df5d5121cd57f8c8c68749701be3b
Nicholas Ang <nicholas.ang@data4life-asia.care>
2025-10-27 17:38:06 +0800

    add useKernelViewer hook

---
+++ ui/apps/portal/src/hooks/useKernelViewer.ts
@@ -0,0 +1,61 @@
+import { useCallback, useEffect, useState } from "react";
+import { api } from "../axios/api";
+
+export type ViewerStatus = "starting" | "up" | "stopping" | "down" | "failed" | "idle";
+
+type UseKernelViewerResponse = [
+  viewerStatus: ViewerStatus,
+  startViewer: (viewerCode: string) => Promise<void>,
+  stopViewer: () => Promise<void>
+];
+
+export const useKernelViewer = (id: string, selectedDatasetId: string = ""): UseKernelViewerResponse => {
+  const [viewerStatus, setViewerStatus] = useState<ViewerStatus>("idle");
+
+  const fetchViewerStatus = useCallback(async () => {
+    try {
+      const response = await api.strategusResults.getStrategusResultViewerStatus(id);
+      if (response.running) {
+        setViewerStatus("up");
+      } else {
+        setViewerStatus("down");
+      }
+    } catch (error) {
+      console.error("Error fetching viewer status:", error);
+      setViewerStatus("failed");
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchViewerStatus();
+  }, [fetchViewerStatus]);
+
+  const startViewer = useCallback(
+    async (viewerCode: string) => {
+      try {
+        setViewerStatus("starting");
+        await api.strategusResults.startStrategusResultViewer(id, selectedDatasetId, viewerCode || "");
+        setViewerStatus("up");
+      } catch (error: any) {
+        console.error("Error starting viewer:", error);
+        setViewerStatus("failed");
+        throw error;
+      }
+    },
+    [selectedDatasetId]
+  );
+
+  const stopViewer = useCallback(async () => {
+    try {
+      setViewerStatus("stopping");
+      await api.strategusResults.stopStrategusResultViewer(id);
+      setViewerStatus("down");
+    } catch (error: any) {
+      console.error(error);
+      setViewerStatus("failed");
+      throw error;
+    }
+  }, [selectedDatasetId, id]);
+
+  return [viewerStatus, startViewer, stopViewer];
+};
