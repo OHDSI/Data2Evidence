59b3cd45583c3b8add6a1bd08246edc058766e5a
alicia-koh <alicia.koh@data4life-asia.care>
2025-10-28 17:41:34 +0800

    Update types.py

--- flows/data_transformation/white_rabbit_plugin/types.py
+++ flows/data_transformation/white_rabbit_plugin/types.py
@@ -1,19 +1,15 @@
-from pydantic import BaseModel, Field, validator
-from typing import Optional
 from enum import Enum
+from typing import Optional
+from pydantic import BaseModel, Field
 
 
-class ServiceCredentials(BaseModel):
-    PG__DB_NAME: str = Field(..., strict=True)
-    PG__PORT: str = Field(..., strict=True)
-    PG__HOST: str = Field(..., strict=True)
-    PERSEUS__FILES_MANAGER_HOST: str = Field(..., strict=True)
-    PG_ADMIN_USER: str = Field(..., strict=True)
-    PG_ADMIN_PASSWORD: str = Field(..., strict=True)
+WHITERABBIT_DIR_PATH = '/app/whiterabbit'
+WHITERABBIT_BIN_PATH = f'{WHITERABBIT_DIR_PATH}/dist/bin'
+WHITERABBIT_CSV_DIR = f'{WHITERABBIT_DIR_PATH}/csvfiles'
 
 
 class WhiteRabbitRunType(str, Enum):
-    SCAN_REPORT_FILES = 'SCAN_REPORT_FILES'  # can be by files or db
+    SCAN_REPORT_FILES = 'SCAN_REPORT_FILES'
     SCAN_REPORT_DB = 'SCAN_REPORT_DB'
     GENERATE_ETL_REPORT = 'GENERATE_ETL_REPORT'
 
@@ -24,23 +20,69 @@ class WhiteRabbitRequestType(BaseModel):
     username: Optional[str] = None
 
 
-class iniSettings(BaseModel):
-    WORKING_FOLDER: str
-    DATA_TYPE: str
-    SERVER_LOCATION: str
-    USER_NAME: str
-    PASSWORD: str
-    DATABASE_NAME: str
-    DELIMITER: str
-    TABLES_TO_SCAN: str
-    SCAN_FIELD_VALUES: str
-    MIN_CELL_COUNT: str
-    MAX_DISTINCT_VALUES: str
-    ROWS_PER_TABLE: str
-    CALCULATE_NUMERIC_STATS: str
-    NUMERIC_STATS_SAMPLER_SIZE: str
+class INISettings(BaseModel):
 
+    scan_type: WhiteRabbitRunType
 
+    # For scanning source database tables
+    server_location: str = Field(default="")
+    user_name: str = Field(default="")
+    password: str = Field(default="")
+    schema: str = Field(default="")
+    database: str = Field(default="")
+    data_type: str = Field(default="")
+
+    # For scanning CSV Files, currently on ',' supported
+    delimiter: str = Field(default=",")
+
+    tables_to_scan: str = Field(default="*", description="Tables to scan")
+    scan_field_values: str = Field(default="yes")
+    min_cell_count: str = Field(default="5")
+    max_distinct_values: str = Field(default="1000")
+    rows_per_table: str = Field(default="100000")
+    calculate_numeric_stats: str = Field(default="yes", description="Numeric stats")
+    numeric_stats_sampler_size: str = Field(default="500", description="Numeric stats reservoir size")
+
+    @property
+    def working_folder(self) -> str:
+        if self.scan_type == WhiteRabbitRunType.SCAN_REPORT_FILES:
+            return WHITERABBIT_CSV_DIR
+        return WHITERABBIT_DIR_PATH
+    
+    @property
+    def scan_report_path(self) -> str:
+        """Get the expected path for the ScanReport.xlsx file based on scan type."""
+        return f"{self.working_folder}/ScanReport.xlsx"
+    
+    @property
+    def data_type_source(self) -> str:
+        if self.scan_type == WhiteRabbitRunType.SCAN_REPORT_FILES:
+            return "Delimited text files"
+        elif self.scan_type == WhiteRabbitRunType.SCAN_REPORT_DB:
+            return self.data_type
+
+    def dump_settings_json(self) -> dict:
+        '''
+        Serialize only the required fields to create the config.ini file
+        '''
+        return {
+            "working_folder": self.working_folder,
+            "data_type": self.data_type_source,
+            "server_location": self.server_location,
+            "user_name": self.user_name,
+            "password": self.password,
+            "database_name": self.schema,
+            "delimiter": self.delimiter,
+            "tables_to_scan": self.tables_to_scan,
+            "scan_field_values": self.scan_field_values,
+            "min_cell_count": self.min_cell_count,
+            "max_distinct_values": self.max_distinct_values,
+            "rows_per_table": self.rows_per_table,
+            "calculate_numeric_stats": self.calculate_numeric_stats,
+            "numeric_stats_sampler_size": self.numeric_stats_sampler_size,
+        }
+
+# Todo: Check if need to remove
 class FileSaveResponse(BaseModel):
     id: int
     username: str
