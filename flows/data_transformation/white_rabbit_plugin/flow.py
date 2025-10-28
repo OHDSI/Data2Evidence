050ac88b83edcfbcfd32f8a2aa3c879322f04862
alicia-koh <alicia.koh@data4life-asia.care>
2025-10-28 17:36:48 +0800

    Combine scan flow

--- flows/data_transformation/white_rabbit_plugin/flow.py
+++ flows/data_transformation/white_rabbit_plugin/flow.py
@@ -1,52 +1,57 @@
+from xvfbwrapper import Xvfb
+
 from prefect import flow
 from prefect.logging import get_run_logger
-from .types import WhiteRabbitRequestType, WhiteRabbitRunType
+
 from .tasks import *
 from .data_mapping_tasks import *
+from .types import WhiteRabbitRequestType, WhiteRabbitRunType
 
 
 @flow(log_prints=True)
 def white_rabbit_plugin(options: WhiteRabbitRequestType):
     logger = get_run_logger()
-    logger.info("triggering white rabbit flow")
-
-    match options.run_type:
-        case WhiteRabbitRunType.SCAN_REPORT_DB:
-            logger.info("scan report db")
-            scan_report_db_flow(options)
-        case WhiteRabbitRunType.SCAN_REPORT_FILES:
-            logger.info("scan report files")
-            scan_report_file_flow(options)
-        case WhiteRabbitRunType.GENERATE_ETL_REPORT:
-            logger.info("generate etl report")
-            generate_etl_report_flow(options)
-
-
-def scan_report_db_flow(options: WhiteRabbitRequestType):
-    start_awt_display()
-    create_white_rabbit_settings(options.data)
-    create_scan_report()
-    save_response: FileSaveResponse = save_scan_report_conversion(
-        options.username)
-    process_scan_report(
-        save_response['id'], save_response['fileName'], options.username)
+    x11_display_number = 1
+    logger.info(f"Starting X11 virtual display server (Xvfb) on display :{x11_display_number} for Java AWT operations...")
+    with Xvfb(display=x11_display_number):
+        '''
+        Starts a virtual X11 display server (Xvfb) to enable WhiteRabbit to run headless.
+        This is needed for WhiteRabbit to run headless since it uses Java AWT components.
+        '''
+        match options.run_type:
+            case WhiteRabbitRunType.SCAN_REPORT_DB | WhiteRabbitRunType.SCAN_REPORT_FILES:
+                scan_report_flow(options)
+            case WhiteRabbitRunType.GENERATE_ETL_REPORT:
+                generate_etl_report_flow(options)
 
 
-def scan_report_file_flow(options: WhiteRabbitRequestType):
-    start_awt_display()
-    file_contents = options.data.get('files', [])
-    settings = options.data.get('settings', {})
-    generate_csv_files_from_json(file_contents)
-    create_white_rabbit_settings(
-        {'data_type': "Delimited text files", **settings})
-    create_scan_report()
-    save_response: FileSaveResponse = save_scan_report_conversion(
-        options.username)
-    process_scan_report(
-        save_response['id'], save_response['fileName'], options.username)
+def scan_report_flow(options: WhiteRabbitRequestType):
+    scan_type = options.run_type
 
+    config_ini_path, scan_report_path = create_white_rabbit_settings(scan_type, options.data)
+
+    # Todo: Update to read csv from supabase storage
+    if scan_type == WhiteRabbitRunType.SCAN_REPORT_FILES:
+        # Todo: To remove, hardcoded for testing
+        node_id = "eb56c955-440d-4d6f-a2f4-143470ec0d9b"
+        file_name = "dm.csv"
+        file_downloaded = download_file_from_supabase_storage(node_id, file_name)
+
+    if (scan_type == WhiteRabbitRunType.SCAN_REPORT_FILES and file_downloaded) \
+        or scan_type == WhiteRabbitRunType.SCAN_REPORT_DB:
+        scan_report_path = create_scan_report(config_ini_path, scan_report_path)
+
+    # Todo: Upload to supabase storage under node id
+    # save_response = save_scan_report_conversion(options.username, scan_report_path)
+
+    # Todo: Refactor 
+    # process_scan_report(save_response, scan_report_path, options.username)
+    
+    
 
 def generate_etl_report_flow(options: WhiteRabbitRequestType):
-    start_awt_display()
-    generateDataJson(options.data)
-    generateETLWordDocument()
+    data_mapping_file_name = "datamapping.json"
+
+    data_mapping_file_path = generate_data_json(options.data, data_mapping_file_name)
+    
+    generate_etl_word_document(data_mapping_file_path)
