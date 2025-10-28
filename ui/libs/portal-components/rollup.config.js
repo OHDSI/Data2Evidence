d40f4fab7504f299413037ee98b6fd6fe6ca1f1d
suwarno ong <suwarno.ong@hotmail.com>
2025-10-28 12:38:41 +0800

    Revert rollup changes in portal-components

--- ui/libs/portal-components/rollup.config.js
+++ ui/libs/portal-components/rollup.config.js
@@ -51,6 +51,6 @@ export default [
     // input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: [/\.(css|less|scss)$/, /@mui\//],
+    external: [/\.(css|less|scss)$/],
   },
 ];
