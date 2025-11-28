628bda4d8f1c66552daf35201e22976d0128657b
maggie-li-yd <maggie.li@data4life-asia.care>
2025-11-28 13:58:31 +0800

    update concept-sets e2e

--- tests/e2e/tests/12-concepts/concept-sets.spec.ts
+++ tests/e2e/tests/12-concepts/concept-sets.spec.ts
@@ -61,16 +61,14 @@ test(TEST_NAME, async ({ page }) => {
     await page.getByRole('button', { name: 'Close' }).click()
   }
 
-  // await test.step('Attempt to create another concept set with the same name', async () => {
-  //   await page.getByTestId('button').click()
-  //   await page.getByRole('textbox', { name: 'Concept set name' }).click()
-  //   await page.getByRole('textbox', { name: 'Concept set name' }).fill(conceptSetName)
-  //   await page.getByRole('button', { name: 'Create' }).click()
-  //   await expect(
-  //     page.getByText(`Concept set name "${conceptSetName}" already exists. Please enter another name.`)
-  //   ).toBeVisible()
-  //   await page.getByRole('button', { name: 'Close' }).click()
-  // })
+  await test.step('Attempt to create another concept set with the same name', async () => {
+    await page.getByTestId('button').click()
+    await page.getByRole('textbox', { name: 'Concept set name' }).click()
+    await page.getByRole('textbox', { name: 'Concept set name' }).fill(conceptSetName)
+    await page.getByRole('button', { name: 'Create' }).click()
+    await expect(page.getByText(`Please enter another name.`)).toBeVisible()
+    await page.getByRole('button', { name: 'Close' }).click()
+  })
 
   // Cohort builder
   await page.getByRole('link', { name: 'Cohorts' }).click()
