3424bc95874a22dc29d0cae39f2f0553d4bab729
alicia-koh <110385419+alicia-koh@users.noreply.github.com>
2025-12-13 13:36:48 +0800

    Trigger create cache plugin for fhir dataset from UI (#1342)
    * Add cache schema name param to fhir cache plugin
    
    * Link fhir cache plugin to UI
    
    * Update default fhir dataset dialect value
    
    * Remove check for fhir project id
    
    * Fix tables not being created
    
    * Update flow.py

--- flows/base/create_cachedb_fhir_plugin/config.py
+++ flows/base/create_cachedb_fhir_plugin/config.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel
 class CreateDuckdbDatabaseFileType(BaseModel):
     databaseCode: str
     schemaName: str
+    cacheSchemaName: str
+
+    @property
+    def sourceDatabase(self) -> str:
+        return f"{self.databaseCode}__srcdb"
--- flows/base/create_cachedb_fhir_plugin/utils.py
+++ flows/base/create_cachedb_fhir_plugin/utils.py
@@ -1,5 +1,3 @@
-import os
-
 from prefect import task
 
 from _shared_flow_utils.types import SupportedDatabaseDialects
