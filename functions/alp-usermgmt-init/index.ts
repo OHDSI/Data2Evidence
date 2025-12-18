c7410af555382e64ca19caadeab6fe9d72312505
hengxian-jiang <155619404+hengxian-jiang@users.noreply.github.com>
2025-12-18 10:58:17 +0800

    [Integrate-574] Move out init functions to main function (#1441)
    * Move out init functions
    
    * Rename to supabase-storage-init
    
    * Delete old supabase-storage init folder
    
    * Remove db folder in white-rabbit function
    
    * Add deno.lock files for new created init functions
    
    * Remove migration related files in perseus function
    
    * Remove unused migration related files in files-manager function
    
    * Remove migrations and seeds from usermgmt function
    
    * Remove portal migration and seed files in portal function
    
    * Remove migrations for strategus-analysis function
    
    * Update user-artifact seeders
    
    * Fix user seeding in usermgmt

---
+++ functions/alp-usermgmt-init/index.ts
@@ -0,0 +1,24 @@
+import knex from "knex";
+import config from "./src/db/knexfile-admin.ts";
+import { MigrationSource } from "./src/db/MigrationSource.ts";
+import { SeedSource } from "./src/db/SeedSource.ts";
+import * as pg from 'pg'
+
+const k = knex(config);
+
+try {
+  console.log("Initializing DataSource...");
+  console.log(">>> Running Migrations <<<");
+  const migrationResult = await k.migrate.latest({ migrationSource: new MigrationSource() });
+  console.log("Migrations Done:", migrationResult);
+
+  console.log(">>> Running Seeds <<<");
+  const seedResult = await k.seed.run({ seedSource: new SeedSource() });
+  console.log("Seeds Done:", seedResult);
+
+  console.log(">>> Initialization Complete <<<");
+} catch (error) {
+  console.error("Error during DataSource initialization:", error);
+} finally {
+  await k.destroy();
+}
