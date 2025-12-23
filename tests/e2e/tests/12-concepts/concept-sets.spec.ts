045da4025e2ed3833e5191e2980962272b52ceda
maggie-li-yd <maggie.li@data4life-asia.care>
2025-12-23 15:20:52 +0800

    update concept sets e2e

--- tests/e2e/tests/12-concepts/concept-sets.spec.ts
+++ tests/e2e/tests/12-concepts/concept-sets.spec.ts
@@ -71,6 +71,7 @@ test(TEST_NAME, async ({ page }) => {
 
   await test.step('Attempt to create another concept set with the same name', async () => {
     await page.reload()
+    await page.getByRole('tab', { name: 'Concept Sets' }).click()
     await takeScreenshot(page)
     await page.getByTestId('button').click()
     await page.getByRole('textbox', { name: 'Concept set name' }).click()
