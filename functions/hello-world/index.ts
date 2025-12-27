86580215a5ccda52d2f936cced13cf8e383d442c
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2025-12-27 18:06:01 +0800

    cleanup

--- functions/hello-world/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { HelloWorldController } from "./controllers/HelloWorldController.js";
-
-const app = express();
-
-app.use(express.json());
-app.use("/hello-world", new HelloWorldController().router);
-app.listen(8000);
