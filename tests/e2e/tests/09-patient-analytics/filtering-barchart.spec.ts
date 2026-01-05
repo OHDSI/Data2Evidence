61d14ce72f5c975f28e977ec3ca6158dc0d2a27c
Jerome Ng <jerome.ng@data4life-asia.care>
2026-01-05 08:08:21 +0800

    update

--- tests/e2e/tests/09-patient-analytics/filtering-barchart.spec.ts
+++ tests/e2e/tests/09-patient-analytics/filtering-barchart.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from '@playwright/test'
+import { takeScreenshot } from '../screenshot-capture'
 
 const TEST_NAME = 'filtering-barchart'
 const SHOULD_SKIP = false
 test.fixme(SHOULD_SKIP, `${TEST_NAME} test is temporarily disabled.`)
 
-test(TEST_NAME, async ({ page }) => {
+test(TEST_NAME, async ({ page }, testInfo) => {
   test.slow()
   // Sign in
   await page.goto('/d2e/portal')
@@ -20,7 +21,7 @@ test(TEST_NAME, async ({ page }) => {
   await page.getByRole('button', { name: 'D2E' }).click()
   await expect(page.getByText('2,694 / 2,694')).toBeVisible()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 })
+  await takeScreenshot(page, testInfo)
 
   // Add filter card
   await page.getByTitle('Add Filter Card').getByRole('button').click()
@@ -64,7 +65,7 @@ test(TEST_NAME, async ({ page }) => {
     // Modal not present, continue
   }
 
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 })
+  await takeScreenshot(page, testInfo)
 
   // Set X1-axis to condition concept name
   await page
@@ -75,7 +76,7 @@ test(TEST_NAME, async ({ page }) => {
   await page.locator('div.dropdownmenu-container').getByText('Condition Occurrence A').click()
   await page.locator('#pane-right').getByText('Condition concept Name').click()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 })
+  await takeScreenshot(page, testInfo)
 
   // Filter condition concept name to chronic sinusitis
   await page.getByText('All').nth(2).click()
@@ -85,7 +86,7 @@ test(TEST_NAME, async ({ page }) => {
     .fill('Chronic sinusitis')
   await page.getByText('Chronic sinusitis - Chronic sinusitis').click()
   await expect(page.getByText('812 / 2,694')).toBeVisible()
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 })
+  await takeScreenshot(page, testInfo)
 
   // Set X1-axis to gender
   await page.getByRole('button', { name: 'Basic Data Gender ◢' }).click()
@@ -94,14 +95,14 @@ test(TEST_NAME, async ({ page }) => {
   await page.locator('#pane-right').getByRole('list').getByText('Basic Data').click()
   await page.locator('#pane-right').getByText('Gender').nth(2).click()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 })
+  await takeScreenshot(page, testInfo)
 
   // Set Y-axis to month of birth
   await page.locator('div.axis-menu-button-wrapper').nth(6).getByRole('button').click()
   await page.locator('div.dropdownmenu-container').getByText('Basic Data').last().click()
   await page.locator('div.dropdownmenu-container').getByText('Month of Birth').last().click()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 })
+  await takeScreenshot(page, testInfo)
 
   // Set Y-axis to patient count
   await page.locator('div.axis-menu-button-wrapper').nth(6).getByRole('button').click()
@@ -126,7 +127,7 @@ test(TEST_NAME, async ({ page }) => {
     .click()
   await page.locator('#pane-right').getByText('Race concept id').click()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
-  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.02 })
+  await takeScreenshot(page, testInfo)
 
   // Set X2-axis to year of birth with bin size of 50
   await page.locator('div.axis-menu-button-wrapper').nth(2).getByRole('button').first().click()
@@ -142,13 +143,13 @@ test(TEST_NAME, async ({ page }) => {
   await page.getByRole('textbox', { name: 'Size of the Bins' }).press('Enter')
   await page.locator('.modal-wrapper').click()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
-  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.02 })
+  await takeScreenshot(page, testInfo)
 
   // Reset X2-axis
   await page.locator('div.axis-menu-button-wrapper').nth(2).getByRole('button').first().click()
   await page.locator('div.dropdownmenu-container').getByText('Reset Selection').nth(1).click()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
-  await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.02 })
+  await takeScreenshot(page, testInfo)
 
   // Set attribute for stacked chart
   await page.locator('div.axis-menu-button-wrapper').nth(4).getByRole('button').click()
@@ -160,41 +161,41 @@ test(TEST_NAME, async ({ page }) => {
     .click()
   await page.locator('#pane-right').getByText('Month of Birth').first().click()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 1200 })
+  await takeScreenshot(page, testInfo)
 
   // Set month of birth to 11 in filter card
   await page.getByTitle('Basic Data - Month of Birth').first().click()
   await page.getByTitle('Basic Data - Month of Birth').getByRole('textbox').fill('11')
   await page.getByTitle('Basic Data - Month of Birth').getByRole('textbox').press('Enter')
   await expect(page.getByText('115 / 2,694')).toBeVisible()
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 })
+  await takeScreenshot(page, testInfo)
 
   // Remove condition occurrence filter card
   await page.locator('span[title="Select Filter Attributes"]').nth(1).click()
   await page.getByRole('menuitem').getByText('Remove Filter Card').click()
   await expect(page.getByText('247 / 2,694')).toBeVisible()
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 })
+  await takeScreenshot(page, testInfo)
 
   // Switch to list view
   await page.locator('button.chartButton').nth(1).click()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 })
+  await takeScreenshot(page, testInfo)
 
   // Export to ZIP file
   await page.locator('button.toolbarButton').nth(1).click()
   await page.getByRole('menuitem').getByText('Export to ZIP File').click()
   await page.locator('span.buttonContent').nth(1).click()
   await page.waitForTimeout(5000) // Wait for download to complete
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 }) // Not sure what to expect
+  await takeScreenshot(page, testInfo)
 
   // Switch to chart view
   await page.locator('button.chartButton').first().click()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 })
+  await takeScreenshot(page, testInfo)
 
   // Reset filter card
   await page.getByRole('button', { name: '↺' }).click()
   await page.locator('button[title="Reset"]').click()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
-  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 })
+  await takeScreenshot(page, testInfo)
 })
