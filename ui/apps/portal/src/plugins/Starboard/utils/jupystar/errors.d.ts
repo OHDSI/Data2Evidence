ed4e352fab7450ad714f17027e7fe2965a29f56d
suwarno ong <48011859+suwarnoong@users.noreply.github.com>
2026-01-06 11:46:15 +0800

    1274 Remove built-in starboard in portal (#1585)

--- ui/apps/portal/src/plugins/Starboard/utils/jupystar/errors.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class JupystarError extends Error {
-    constructor(reason: string);
-}
-export declare class BackwardsCompatibilityError extends JupystarError {
-    constructor(reason: string);
-}
--- ui/apps/portal/src/plugins/Starboard/utils/jupystar/options.d.ts
+++
@@ -1,5 +0,0 @@
-export interface JupystarOptions {
-    convertLatexBlocks: boolean;
-}
-export declare const DEFAULT_JUPYSTAR_OPTIONS: JupystarOptions;
-export declare function getJupystarOptions(userOptions?: Partial<JupystarOptions>): JupystarOptions;
