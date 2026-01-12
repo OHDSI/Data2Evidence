65d6ed921dcfc0ba38ba6eb131aa2fd47359d186
Jerome Ng <jerome.ng@data4life-asia.care>
2026-01-12 16:32:00 +0800

    Fix apps/flow build

--- ui/apps/flow/src/components/Dialog/ScanProgressDialog/ScanProgressDialog.tsx
+++ ui/apps/flow/src/components/Dialog/ScanProgressDialog/ScanProgressDialog.tsx
@@ -44,7 +44,7 @@ export const ScanProgressDialog: FC<ScanProgressDialogProps> = ({
   const [scanCompleted, setScanCompleted] = useState(false);
   const [scanFailed, setScanFailed] = useState(false);
   const [log, setLog] = useState<string>("");
-  const intervalRef = useRef<number | null>(null);
+  const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
   const updateNodeInternals = useUpdateNodeInternals();
   // setTableSourceHandles
   // setScannedSchema
