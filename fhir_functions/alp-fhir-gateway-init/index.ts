6b0770301c292c7f1ea8fed722d2fb2de6b68d04
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2026-02-05 07:28:21 +0800

    cleanup

--- fhir_functions/alp-fhir-gateway-init/index.ts
+++ fhir_functions/alp-fhir-gateway-init/index.ts
@@ -4,5 +4,4 @@ try {
     await seed();
 } catch (error) {
     console.error("FHIR gateway init failed:", error);
-    // Don't re-throw - this is an init script, failing gracefully is better than crashing the runtime
 }
