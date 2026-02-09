4e5a771b8ae5c6c535a21908c29e1c14d48834bd
Khairul Syazwan <khairul.masri@data4life-asia.care>
2026-02-09 15:23:07 +0800

    Remove logs

--- ui/apps/vue-mri-ui-lib/src/store/modules/wizardConfig.ts
+++ ui/apps/vue-mri-ui-lib/src/store/modules/wizardConfig.ts
@@ -8,7 +8,6 @@ const state = {
 // getters
 const getters = {
   getWizardConfig: modulestate => {
-    console.log('[WizardConfig Getter] getWizardConfig called, returning:', modulestate.wizardConfig)
     return modulestate.wizardConfig
   },
 }
@@ -16,12 +15,9 @@ const getters = {
 // actions
 const actions = {
   setWizardConfig({ commit }, wizardConfig) {
-    console.log('[WizardConfig Action] setWizardConfig called with:', wizardConfig)
     commit(types.SET_WIZARD_CONFIG, wizardConfig)
-    console.log('[WizardConfig Action] SET_WIZARD_CONFIG mutation committed')
   },
   clearWizardConfig({ commit }) {
-    console.log('[WizardConfig Action] clearWizardConfig called')
     commit(types.CLEAR_WIZARD_CONFIG)
   },
 }
@@ -29,13 +25,9 @@ const actions = {
 // mutations
 const mutations = {
   [types.SET_WIZARD_CONFIG](modulestate, wizardConfig) {
-    console.log('[WizardConfig Mutation] SET_WIZARD_CONFIG called with:', wizardConfig)
-    console.log('[WizardConfig Mutation] Before:', modulestate.wizardConfig)
     modulestate.wizardConfig = wizardConfig
-    console.log('[WizardConfig Mutation] After:', modulestate.wizardConfig)
   },
   [types.CLEAR_WIZARD_CONFIG](modulestate) {
-    console.log('[WizardConfig Mutation] CLEAR_WIZARD_CONFIG called')
     modulestate.wizardConfig = null
   },
 }
