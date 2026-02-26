0050cea80b3c1d1d2ad55e422fe93f5cb421249a
LSriragavan <sriragavan@gmail.com>
2026-02-26 12:13:56 +0800

    remove using class

--- tests/backend_integration_tests/lib/db-connection-util.js
+++ tests/backend_integration_tests/lib/db-connection-util.js
@@ -1,3 +1,4 @@
+var assert = require('assert')
 const { PGConnection } = require('./pg/pg-connection-lib')
 const { HDBConnection } = require('./hdb/hdb-connection-lib')
 var utils = require('./utils')
@@ -5,14 +6,19 @@ var utils = require('./utils')
 const dbDialect = process.env.DB_DIALECT ? process.env.DB_DIALECT : 'pg'
 
 function createDBConnection(credentials) {
-  const log = utils.getLogger(credentials.getLogStatus(), 'In db-connection-util: ')
-  log(`DB dialect: ${dbDialect}`)
-  if (dbDialect.uppercaseString() === 'PG') {
-    // load PG lib
-    return PGConnection.createConnection(credentials)
-  } else if (dbDialect.uppercaseString() === 'HDB') {
-    // load HDB lib
-    return HDBConnection.createConnection(credentials)
+  console.log(`DB dialect: ${dbDialect}`)
+  assert(credentials, 'Invalid credentials object!')
+  assert(credentials.user, 'Invalid user specified in credentials object!')
+  assert(credentials.password, 'Invalid password specified in credentials object!')
+  assert(credentials.host, 'Invalid host specified in credentials object!')
+  assert(credentials.port, 'Invalid port specified in credentials object!')
+  if (dbDialect.toUpperCase() === 'PG') {
+    assert(credentials.database, 'Invalid database specified in credentials object!')
+    // create PG connection
+    return new PGConnection(credentials)
+  } else if (dbDialect.toUpperCase() === 'HDB') {
+    // create HDB connection
+    return new HDBConnection(credentials)
   }
 }
 
@@ -22,57 +28,11 @@ function getSystemCredentials(hostConfig) {
   credentials.port = hostConfig.dbport
   credentials.user = hostConfig.system_user
   credentials.password = hostConfig.system_password
-  if (dbDialect.uppercaseString() === 'PG') {
+  if (dbDialect.toUpperCase() === 'PG') {
     credentials.database = hostConfig.database
   }
 
   return credentials
-}
-
-module.exports = class DBConnection {
-  constructor(user, password, host, port) {
-    this.user = user
-    this.password = password
-    this.host = host
-    this.port = port
-  }
-
-  /**
-   * This is an abstract method to initialize connection
-   */
-  initConnection(cb) {}
-
-  /**
-   * This is an abstract method, will be implemented in the sub classes
-   * @param {string}
-   *            sqlCmd SQL command.
-   * @param {function} cb - callback
-   */
-  executeSqlCommand(sqlCmd, cb) {}
-
-  /**
-   * Prepare an SQL statement and execute it with the passed paramters.
-   *
-   * @param {string}
-   *            sqlCmd SQL command with value parameterPlaceholders
-   * @param {Arrax} parameterArray - array of parameters (in correct order)
-   * @param {Function} cb - callback
-   */
-  executeSqlStatement(sqlCmd, parameterArray, cb) {}
-  
-  /**
-   * This is an abstract method, will be implemented in the sub classes
-   *
-   * @param {string} sqlProc  - SQL procedure
-   * @param {Object} params  - paramter object (JSON)
-   * @param {function} cb  - callback
-   */
-  callSqlProcedure(sqlProc, params, cb) {}
-
-  /**
-   * This is an abstract method to implementation (if any) to terminate the DB connection after processing remaining tasks
-   */
-  endConnection() {}
 }
 
 module.exports = { createDBConnection, getSystemCredentials }
--- tests/backend_integration_tests/lib/pg/pg-connection-lib.js
+++ tests/backend_integration_tests/lib/pg/pg-connection-lib.js
@@ -1,164 +1,105 @@
 const pg = require('pg')
-const format = require('pg-format')
 const { Pool } = pg
-var assert = require('assert')
-const DBConnection = require('../db-connection-util')
 
-
-module.exports = class PGConnection extends DBConnection {
-  constructor(user, password, host, port, database) {
-    super(user, password, host, port)
-    this.database = database
-
-    this.pool = new Pool({
-      user,
-      password,
-      host,
-      port,
-      database,
-      // min: 3,
-      max: 10
-      // idleTimeoutMillis:5000,
-      // maxLifetimeSeconds:10
+function PGConnection(credentials) {
+  this.database = credentials.database
+  creadentials["max"] = 10
+  this.pool = new Pool(credentials)
+  this.client = null
+}
+/**
+ * Initialize PG connection
+ */
+PGConnection.prototype.initConnection = cb => {
+  try {
+    this.pool.on('error', (err, client) => {
+      if (err) {
+        console.error('Unexpected error on idle client', err)
+        client.release(true)
+        cb(err)
+      }
     })
-    this.client = null
-  }
-
-  static createConnection(credentials) {
-    assert(credentials, 'Invalid credentials object!')
-    assert(credentials.user, 'Invalid user specified in credentials object!')
-    assert(credentials.password, 'Invalid password specified in credentials object!')
-    assert(credentials.host, 'Invalid host specified in credentials object!')
-    assert(credentials.port, 'Invalid port specified in credentials object!')
-    assert(credentials.database, 'Invalid database specified in credentials object!')
-
-    return new PGConnection(
-      credentials.user,
-      credentials.password,
-      credentials.host,
-      credentials.port,
-      credentials.database
-    )
-  }
-
-  /**
-   * Initialize PG connection
-   */
-  initConnection(cb) {
-    try {
-      this.pool.on('error', (err, client) => {
-        if (err) {
-          console.error('Unexpected error on idle client', err)
-          client.release(true)
-          cb(err)
-        }
-      })
-    } catch (err) {
-      err.stack
-      cb(err)
-    }
-  }
-
-  /**
-   * Carry out and commit an SQL command.
-   *
-   * @param {string}
-   *            sqlCmd SQL command.
-   * @param {function} cb - callback
-   */
-  executeSqlCommand(query, cb) {
-    console.log(`Query to be executed:\n${query}`)
-    try {
-      this.pool.on('error', (err, client) => {
-        if (err) {
-          console.error('Unexpected error on idle client', err)
-          client.release(true)
-          cb(err)
-        }
-      })
-      return this.pool.query(query).then(res => {
-        console.log(`Executed query:\n${query}`)
-        cb(null, res.rows)
-      })
-    } catch (err) {
-      err.stack
-      cb(err)
-    }
-  }
-
-  /**
-   * Prepare an SQL statement and execute it with the passed paramters.
-   *
-   * @param {string}
-   *            sqlCmd SQL command with value parameterPlaceholders
-   * @param {Arrax} parameterArray - array of parameters (in correct order)
-   * @param {Function} cb - callback
-   */
-  executeSqlStatement(sqlCmd, parameterArray, cb) {
-    console.log(`Query to be executed:\n${query}`)
-    console.log(`parameterArray:\n${parameterArray}`)
-
-    return this.pool.query(sqlCmd, parameterArray).then(res => {
-      console.log(`Executed query:\n${query}`)
-      cb(null, res.rows)
-    })
-  }
-
-  /**
-   * Call an SQL procedure.
-   *
-   * @param {string} sqlProc  - SQL procedure
-   * @param {Object} params  - paramter object (JSON)
-   * @param {function} cb  - callback
-   */
-  callSqlProcedure(sqlProc, params, cb) {
-    console.log(`sqlProc:\n${sqlProc}`)
-    console.log(`params:\n${params}`)
-    try {
-      this.pool.on('error', (err, client) => {
-        if (err) {
-          console.error('Unexpected error on idle client', err)
-          client.release(true)
-          cb(err)
-        }
-      })
-      return this.pool.query(`CALL ${sqlProc}`, params).then(res => {
-        console.log(`Executed query:\n${query}`)
-        cb(null, res)
-      })
-    } catch (err) {
-      err.stack
-      cb(err)
-    }
-  }
-
-  async releaseClient() {
-    if (this.client) {
-      await this.client.end()
-    }
-  }
-
-  async executeStatement(schema, table, columns, data) {
-    const query = `INSERT INTO "${schema}"."${table}" (%I) VALUES %L`
-    const finalQuery = format(query, columns, data)
-    return await executeQuery(finalQuery)
+  } catch (err) {
+    err.stack
+    cb(err)
   }
 }
 
 /**
- * Get the PD DB SYSTEM login data for the PG connection
+ * Carry out and commit an SQL command.
  *
- * @returns {Object} - JSON object with the login details
+ * @param {string}
+ *            sqlCmd SQL command.
+ * @param {function} cb - callback
  */
-HostConfig.prototype.getPGSystemCredentials = function () {
-  var credentials = {}
-  credentials.host = this.dbhost
-  credentials.port = this.dbport
-  credentials.database = this.database
-  credentials.user = this.system_user
-  credentials.password = this.system_password
+PGConnection.prototype.executeSqlCommand = (sqlCmd, cb) => {
+  console.log(`Query to be executed:\n${sqlCmd}`)
+  try {
+    this.pool.on('error', (err, client) => {
+      if (err) {
+        console.error('Unexpected error on idle client', err)
+        client.release(true)
+        cb(err)
+      }
+    })
+    return this.pool.query(sqlCmd).then(res => {
+      console.log(`Executed query:\n${sqlCmd}`)
+      cb(null, res.rows)
+    })
+  } catch (err) {
+    err.stack
+    cb(err)
+  }
+}
 
-  return credentials
+/**
+ * Prepare an SQL statement and execute it with the passed paramters.
+ *
+ * @param {string}
+ *            sqlCmd SQL command with value parameterPlaceholders
+ * @param {Arrax} parameterArray - array of parameters (in correct order)
+ * @param {Function} cb - callback
+ */
+PGConnection.prototype.executeSqlStatement = (sqlCmd, parameterArray, cb) => {
+  console.log(`Query to be executed:\n${query}`)
+  console.log(`parameterArray:\n${parameterArray}`)
+
+  return this.pool.query(sqlCmd, parameterArray).then(res => {
+    console.log(`Executed query:\n${query}`)
+    cb(null, res.rows)
+  })
+}
+
+/**
+ * Call an SQL procedure.
+ *
+ * @param {string} sqlProc  - SQL procedure
+ * @param {Object} params  - paramter object (JSON)
+ * @param {function} cb  - callback
+ */
+PGConnection.prototype.callSqlProcedure = (sqlProc, params, cb) => {
+  console.log(`sqlProc:\n${sqlProc}`)
+  console.log(`params:\n${params}`)
+  try {
+    this.pool.on('error', (err, client) => {
+      if (err) {
+        console.error('Unexpected error on idle client', err)
+        client.release(true)
+        cb(err)
+      }
+    })
+    return this.pool.query(`CALL ${sqlProc}`, params).then(res => {
+      console.log(`Executed query:\n${query}`)
+      cb(null, res)
+    })
+  } catch (err) {
+    err.stack
+    cb(err)
+  }
+}
+
+PGConnection.prototype.releaseClient = () => {
+  console.log('Not implemented')
 }
 
 module.exports = { PGConnection }
