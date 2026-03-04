0e2dcb14fdee256b294996393ed2559f292d1158
Zhi Min <zhi.min@data4life-asia.care>
2026-03-04 20:53:00 +0800

    [Data - 1847] User accessible dataset with researcher role (#1887)
    * user accessible dataset withresearcher role
    
    * refractor
    
    * update phenotype getMe and getRoles

--- functions/demo/api/UserMgmtAPI.ts
+++ functions/demo/api/UserMgmtAPI.ts
@@ -48,9 +48,25 @@ export class UserMgmtAPI {
     }
   }
 
+  async getMyRoles(): Promise<{
+    datasetRoles: { tenantId: string; datasetId: string; role: string }[];
+  }> {
+    try {
+      const options = await this.getRequestConfig();
+      const url = `${this.baseURL}/me/roles`;
+      const result = await this.channel.get(url, options);
+      return result.data;
+    } catch (error: any) {
+      const status = error.status || error.response?.status;
+      const responseData = error.response?.data;
+      console.error(`Error while getting /me/roles: ${error.message}, status: ${status}, data: ${JSON.stringify(responseData)}`);
+      throw error;
+    }
+  }
+
   async getMe(): Promise<{ id: string; username: string }> {
     try {
-      const options = this.getRequestConfig();
+      const options = await this.getRequestConfig();
       const url = `${this.baseURL}/me`;
       const result = await this.channel.get(url, options);
       return result.data;
