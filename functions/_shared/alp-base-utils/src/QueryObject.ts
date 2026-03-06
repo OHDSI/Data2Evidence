cf93dd7868775ce808ac914c1f2e4e2c4dc28246
LSriragavan <sriragavan@gmail.com>
2026-03-06 18:31:33 +0800

    debug

--- functions/_shared/alp-base-utils/src/QueryObject.ts
+++ functions/_shared/alp-base-utils/src/QueryObject.ts
@@ -339,6 +339,8 @@ export class QueryObject {
       const shortenedQuery = this.shortenIdentifier(preparedQuery.sql);
       logger.debug(`[QueryObject.executeQuery]preparedQuery: ${JSON.stringify(preparedQuery)}`)
       logger.debug(`[QueryObject.executeQuery]shortenedQuery: ${JSON.stringify(shortenedQuery)}`)
+      logger.debug(`[QueryObject.executeQuery]schemaName: ${JSON.stringify(schemaName)}`)
+      logger.debug(`[QueryObject.executeQuery]connection: ${JSON.stringify(connection)}`)
 
       connection.executeQuery(
         shortenedQuery,
