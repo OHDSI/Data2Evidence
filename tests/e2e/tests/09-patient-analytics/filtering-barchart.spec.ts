21135ae1c429bac609f127d052e6022ec25acb0b
maggie-li-yd <maggie.li@data4life-asia.care>
2026-03-23 12:52:33 +0800

    update e2e tests

--- tests/e2e/tests/09-patient-analytics/filtering-barchart.spec.ts
+++ tests/e2e/tests/09-patient-analytics/filtering-barchart.spec.ts
@@ -74,11 +74,18 @@ test(TEST_NAME, async ({ page }) => {
   await page.waitForTimeout(500)
   await expect(page).toHaveScreenshot()
 
+  // reset x2-axis
+  await page
+    .locator('div.axis-menu-button-wrapper')
+    .getByRole('button', { name: 'Basic Data Month of Birth ◢' })
+    .click()
+  await page.locator('div.dropdownmenu-container').getByText('Reset Selection').click()
+  await expect(page.locator('.loading-animation-component')).not.toBeVisible()
   // Set X1-axis to condition concept name
   await page
     .locator('div.axis-menu-button-wrapper')
     .first()
-    .getByRole('button', { name: 'Basic Data Month of Birth ◢' })
+    .getByRole('button', { name: 'Basic Data Gender ◢' })
     .click()
   await page.locator('div.dropdownmenu-container').getByText('Condition Occurrence A').click()
   await page.locator('#pane-right').getByText('Condition concept Name').click()
