d8504e6737ab6c80068f75ef4f0a35f977e5e14d
maggie-li-yd <maggie.li@data4life-asia.care>
2026-03-30 13:08:35 +0800

    restore button text to inclusion report

--- ui/apps/vue-mri-ui-lib/src/components/ChartToolbar.vue
+++ ui/apps/vue-mri-ui-lib/src/components/ChartToolbar.vue
@@ -31,7 +31,7 @@
         />
       </div>
       <div class="dashboardButton" v-if="getActiveBookmark && enableInclusionReport">
-        <VButton @click="openInclusionReportModal">Attrition Plot</VButton>
+        <VButton @click="openInclusionReportModal">Inclusion Report</VButton>
       </div>
       <div class="d-flex">
         <template v-for="chart in chartConfig" :key="chart.name">
