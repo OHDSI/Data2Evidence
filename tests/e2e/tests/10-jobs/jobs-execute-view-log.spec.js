abc2da9e0bd32c03f1218355688f22e9982dc085
hengxian-jiang <hengxian.jiang@u.nus.edu>
2026-04-02 14:16:28 +0800

    Fix dqd and dc e2e

--- tests/e2e/tests/10-jobs/jobs-execute-view-log.spec.js
+++ tests/e2e/tests/10-jobs/jobs-execute-view-log.spec.js
@@ -112,6 +112,6 @@ test(TEST_NAME, async ({ page, context }) => {
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
   await expect(page.getByTestId('dialog-title')).toHaveText(/Results for dataset+/)
   await expect(page.getByRole('dialog')).toHaveText(
-    /.+1047 out of 1933 passed checks are Not Applicable, due to empty tables or fields.+/
+    /.+1,047 out of 1,933 passed checks are Not Applicable, due to empty tables or fields.+/
   )
 })
