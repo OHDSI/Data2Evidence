5f1a118d505a6a2ed5627bde5b411b6d6cc3e2a1
Zhimin-arya <zhi.min@data4life-asia.care>
2026-04-15 17:05:57 +0800

    e2e add search embedding for hybrid_search

--- tests/e2e/tests/04-attribute/hybrid-search.spec.ts
+++ tests/e2e/tests/04-attribute/hybrid-search.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '../fixtures'
+import { MINUTE_5 } from '../const'
 
 const TEST_NAME = 'attribute-hybrid-search'
 const SHOULD_SKIP = false
@@ -12,7 +13,24 @@ test(TEST_NAME, async ({ page }) => {
   await page.locator('input[name="password"]').fill('Updatepassword12345')
   await page.getByRole('button', { name: 'Sign in' }).click()
   await page.getByTestId('button').nth(1).click()
+
+  // Embedding concepts in admin portal
   await page.getByRole('button', { name: 'Switch to Admin portal' }).click()
+  await page.getByRole('link', { name: 'Datasets' }).click()
+  await page.locator('tr').filter({ hasText: 'Demo dataset' }).getByText('Select action').click()
+  await page.getByRole('option', { name: 'Setup semantic search' }).click()
+  await page.getByRole('button', { name: 'Setup semantic search' }).click()
+  await page.getByTestId('dialog-close').click()
+  await page.getByRole('link', { name: 'Jobs' }).click()
+  await expect(page.locator('a:has-text("Job Runs")')).toBeVisible()
+  const search_embed_entry = page
+    .locator('.flow-run-list-item')
+    .filter({ has: page.locator('a:has-text("search_embedding_plugin")') })
+    .first()
+  const search_embed_state = search_embed_entry.locator('.state-badge')
+  await expect(search_embed_state).toHaveText('Completed', { timeout: MINUTE_5 })
+
+  // Go to hybrid search setting pageEnable hybrid search and save
   await page.getByRole('link', { name: 'Setup' }).click()
   await page
     .locator('div')
@@ -20,10 +38,11 @@ test(TEST_NAME, async ({ page }) => {
     .getByTestId('button')
     .click()
 
-  // Enable hybrid search and save
   await page.getByRole('textbox', { name: 'Semantic Ratio' }).click()
   await page.getByRole('textbox', { name: 'Semantic Ratio' }).fill('0.5')
-  await page.getByText('Enable Hybrid Search').click()
+  if (!(await page.getByText('Enable Hybrid Search').isChecked())) {
+    await page.getByText('Enable Hybrid Search').click()
+  }
   await page.getByTestId('button').click()
   await expect(page.getByTestId('snackbar').locator('div').filter({ hasText: 'Changes saved' }).first()).toBeVisible()
   await page.reload()
@@ -32,7 +51,25 @@ test(TEST_NAME, async ({ page }) => {
   await expect(page.getByRole('textbox', { name: 'Semantic Ratio' })).toHaveValue('0.5')
   await expect(page.getByText('Enable Hybrid Search')).toBeChecked()
 
+  await page.getByRole('link', { name: 'Account' }).click()
+  await page.getByRole('button', { name: 'Switch to Researcher portal' }).click()
+  await page.getByText('Demo dataset').first().click()
+  await page.getByRole('link', { name: 'Concepts' }).click()
+
+  await page.getByRole('textbox', { name: 'search terms' }).click()
+  await page.getByRole('textbox', { name: 'search terms' }).fill('MALE')
+  await page.getByRole('textbox', { name: 'search terms' }).press('Enter')
+  await expect(page.getByRole('cell', { name: '1100.8961' })).toBeVisible()
+
   // Disable hybrid search and save
+  await page.getByRole('link', { name: 'Account' }).click()
+  await page.getByRole('button', { name: 'Switch to Admin portal' }).click()
+  await page.getByRole('link', { name: 'Setup' }).click()
+  await page
+    .locator('div')
+    .filter({ hasText: /^Hybrid searchConfigure hybrid search for conceptsConfigure$/ })
+    .getByTestId('button')
+    .click()
   await page.getByRole('textbox', { name: 'Semantic Ratio' }).click()
   await page.getByRole('textbox', { name: 'Semantic Ratio' }).fill('0')
   await page.getByText('Enable Hybrid Search').click()
