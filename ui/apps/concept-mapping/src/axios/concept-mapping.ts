792e87cba1f522868c5ca7001afd1c0b5fcf80ae
Nicholas Ang <nicholas.ang@data4life-asia.care>
2026-04-21 09:54:09 +0800

    Update concept-mapping.ts

--- ui/apps/concept-mapping/src/axios/concept-mapping.ts
+++ ui/apps/concept-mapping/src/axios/concept-mapping.ts
@@ -2,17 +2,23 @@ import { request } from "./request";
 const CONCEPT_MAPPING_URL = "concept-mapping";
 
 export class ConceptMapping {
-  public getConceptMappings = (datasetId: string) => {
+  public getConceptMappings = (databaseCode: string, schemaName: string) => {
     return request({
       baseURL: CONCEPT_MAPPING_URL,
       method: "GET",
       params: {
-        datasetId: datasetId,
+        databaseCode: databaseCode,
+        schemaName: schemaName,
       },
     });
   };
 
-  public saveConceptMappings = (databaseCode: string, schemaName: string, sourceVocabularyId: string, conceptMappings: string) => {
+  public saveConceptMappings = (
+    databaseCode: string,
+    schemaName: string,
+    sourceVocabularyId: string,
+    conceptMappings: string
+  ) => {
     return request({
       baseURL: CONCEPT_MAPPING_URL,
       method: "POST",
