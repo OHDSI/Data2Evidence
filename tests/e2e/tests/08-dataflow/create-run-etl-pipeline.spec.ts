75a4a1e071363f3d9ab75070687078f563ff8d51
Afreen Sikandara <afreen.sikandara@data4life-asia.care>
2026-04-22 13:54:18 +0800

    Add variables and view output of writer node

--- tests/e2e/tests/08-dataflow/create-run-etl-pipeline.spec.ts
+++ tests/e2e/tests/08-dataflow/create-run-etl-pipeline.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-const TEST_NAME = 'create-run-pipelines'
+const TEST_NAME = 'create-run-etl-pipeline'
 const SHOULD_SKIP = false
 test.fixme(SHOULD_SKIP, `${TEST_NAME} test is temporarily disabled.`)
 
@@ -110,15 +110,52 @@ test(TEST_NAME, async ({ page }) => {
     await page.getByLabel('Create new dataflow').getByRole('button').click();
   }
   await page.getByRole('textbox', { name: 'Name' }).click();
-  await page.getByRole('textbox', { name: 'Name' }).fill('TestDE');
+  await page.getByRole('textbox', { name: 'Name' }).fill('ETL - Test Stable Nodes');
   await page.getByRole('textbox', { name: 'Comment' }).click();
   await page.getByRole('textbox', { name: 'Comment' }).fill('DE Testing');
   await page.getByRole('button', { name: 'Create' }).click();
-  await page.getByText('PythonStableRun python code.').click();
-  // await expect(page.getByRole('combobox', { name: 'TestDE' })).toBeVisible();
-  // await page.getByRole('combobox', { name: 'TestDE' }).click();
-  // await expect(page.getByRole('option', { name: 'TestDE' })).toBeVisible();
-  // await page.getByRole('option', { name: 'TestDE' }).click();
+
+  // Check if 'PythonStableRun python code.' is not present, then click 'Add node' and the button with the same text
+  const pythonNodeText = 'PythonStableRun python code.';
+  const pythonNodeCheck = page.getByText(pythonNodeText);
+  // Wait for the Python node to be visible with a 5-second timeout
+  const isPythonNodeVisible = await pythonNodeCheck.isVisible({ timeout: 5000 }).catch(() => false);
+  // Wait for the timeout and then check if the Python node is visible
+  await page.waitForTimeout(5000);
+  const isPythonNodeVisible2 = await pythonNodeCheck.isVisible();
+  if (!isPythonNodeVisible2) {
+    await page.getByRole('button', { name: 'Add node' }).click();
+    await page.getByText(pythonNodeText).click();
+  } else {
+    await page.getByText(pythonNodeText).click();
+  }
+
+  // Click the button to add variables
+  const variablesMainButton = page.getByLabel('Variables').getByRole('button');
+  await variablesMainButton.click();
+
+  // Enter 'import json' into the input box and press Enter
+  const inputBox = page.locator('input#autocomplete-flow-tags');
+  await inputBox.fill('import json');
+  await inputBox.press('Enter');
+
+  // Click the "Add Variable" button
+  await page.locator('button[title="Add Variable"]').click();
+
+  // Verify 'Key' and 'Value' fields are visible under 'Variables'
+  const variablesSection = page.locator('.flow-variables-drawer__variables');
+  await expect(variablesSection).toBeVisible();
+  await expect(variablesSection.locator('label:has-text("Key")')).toBeVisible();
+  await expect(variablesSection.locator('label:has-text("Value")')).toBeVisible();
+
+  // Fill the input field with placeholder 'Enter variable key'
+  await page.locator('input[placeholder="Enter variable key"]').fill('append_text');
+
+  // Fill the textarea with placeholder 'Enter variable value'
+  await page.locator('textarea[placeholder="Enter variable value"]').fill('test');
+  
+  //Apply changes
+  await page.getByRole('button', { name: 'Apply' }).click();
 
   // Edit python node
   const pythonNodeTestId = await getNodeTestIdByLabel('python_node_0');
@@ -139,7 +176,7 @@ test(TEST_NAME, async ({ page }) => {
   await page.locator('d4l-input').filter({ hasText: 'Description' }).getByPlaceholder(' ').click();
   await page.locator('d4l-input').filter({ hasText: 'Description' }).getByPlaceholder(' ').fill('testing');
   await page.getByText('def exec(myinput): return "').click();
-  await fillMonacoEditor('def exec(myinput):\n df = myinput.get("db_reader_node_0").result\ndf["source_description"] = df["source_description"].astype(str) + "."\nreturn df');
+  await fillMonacoEditor('import json\n\ndef exec(myinput):\n data = {"name": "John", "age": 30}\njson_string = json.dumps(data)\nprint(json_string)\ndf = myinput.get("db_reader_node_0").result\ndf["source_description"] = df["source_description"].astype(str) + "."\nreturn df');
 
   // Ensure python node is edited
   await page.getByRole('button', { name: 'Apply' }).click();
@@ -170,9 +207,9 @@ test(TEST_NAME, async ({ page }) => {
   await expect(dbWriterNode).toMatchAriaSnapshot(`- text: Describe the task of node db_writer_node_0`);
 
   await moveNodeByLabel('db_reader_node_0', -360, 20);
-  await moveNodeByLabel('test_python_node', -180, 20);
+  await moveNodeByLabel('test_python_node', -100, 20);
   await moveNodeByLabel('py2table_node_0', 0, 20);
-  await moveNodeByLabel('db_writer_node_0', 250, 20);
+  await moveNodeByLabel('db_writer_node_0', 360, 20);
 
   await connectNodesByLabel('db_reader_node_0', 'test_python_node');
   await connectNodesByLabel('test_python_node', 'py2table_node_0');
@@ -218,9 +255,9 @@ test(TEST_NAME, async ({ page }) => {
   await page.getByRole('button', { name: 'Save' }).click();
   await page.getByRole('button').filter({ hasText: /^$/ }).click();
   await page.getByRole('textbox', { name: 'Name' }).click();
-  await page.getByRole('textbox', { name: 'Name' }).fill('Test_DE');
+  await page.getByRole('textbox', { name: 'Name' }).fill('ETL - Test Stable Nodes_DE');
   await page.getByRole('textbox', { name: 'Describe your changes' }).click();
-  await page.getByRole('textbox', { name: 'Describe your changes' }).fill('Test_DE for testing');
+  await page.getByRole('textbox', { name: 'Describe your changes' }).fill('ETL - Test Stable Nodes_DE for testing');
   await page.getByRole('button', { name: 'Save' }).click();
 
   // Run flow
@@ -229,5 +266,22 @@ test(TEST_NAME, async ({ page }) => {
   const runningButton = runningPanel.locator('button.run-flow-button.run-flow-button--running');
   await expect(runningButton).toBeVisible();
   await expect(runningButton).toBeDisabled();
-  await expect(runningPanel).toBeHidden({ timeout: 20000 });
+  await expect(runningPanel).toBeHidden({ timeout: 25000 });
+
+  // Verify "View Output" buttons for all nodes
+  const nodes = ['db_reader_node_0', 'test_python_node', 'py2table_node_0', 'db_writer_node_0'];
+  for (const nodeLabel of nodes) {
+    const node = getNodeByLabel(nodeLabel);
+    await expect(node).toBeVisible();
+    const viewOutputButton = node.locator('button', { hasText: 'View Output' });
+    await expect(viewOutputButton).toBeVisible();
+  }
+
+  // Verify "View Output" button output for db_writer_node_0
+  await dbWriterNode.locator('button', { hasText: 'View Output' }).click();
+  await page.waitForTimeout(3000); // Wait for 3 seconds for the panel to open
+
+  // Check if the content with 'data-mode-id="plaintext"' contains 'error: false'
+  const plainTextContent = page.locator('[data-mode-id="plaintext"]');
+  await expect(plainTextContent).toContainText('"error": false');
 });
