4f89811b806824cd9e41804e31148ff05bc39586
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2026-05-02 21:08:52 +0800

    fix

--- plugins/functions/d2e-webapi/src/routes/vocabulary.ts
+++ plugins/functions/d2e-webapi/src/routes/vocabulary.ts
@@ -387,7 +387,6 @@ export const vocabulary: FastifyPluginAsyncZod = async function (app) {
         sortOrder
       );
 
-      console.log("DEBUG searchConcept result:", JSON.stringify(result));
       res.send(result);
     }
   );
