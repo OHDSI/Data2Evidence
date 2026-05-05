977caae2bb40fa9cdf4f8ffcd4d9db4c2acac77c
Khairul Syazwan <khairul.masri@data4life-asia.care>
2026-05-05 14:52:16 +0800

    fix(layout): enforce full-height contract for portal vue-mri

--- plugins/ui/apps/portal/src/singleSpa/SingleSpaAppContainer.tsx
+++ plugins/ui/apps/portal/src/singleSpa/SingleSpaAppContainer.tsx
@@ -7,7 +7,15 @@ interface SingleSpaAppContainerProps {
   appName: string;
 }
 
-const wrapperStyle: React.CSSProperties = { position: "relative", width: "100%", height: "100%" };
+const wrapperStyle: React.CSSProperties = {
+  position: "relative",
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  flex: 1,
+  minHeight: 0,
+};
 
 const loaderContainerStyle: React.CSSProperties = {
   position: "absolute",
@@ -23,6 +31,10 @@ const containerStyle: React.CSSProperties = {
   width: "100%",
   height: "100%",
   position: "relative",
+  display: "flex",
+  flexDirection: "column",
+  flex: 1,
+  minHeight: 0,
 };
 
 export const SingleSpaAppContainer: React.FC<SingleSpaAppContainerProps> = ({ appName }) => {
