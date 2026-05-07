60d558c66b985bbc70677955f954560f028145ad
hengxian-jiang <hengxian.jiang@u.nus.edu>
2026-05-07 15:59:04 +0800

    Temp take screenshots, update ui mount in ci

--- tests/e2e/tests/09-patient-analytics/extended-query-logic.spec.ts
+++ tests/e2e/tests/09-patient-analytics/extended-query-logic.spec.ts
@@ -1,11 +1,12 @@
 import { test, expect } from '../fixtures'
+import { takeScreenshot } from '../screenshot-capture'
 
 const TEST_NAME = 'patient-analytics-extended-query-logic'
 const SHOULD_SKIP = false
 test.fixme(SHOULD_SKIP, `${TEST_NAME} test is temporarily disabled.`)
 test.describe.configure({ retries: 3 }) // Re-try up to 3 times for flaky tests
 
-test(TEST_NAME, async ({ page }) => {
+test(TEST_NAME, async ({ page }, testInfo) => {
   await page.goto('/d2e/portal')
   await page.locator('input[name="identifier"]').click()
   await page.locator('input[name="identifier"]').fill('admin')
@@ -46,12 +47,12 @@ test(TEST_NAME, async ({ page }) => {
   // This one does the actual change to AND
   await page.getByRole('button', { name: 'AND ' }).first().click()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
-  await expect(page).toHaveScreenshot()
+  await takeScreenshot(page, testInfo)
 
   // Click OR to change into AND
   await page.getByRole('button', { name: 'OR ' }).first().click()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
-  await expect(page).toHaveScreenshot()
+  await takeScreenshot(page, testInfo)
 
   // Click AND to change into OR
   await page.getByRole('button', { name: 'AND ' }).first().click()
@@ -69,7 +70,7 @@ test(TEST_NAME, async ({ page }) => {
   await page.getByText('Condition Occurrence B').nth(1).hover()
   await page.locator('#pane-right').getByText('Condition Start Date').click()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
-  await expect(page).toHaveScreenshot()
+  await takeScreenshot(page, testInfo)
 
   // Click and Drag and press drilldown
   await page.mouse.move(800, 200)
