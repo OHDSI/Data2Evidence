e255cddf26c66c0742faa87e6f6d88bb370a1bd2
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2026-06-21 12:24:27 +0800

    atlas: register @ohdsi/jobs-plugin (Jobs menu, separate from Atlas3/Prefect jobs)

--- plugins/atlas/scripts/postinstall.js
+++ plugins/atlas/scripts/postinstall.js
@@ -188,6 +188,7 @@ const PLUGINS = [
   { pkg: '@ohdsi/strategus-plugin', id: 'strategus-plugin', repoints: [] },
   { pkg: '@ohdsi/notebook-plugin', id: 'notebook-plugin', repoints: [] },
   { pkg: '@ohdsi/network-plugin', id: 'network-plugin', repoints: [] },
+  { pkg: '@ohdsi/jobs-plugin', id: 'jobs-plugin', repoints: [] },
 ];
 
 for (const { pkg, id, repoints } of PLUGINS) {
