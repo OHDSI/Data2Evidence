276e3ad2028cf8f295a4dccd17318f7ed5f0115e
suwarno ong <suwarno.ong@hotmail.com>
2026-07-02 12:01:20 +0800

    Take screenshots

--- tests/e2e/tests/09-patient-analytics/cohort-entry-and-exit.spec.ts
+++ tests/e2e/tests/09-patient-analytics/cohort-entry-and-exit.spec.ts
@@ -1,11 +1,12 @@
 import { test, expect } from '../fixtures'
+import { takeScreenshot } from '../screenshot-capture'
 
 const TEST_NAME = 'patient-analytics-cohort-entry-and-exit'
 const SHOULD_SKIP = false
 test.fixme(SHOULD_SKIP, `${TEST_NAME} test is temporarily disabled.`)
 test.describe.configure({ retries: 3 }) // Re-try up to 3 times for flaky tests
 
-test(TEST_NAME, async ({ page }) => {
+test(TEST_NAME, async ({ page }, testInfo) => {
   await page.goto('/d2e/portal')
   await page.locator('input[name="identifier"]').click()
   await page.locator('input[name="identifier"]').fill('admin')
@@ -54,12 +55,12 @@ test(TEST_NAME, async ({ page }) => {
   await page.locator('#pane-right').getByRole('list').getByText('Condition Occurrence A').click()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
   await page.waitForTimeout(2000) // Wait 2 seconds for "A filter card has been added..." popup in previous action to disappear
-  await expect(page).toHaveScreenshot()
+  await takeScreenshot(page, testInfo)
 
   // Change AND to OR, CEE should be removed from filtercards
   await page.getByRole('button', { name: 'AND ' }).click()
   await expect(page.locator('.loading-animation-component')).not.toBeVisible()
-  await expect(page).toHaveScreenshot()
+  await takeScreenshot(page, testInfo)
 
   // Go to PA config and uncheck CEE
   await page.getByRole('link', { name: 'Account' }).click()
