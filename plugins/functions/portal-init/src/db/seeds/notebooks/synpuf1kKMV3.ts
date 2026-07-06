a00ffcd2c1df7eeb4b3df983c4df8c312db1def5
Khairul Syazwan <khairul.masri@data4life-asia.care>
2026-07-06 10:27:53 +0800

    docs(notebooks): await async concept-set helper in seed notebooks
    Update commented get_concepts_in_concept_set calls to await now that
    pyodidepyqe helpers are async.

--- plugins/functions/portal-init/src/db/seeds/notebooks/synpuf1kKMV3.ts
+++ plugins/functions/portal-init/src/db/seeds/notebooks/synpuf1kKMV3.ts
@@ -50,7 +50,7 @@ conceptSetQuery.show_concept_set_list()
 Use get_concepts_in_concept_set() method to see all concepts in a particular concept set with concept_set_id
 
 # %% [python]
-# conceptSetQuery.get_concepts_in_concept_set(44)
+# await conceptSetQuery.get_concepts_in_concept_set(44)
 
 # %% [markdown]
 Define criteria for Type 2 Diabetes
