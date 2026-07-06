3119c2f684d411d30fd00e0c5bc4127b7de2dc7f
maggie-li-yd <maggie.li@data4life-asia.care>
2026-07-06 14:42:22 +0800

    add unit test for ai button

--- plugins/ui/apps/portal/src/components/AiAssistant/AiAssistantButton.tsx
+++ plugins/ui/apps/portal/src/components/AiAssistant/AiAssistantButton.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from "react";
 import Button from "@mui/material/Button";
 import { useEnabledFeatures } from "../../hooks";
+import { useTranslation } from "../../contexts";
 import "./AiAssistantButton.scss";
 
 const AI_ASSISTANT_FEATURE_FLAG = "aiAssistant";
 
 export const AiAssistantButton: FC = () => {
   const [features] = useEnabledFeatures();
+  const { getText, i18nKeys } = useTranslation();
 
   if (!features.includes(AI_ASSISTANT_FEATURE_FLAG)) {
     return null;
@@ -14,7 +16,7 @@ export const AiAssistantButton: FC = () => {
 
   return (
     <Button variant="contained" className="ai-assistant-fab">
-      Placeholder
+      {getText(i18nKeys.FEATURE__AI_ASSISTANT)}
     </Button>
   );
 };
