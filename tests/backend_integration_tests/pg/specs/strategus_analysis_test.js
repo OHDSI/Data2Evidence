1dbe8514411a5bf15a6feb934d557b9c062d05e8
santan maddi <saisantan2004@gmail.com>
2026-07-15 12:45:38 +0800

    add a comment

--- tests/backend_integration_tests/pg/specs/strategus_analysis_test.js
+++ tests/backend_integration_tests/pg/specs/strategus_analysis_test.js
@@ -93,7 +93,7 @@ describe('-- STRATEGUS ANALYSIS TESTS --', function () {
             datasetId: process.env.DATASET_ID,
             studyId: HARDCODED_STUDY.studyId,
             tokenStudyCode: HARDCODED_STUDY.tokenStudyCode,
-            uploadResults: false
+            uploadResults: false // TODO: set to true once we have a results schema
           }
         }),
         contentType: 'application/json',
