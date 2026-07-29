de6b3c96eeb4a5524581a760d13ee0285f1de740
brandantck <brandan.tanck@gmail.com>
2026-07-29 13:18:52 +0800

    update comment

--- plugins/functions/cdw-svc/src/utils/utils.ts
+++ plugins/functions/cdw-svc/src/utils/utils.ts
@@ -568,7 +568,7 @@ export async function getAnalyticsConnection(
 }
 
 const setSchema = async (connection, schema) => {
-  const escapedSchema = String(schema).replace(/"/g, '""'); // Remove double quotes
+  const escapedSchema = String(schema).replace(/"/g, '""'); // Escape double quotes for a quoted identifier
   await new Promise<void>((resolve, reject) => {
     connection.executeUpdate(`SET SCHEMA "${escapedSchema}"`, [], (err) => {
       if (err) {
